1379c9238161d01b5a5dca634df15babab248eae
afourmy <antoine.fourmy@gmail.com>
2017-11-21 07:43:16 +0100

    commit template

---
+++ app/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.template import loader
+from django.http import HttpResponse
+
+
+def index(request):
+    context = {}
+    template = loader.get_template('app/index.html')
+    return HttpResponse(template.render(context, request))
+
+
+def gentella_html(request):
+    context = {}
+    # The template to be loaded as per gentelella.
+    # All resource paths for gentelella end in .html.
+
+    # Pick out the html file name from the url. And load that template.
+    load_template = request.path.split('/')[-1]
+    template = loader.get_template('app/' + load_template)
+    return HttpResponse(template.render(context, request))
+

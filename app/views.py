01e008875fa4763ebd1955bf4c25002856360f46
afourmy <antoine.fourmy@gmail.com>
2017-11-21 16:22:27 +0100

    create device webpage step 1

--- app/views.py
+++ app/views.py
@@ -2,12 +2,10 @@ from django.shortcuts import render
 from django.template import loader
 from django.http import HttpResponse
 
-
 def index(request):
     context = {}
     template = loader.get_template('app/index.html')
     return HttpResponse(template.render(context, request))
-
 
 def gentella_html(request):
     context = {}

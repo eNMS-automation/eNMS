24b9ee4f40ea2cc75a7ee07d99461bf4b865dee2
afourmy <antoine.fourmy@gmail.com>
2017-11-22 16:30:51 +0100

    add excel import of devices
    Former-commit-id: e93f9e473ec8ddb6dba79a4c4009806b78526557

--- app/functions.py
+++ app/functions.py
@@ -1,3 +1,10 @@
+from .models import Device
+import xlrd
+
 def handle_uploaded_file(f):
-    for chunk in f.chunks():
-        print(chunk)
+    book = xlrd.open_workbook(file_contents=f.read())
+    sheet = book.sheet_by_index(0)
+    for row_index in range(1, sheet.nrows):
+        keys = ('hostname', 'ip_address', 'vendor')
+        values = sheet.row_values(row_index)
+        Device.objects.create(**dict(zip(keys, values)))

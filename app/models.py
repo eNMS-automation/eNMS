6edfb9bc7b05a8473b07470cf2e20d65eab3c73a
afourmy <antoine.fourmy@gmail.com>
2017-11-22 16:08:25 +0100

    update of the device overview webpage

--- app/models.py
+++ app/models.py
@@ -2,6 +2,7 @@ from __future__ import unicode_literals
 from django.db import models
 
 class Device(models.Model):
-    hostname = models.CharField(max_length=30)
-    ip_address = models.CharField(max_length=16)
-    vendor = models.CharField(max_length=30)
+    
+    hostname = models.CharField(max_length=30, verbose_name="Hostname")
+    ip_address = models.CharField(max_length=16, verbose_name="IP address")
+    vendor = models.CharField(max_length=30, verbose_name="Vendor")

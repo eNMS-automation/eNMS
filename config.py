38b35d195218270850324515988e0a0f69a02a30
afourmy <antoine.fourmy@gmail.com>
2017-12-09 08:37:04 +0100

    update
    Former-commit-id: ddabdd402fd36d64b78e004b80f4dc861ce84c5c

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
+from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 import os.path as op
 
 basedir = op.abspath(op.dirname(__file__))
@@ -7,3 +8,11 @@ SECRET_KEY = 'dw)$@(#--dadwa'
 # SQL Alchemy
 SQLALCHEMY_DATABASE_URI = 'sqlite:///' + op.join(basedir, 'database.db')
 SQLALCHEMY_TRACK_MODIFICATIONS = False
+
+# # AP Scheduler
+JOBS = []
+SCHEDULER_JOBSTORES = {'default': SQLAlchemyJobStore(url='sqlite:///flask_context.db')}
+SCHEDULER_API_ENABLED = True
+# SCHEDULER_EXECUTORS = {
+#     'default': {'type': 'threadpool', 'max_workers': 100}
+# }

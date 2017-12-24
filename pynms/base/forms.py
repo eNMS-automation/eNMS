bab7dcbb68c0c9802153346079bfed1665df492b
minto <minto@hotmail.fr>
2017-12-24 02:41:34 +0100

    dashboard control first part
    Former-commit-id: 28067ef3cbbecb749a01ba9877f069d152fdef93

--- pynms/base/forms.py
+++ pynms/base/forms.py
@@ -1,5 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import *
+from objects.properties import *
 
 ## Login and registration
 
@@ -11,4 +12,11 @@ class CreateAccountForm(FlaskForm):
     username = TextField('Username')
     email = TextField('Email')
     password = PasswordField('Password')
-    
+
+class NodePropertiesForm(FlaskForm):
+    property_choices = [(p, pretty_names[p]) for p in node_public_properties]
+    properties = SelectMultipleField('Node properties', choices=property_choices)
+
+class LinkPropertiesForm(FlaskForm):
+    property_choices = [(p, pretty_names[p]) for p in link_public_properties]
+    properties = SelectMultipleField('Link properties', choices=property_choices)

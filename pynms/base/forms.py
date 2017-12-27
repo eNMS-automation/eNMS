4652f978bba7c4a6cf7c72d428dd0e8a1915e05d
minto <minto@hotmail.fr>
2017-12-27 09:18:45 +0100

    switch login / registration system to users blueprint bugs

--- pynms/base/forms.py
+++ pynms/base/forms.py
@@ -3,17 +3,6 @@ from wtforms import *
 from objects.properties import *
 from .properties import pretty_names
 
-## Login and registration
-
-class LoginForm(FlaskForm):
-    username = TextField('Username')
-    password = PasswordField('Password')
-    
-class CreateAccountForm(FlaskForm):
-    username = TextField('Username')
-    email = TextField('Email')
-    password = PasswordField('Password')
-
 class DiagramPropertiesForm(FlaskForm):
     node_choices = [(p, pretty_names[p]) for p in node_diagram_properties]
     node_properties = SelectMultipleField('Node properties', choices=node_choices)

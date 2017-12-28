0ef5a4b92ea50eb8b33dd5961504685826793855
minto <minto@hotmail.fr>
2017-12-28 10:55:42 +0100

    fix node ssh connection with putty part 1
    Former-commit-id: b5cc59ba6ade5c4c2c92178438c06e763c2e7c07

--- pynms/views/routes.py
+++ pynms/views/routes.py
@@ -1,10 +1,12 @@
 from base.routes import _render_template
-from flask import Blueprint, request
+from flask import Blueprint, current_app, jsonify, request
 from flask_login import login_required
 from .forms import *
 from objects.models import Node, Link
 from objects.properties import *
+from os.path import join
 from re import search
+from subprocess import Popen
 
 blueprint = Blueprint(
     'views_blueprint', 
@@ -33,7 +35,10 @@ def view(view_type):
                 # of the node property matches the regular expression.
                 else search(request.form[obj.class_type + property], str(value))
                 for property, value in obj.__dict__.items()
+                # we consider only public properties
                 if property in obj.get_properties()
+                # providing that the property field in the form is not empty
+                # (empty field <==> property ignored)
                 and request.form[obj.class_type + property]
                 )
     return _render_template(
@@ -54,19 +59,13 @@ def view(view_type):
             for obj in filter(view_filter, Link.query.all())
         })
 
-@blueprint.route('/ajax_connection_to_node2', methods = ['POST'])
+@blueprint.route('/putty_connection', methods = ['POST'])
 @login_required
-def ajax_request2():
-    id = request.form['id']
-    print(id)
-    return jsonify(id=id)
-
-@blueprint.route('/ajax_connection_to_node', methods = ['POST'])
-@login_required
-def ajax_request():
-    print(t*100000)
-    ip_address = request.form['ip_address']
-    path_putty = join(APPS_FOLDER, 'putty.exe')
-    ssh_connection = '{} -ssh {}'.format(path_putty, ip_address)
+def putty_connection():
+    node = current_app.database.session.query(Node)\
+        .filter_by(id=request.form['id'])\
+        .first()
+    path_putty = join(current_app.path_apps, 'putty.exe')
+    ssh_connection = '{} -ssh {}'.format(path_putty, node.ip_address)
     connect = Popen(ssh_connection.split())
-    return jsonify(ip_address=ip_address)
+    return jsonify(id=id)

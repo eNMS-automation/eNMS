a84dc1ac5153eae0dfa2c46c4d09ca7164000386
minto <minto@hotmail.fr>
2017-12-30 11:13:13 +0100

    app factory part 2

--- source/base/database.py
+++ source/base/database.py
@@ -2,9 +2,8 @@ from flask_sqlalchemy import SQLAlchemy
 from sqlalchemy import create_engine
 from sqlalchemy.orm import scoped_session, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
-from main import app
 
-db = SQLAlchemy(app)
+db = SQLAlchemy()
 
 engine = create_engine(
     'sqlite:///database.db', 
--- source/main.py
+++ source/main.py
@@ -14,6 +14,9 @@ path_app = dirname(abspath(stack()[0][1]))
 if path_app not in sys.path:
     sys.path.append(path_app)
 
+from base.database import db, init_db
+from users.routes import login_manager
+
 def initialize_paths(app):
     path_source = os.path.dirname(os.path.abspath(__file__))
     path_parent = abspath(join(path_source, pardir))
@@ -27,7 +30,17 @@ def start_scheduler(app):
     scheduler.init_app(app)
     scheduler.start()
     return scheduler
-    
+
+def register_extensions(app):
+    db.init_app(app)
+    login_manager.init_app(app)
+    app.database = db
+    app.login_manager = login_manager
+
+def register_blueprints(app):
+    for name in ('base', 'users', 'objects', 'views', 'automation', 'scheduling'):
+        module = __import__(name + '.routes', globals(), locals(), [''])
+        app.register_blueprint(module.blueprint)
 
 def create_app(config='config'):
     app = Flask(__name__, static_folder='base/static')
@@ -35,33 +48,24 @@ def create_app(config='config'):
     
     initialize_paths(app)
     app.scheduler = start_scheduler(app)
+    register_extensions(app)
+    register_blueprints(app)
+
     return app
 
 app = create_app()
 
-
-
-
-
-
-# start the login system
-login_manager = flask_login.LoginManager()
-login_manager.init_app(app)
-
-
-for name in ('base', 'users', 'objects', 'views', 'automation', 'scheduling'):
-    module = __import__(name + '.routes', globals(), locals(), [''])
-    app.register_blueprint(module.blueprint)
-
-from base.database import db, init_db
 from users.models import User
-
-app.database = db
-app.login_manager = login_manager
 
 @login_manager.user_loader
 def user_loader(id):
     return db.session.query(User).filter_by(id=id).first()
+
+@login_manager.request_loader
+def request_loader(request):
+    username = request.form.get('username')
+    user = db.session.query(User).filter_by(username=username).first()
+    return user if user else None
 
 ## tear down SQLAlchemy 
 

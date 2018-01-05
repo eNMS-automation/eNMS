c6be67319e754ea73c04989dda124dc9acb74b1a
afourmy <antoine.fourmy@gmail.com>
2018-01-05 18:18:09 +0100

    update

--- source/views/forms.py
+++ source/views/forms.py
@@ -8,3 +8,8 @@ class ViewOptionsForm(FlaskForm):
     node_label = SelectField('Actions', choices=node_label_choices)
     link_label_choices = [(p, pretty_names[p]) for p in link_public_properties]
     link_label = SelectField('Actions', choices=link_label_choices)
+
+class GoogleEarthForm(FlaskForm):
+    name = TextField('Project name')
+    label_size = IntegerField('Label size', default=1)
+    line_width = IntegerField('Line width', default=2)

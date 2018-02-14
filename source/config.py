656d1e87e9570ee54dda3c7f7016b3af40434c3a
minto <minto@hotmail.fr>
2018-02-14 20:24:34 +0100

    fix scheduler bug: task db not created
    Former-commit-id: 779df805fabbc7a0b25935f793bc6af6221527dd

--- source/config.py
+++ source/config.py
@@ -1,25 +1,24 @@
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
-import os.path as op
 
-basedir = op.abspath(op.dirname(__file__))
-DEBUG = True
-SECRET_KEY = 'key'
-
-# SQL Alchemy
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + op.join(basedir, 'database.db')
-SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-# # AP Scheduler
-JOBS = []
-SCHEDULER_JOBSTORES = {
-    'default': SQLAlchemyJobStore(
-        url='sqlite:///flask_context.db'
-        )
-    }
-SCHEDULER_API_ENABLED = True
-SCHEDULER_EXECUTORS = {
-    'default': {
-        'type': 'threadpool', 
-        'max_workers': 500
+class Config(object):
+    DEBUG = True
+    SECRET_KEY = 'key'
+    
+    # SQL Alchemy
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
+    # # AP Scheduler
+    JOBS = []
+    SCHEDULER_JOBSTORES = {
+        'default': SQLAlchemyJobStore(
+            url = 'sqlite:///flask_context.db'
+            )
         }
-    }
+    SCHEDULER_API_ENABLED = True
+    SCHEDULER_EXECUTORS = {
+        'default': {
+            'type': 'threadpool', 
+            'max_workers': 500
+            }
+        }

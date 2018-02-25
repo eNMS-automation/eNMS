91c7524c32aa5bb1939d169adb8eb780c3d89ac5
minto <minto@hotmail.fr>
2018-02-25 13:56:23 +0100

    remove pytest cache
    Former-commit-id: 3ea32c651ccbf5110826f8e9927e7ee6fb325347

--- tests/test_module.py
+++ tests/test_module.py
@@ -4,3 +4,11 @@ def test_urls(client):
     
     r = client.get('/', follow_redirects=True)
     assert r.status_code == 200
+
+# def test_login(username, password):
+#     r = app.post('/login', data=dict(
+#         username=username,
+#         password=password
+#     ), follow_redirects=True)
+#     print(r)
+#     assert True

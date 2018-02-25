a04b8ab0aaa69b31aaf3d8f25fee675460c6feae
minto <minto@hotmail.fr>
2018-02-25 17:25:46 +0100

    test authentication part 2
    Former-commit-id: 556f00f79386a48e11b61198112bc2023cbc4d4e

--- tests/test_module.py
+++ tests/test_module.py
@@ -1,14 +1,59 @@
-def test_urls(client):
-    r = client.get('/')
-    assert r.status_code == 302
-    
-    r = client.get('/', follow_redirects=True)
-    assert r.status_code == 200
+# (Url, authorization required, redirection)
 
-# def test_login(username, password):
-#     r = app.post('/login', data=dict(
-#         username=username,
-#         password=password
-#     ), follow_redirects=True)
-#     print(r)
-#     assert True
+urls = {
+    '/': (
+        '',
+        'dashboard',
+        'dashboard_control',
+        'project'
+        ),
+    '/users/': (
+        'overview',
+        'manage_users',
+        'login',
+        'create_account',
+        'tacacs_server'
+        ),
+    '/objects/': (
+        'objects',
+        'object_creation',
+        'object_deletion',
+        'object_filtering'
+        ),
+    '/views/': (
+        'geographical_view',
+        'logical_view',
+        'google_earth_export'
+        ),
+    '/scripts/': (
+        'overview',
+        'script_creation',
+        'ansible_script'
+        ),
+    '/tasks/': (
+        'task_management',
+        'calendar'
+        ),
+    '/logs/': (
+        'overview',
+        'syslog_server'
+        )
+    }
+
+free_access = {'/', '/users/login', '/users/create_account'}
+
+def test_authentication(base_client):
+    for blueprint, pages in urls.items():
+        for page in pages:
+            page_url = blueprint + page
+            print(page_url)
+            expected_code = 200 if page_url in free_access else 403
+            r = base_client.get(page_url, follow_redirects=True)
+            assert r.status_code == expected_code
+
+def test_urls(user_client):
+    for blueprint, pages in urls.items():
+        for page in pages:
+            page_url = blueprint + page
+            r = user_client.get(page_url, follow_redirects=True)
+            assert r.status_code == 200

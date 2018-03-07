602537fc5bf1d11ed82e638ab478b4ee2c60ec47
minto <minto@hotmail.fr>
2018-03-07 21:37:12 +0100

    pep8 main
    Former-commit-id: 49105c532f3b3eb7945e795c09119d6070649c30

--- source/config.py
+++ source/config.py
@@ -1,29 +1,30 @@
 from apscheduler.jobstores.sqlalchemy import SQLAlchemyJobStore
 
+
 class Config(object):
 
     # SQL Alchemy
     SQLALCHEMY_DATABASE_URI = 'sqlite:///database.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    
-    # # AP Scheduler
+
+    # AP Scheduler
     JOBS = []
     SCHEDULER_JOBSTORES = {
-        'default': SQLAlchemyJobStore(
-            url = 'sqlite:///flask_context.db'
-            )
-        }
+        'default': SQLAlchemyJobStore(url='sqlite:///flask_context.db')
+    }
     SCHEDULER_API_ENABLED = True
     SCHEDULER_EXECUTORS = {
         'default': {
-            'type': 'threadpool', 
+            'type': 'threadpool',
             'max_workers': 500
-            }
         }
+    }
+
 
 class ProductionConfig(Config):
     DEBUG = False
     SECRET_KEY = 'change-me'
+
 
 class DebugConfig(Config):
     DEBUG = True
--- source/flask_app.py
+++ source/flask_app.py
@@ -3,22 +3,22 @@ from flask import Flask
 from flask_migrate import Migrate
 from importlib import import_module
 from os.path import abspath, dirname, join, pardir
-from sys import dont_write_bytecode, path
+import sys
 import logging
-import os
 
 # prevent python from writing *.pyc files / __pycache__ folders
-dont_write_bytecode = True
+sys.dont_write_bytecode = True
 
 path_source = dirname(abspath(__file__))
 path_parent = abspath(join(path_source, pardir))
-if path_source not in path:
-    path.append(path_source)
+if path_source not in sys.path:
+    sys.path.append(path_source)
 
 from base.database import db, create_database
 from logs.models import SyslogServer
 from tasks.models import scheduler
 from users.routes import login_manager
+
 
 def initialize_paths(app):
     app.path_upload = join(path_parent, 'projects')
@@ -26,11 +26,13 @@ def initialize_paths(app):
     app.kmz_path = join(path_parent, 'kmz')
     app.config['UPLOAD_FOLDER'] = app.path_upload
 
+
 def register_extensions(app, test):
     db.init_app(app)
     login_manager.init_app(app)
     if not test:
         scheduler.init_app(app)
+
 
 def register_blueprints(app):
     blueprints = (
@@ -41,43 +43,50 @@ def register_blueprints(app):
         'users',
         'views',
         'logs',
-        )
+    )
     for blueprint in blueprints:
         module = import_module('{}.routes'.format(blueprint))
         app.register_blueprint(module.blueprint)
+
 
 def configure_login_manager(app, User):
     @login_manager.user_loader
     def user_loader(id):
         return db.session.query(User).filter_by(id=id).first()
-    
+
     @login_manager.request_loader
     def request_loader(request):
         username = request.form.get('username')
         user = db.session.query(User).filter_by(username=username).first()
         return user if user else None
 
+
 def configure_database(app):
     create_database()
+
     @app.teardown_request
     def shutdown_session(exception=None):
         db.session.remove()
-    migrate = Migrate(app, db)
+    Migrate(app, db)
+
 
 def configure_syslog():
     try:
         syslog_server = db.session.query(SyslogServer).one()
         syslog_server.start()
-    except:
+    except Exception:
         pass
 
+
 def configure_logs(app):
-    logging.basicConfig(filename='error.log',level=logging.DEBUG)
+    logging.basicConfig(filename='error.log', level=logging.DEBUG)
     logger = logging.getLogger('netmiko')
     logger.addHandler(logging.StreamHandler())
 
+
 def configure_scheduler(scheduler):
     scheduler.start()
+
 
 def create_app(test=False):
     app = Flask(__name__, static_folder='base/static')
@@ -95,11 +104,8 @@ def create_app(test=False):
     configure_logs(app)
     return app
 
+
 app = create_app()
 
 if __name__ == '__main__':
-    app.run(
-        host = '0.0.0.0',
-        port = int(os.environ.get('PORT', 5100)),
-        threaded = True
-        )
+    app.run(host='0.0.0.0', port=5100, threaded=True)

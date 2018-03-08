c9f2d9fa3325845f190036830b65ec903476cf9e
minto <minto@hotmail.fr>
2018-03-08 06:58:26 +0100

    pep8 test fix
    Former-commit-id: 540e35c49a6fa4e25b2a69f397b719066dfeea91

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,7 @@
 from os import remove
 from os.path import abspath, dirname, join, pardir
-from sys import dont_write_bytecode, path
-from werkzeug.datastructures import ImmutableMultiDict
+from sys import path
 import pytest
-import tempfile
 
 path_source = dirname(abspath(__file__))
 path_parent = abspath(join(path_source, pardir))
@@ -15,19 +13,21 @@ if path_app not in path:
 
 from flask_app import create_app
 
+
 @pytest.fixture
 def base_client():
     app = create_app(test=True)
     yield app.test_client()
     remove(join(path_source, 'database.db'))
 
+
 @pytest.fixture
 def user_client():
     app = create_app(test=True)
     client = app.test_client()
+    create = {'username': '', 'password': '', 'create_account': ''}
+    login = {'username': '', 'password': '', 'login': ''}
     with app.app_context():
-        create = ImmutableMultiDict([('username', 'cisco'), ('password', 'cisco'), ('create_account', '')])
-        login = ImmutableMultiDict([('username', 'cisco'), ('password', 'cisco'), ('login', '')])
         r = client.post('/users/create_account', data=create)
         client.post('/users/login', data=login)
         yield client
--- tests/test_objects.py
+++ tests/test_objects.py
@@ -1,6 +1,25 @@
 from test_base import check_blueprints
 from conftest import path_projects
-from objects.models import *
+from objects.models import (
+    Antenna,
+    Firewall,
+    Host,
+    OpticalSwitch,
+    Regenerator,
+    Router,
+    Switch,
+    Server,
+    BgpPeering,
+    Etherchannel,
+    EthernetLink,
+    OpticalChannel,
+    OpticalLink,
+    Pseudowire,
+    Node,
+    node_class,
+    Link,
+    link_class
+)
 from os.path import join
 from werkzeug.datastructures import ImmutableMultiDict
 
@@ -9,6 +28,7 @@ from werkzeug.datastructures import ImmutableMultiDict
 # test the filtering system
 
 ## Object creation
+
 
 def define_node(subtype, description):
     return ImmutableMultiDict([
@@ -24,7 +44,8 @@ def define_node(subtype, description):
         ('latitude', '14'),
         ('secret_password', 'secret_password'),
         ('add_node', '')
-        ])
+    ])
+
 
 def define_link(subtype, source, destination, name):
     return ImmutableMultiDict([
@@ -36,14 +57,15 @@ def define_link(subtype, source, destination, name):
         ('source', source),
         ('destination', destination),
         ('add_link', '')
-        ])
+    ])
+
 
 def test_manual_object_creation(user_client):
     # we create two nodes per type
     for subtype in node_class:
         for description in ('desc1', 'desc2'):
             obj_dict = define_node(subtype, description)
-            r = user_client.post('/objects/object_creation', data=obj_dict)
+            user_client.post('/objects/object_creation', data=obj_dict)
     # for each type of link, we select the first 3 nodes in the node row
     # and create a link between each pair of nodes (including loopback links)
     for subtype in link_class:
@@ -52,8 +74,8 @@ def test_manual_object_creation(user_client):
             for destination in nodes[:3]:
                 name = '{}: {} to {}'.format(subtype, source, destination)
                 obj_dict = define_link(subtype, source, destination, name)
-                r = user_client.post('/objects/object_creation', data=obj_dict)
-    # there must be 
+                user_client.post('/objects/object_creation', data=obj_dict)
+    # there must be:
     for cls in node_class.values():
         # - two nodes per type of nodes
         assert(len(cls.query.all()) == 2)
@@ -65,10 +87,12 @@ def test_manual_object_creation(user_client):
     # - exactly 6*9 = 54 links in total
     assert len(Link.query.all()) == 54
 
+
 def create_from_file(client, file):
     with open(join(path_projects, file), 'rb') as f:
         data = dict(add_nodes='', file=f)
-        res = client.post('/objects/object_creation', data=data)
+        client.post('/objects/object_creation', data=data)
+
 
 @check_blueprints('/', '/objects/', '/views/')
 def test_object_creation_europe(user_client):
@@ -76,11 +100,13 @@ def test_object_creation_europe(user_client):
     assert len(Node.query.all()) == 33
     assert len(Link.query.all()) == 49
 
+
 @check_blueprints('/', '/objects/', '/views/')
 def test_object_creation_type(user_client):
     create_from_file(user_client, 'node_counters.xls')
     assert len(Node.query.all()) == 27
     assert not Link.query.all()
+
 
 @check_blueprints('/', '/objects/', '/views/')
 def test_object_creation_all_type(user_client):
@@ -100,50 +126,55 @@ def test_object_creation_all_type(user_client):
         OpticalChannel: 42,
         OpticalLink: 14,
         Pseudowire: 22
-        }
+    }
     for cls, number in number_per_subtype.items():
         assert len(cls.query.all()) == number
 
 ## Object deletion
 
+
 nodes = ImmutableMultiDict([('nodes', 'router' + str(i)) for i in range(5, 20)])
 links = ImmutableMultiDict([('links', 'link' + str(i)) for i in range(4, 15)])
+
 
 @check_blueprints('/', '/objects/', '/views/')
 def test_node_deletion(user_client):
     create_from_file(user_client, 'europe.xls')
-    res = user_client.post('/objects/object_deletion', data=nodes)
+    user_client.post('/objects/object_deletion', data=nodes)
     assert len(Node.query.all()) == 18
     assert len(Link.query.all()) == 18
+
 
 @check_blueprints('/', '/objects/', '/views/')
 def test_link_deletion(user_client):
     create_from_file(user_client, 'europe.xls')
-    res = user_client.post('/objects/object_deletion', data=links)
+    user_client.post('/objects/object_deletion', data=links)
     assert len(Node.query.all()) == 33
     assert len(Link.query.all()) == 38
 
 ## Object filtering
+
 
 filter_objects1 = ImmutableMultiDict([
     ('nodelocation', 'france|spain'),
     ('nodelocationregex', 'y'),
     ('linkname', 'link[1|2].'),
     ('linknameregex', 'y'),
-    ])
+])
 
 filter_objects2 = ImmutableMultiDict([
     ('nodelocation', 'france'),
     ('linkname', 'l.*k\\S3'),
     ('linknameregex', 'y'),
-    ])
+])
+
 
 @check_blueprints('/', '/objects/', '/views/')
 def test_object_filtering(user_client):
     create_from_file(user_client, 'europe.xls')
-    res = user_client.post('/objects/object_filtering', data=filter_objects1)
+    user_client.post('/objects/object_filtering', data=filter_objects1)
     assert len(Node.visible_choices()) == 21
     assert len(Link.visible_choices()) == 20
-    res = user_client.post('/objects/object_filtering', data=filter_objects2)
+    user_client.post('/objects/object_filtering', data=filter_objects2)
     assert len(Node.visible_choices()) == 12
     assert len(Link.visible_choices()) == 4

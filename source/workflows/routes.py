7e55d587ab1766f441e5ac3103eaa048f4bee898
minto <minto@hotmail.fr>
2018-03-30 21:42:22 +0200

    update workflow
    Former-commit-id: 41abcdadd8c5fee8ab5f4ce7df9fb385c1fca983

--- source/workflows/routes.py
+++ source/workflows/routes.py
@@ -1,23 +1,50 @@
+from base.properties import pretty_names
 from flask import Blueprint, render_template, request
 from flask_login import login_required
-from .forms import AddScriptForm
+from .forms import AddScriptForm, WorkflowCreationForm
+from .models import Workflow
 from scripts.models import Script
 
 blueprint = Blueprint(
     'workflows_blueprint',
     __name__,
-    url_prefix='/workflow',
+    url_prefix='/workflows',
     template_folder='templates',
     static_folder='static'
 )
 
 
+@blueprint.route('/overview', methods=['GET', 'POST'])
+@login_required
+def workflows():
+    return render_template(
+        'workflow_overview.html',
+        names=pretty_names,
+        fields=('name', 'description'),
+        workflows=Workflow.query.all(),
+    )
+
+
 @blueprint.route('/workflow_creation', methods=['GET', 'POST'])
 @login_required
 def workflow_creation():
+    form = WorkflowCreationForm(request.form)
+    if request.method == 'POST':
+        workflow = Workflow(**request.form)
+        db.session.add(workflow)
+        db.session.commit()
+    return render_template(
+        'workflow_creation.html',
+        form=form
+    )
+
+
+@blueprint.route('/manage_<workflow>', methods=['GET', 'POST'])
+@login_required
+def workflow_manager(workflow):
     form = AddScriptForm(request.form)
     form.scripts.choices = Script.choices()
     return render_template(
-        'workflow.html',
+        'workflow_manager.html',
         form=form
     )

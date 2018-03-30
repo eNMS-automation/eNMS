ec4dd11e70b745aa2bd00d9783e9c1f8b25d2ac5
minto <minto@hotmail.fr>
2018-03-30 21:18:30 +0200

    update

--- source/workflows/forms.py
+++ source/workflows/forms.py
@@ -1,6 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import SelectMultipleField
+from wtforms import SelectMultipleField, TextField
 
 
 class AddScriptForm(FlaskForm):
     scripts = SelectMultipleField('', choices=())
+
+
+class WorkflowCreationForm(FlaskForm):
+    name = TextField('Name')
+    description = TextField('Description')
--- source/workflows/models.py
+++ source/workflows/models.py
@@ -23,3 +23,6 @@ class Workflow(CustomBase):
     id = Column(Integer, primary_key=True)
     name = Column(String(120))
     edges = relationship('ScriptEdge')
+
+    def __init__(self):
+        pass

6893004c84a85a3209e870f87fdfb4038ba484fc
minto <minto@hotmail.fr>
2018-04-07 19:06:58 +0200

    ge refactoring part 1
    Former-commit-id: 15d9d3259bd311533975b2100104ecb4e7dbe927

--- source/views/routes.py
+++ source/views/routes.py
@@ -1,14 +1,24 @@
 from base.database import db, get_obj
 from base.properties import pretty_names, type_to_public_properties
 from collections import OrderedDict
-from flask import Blueprint, current_app, render_template, redirect, request, session, url_for
+from flask import (
+    Blueprint,
+    current_app,
+    jsonify,
+    render_template,
+    redirect,
+    request,
+    session,
+    url_for
+)
 from flask_login import current_user, login_required
 from .forms import GoogleEarthForm, SchedulingForm, ViewOptionsForm
 from json import dumps
-from objects.models import Filter, Node, node_subtypes, Link, link_class
+from objects.models import Filter, Node, node_subtypes, Link
 from os.path import join
 from scripts.models import Script
-from simplekml import Color, Kml, Style
+from simplekml import Kml
+from .styles import create_styles
 from subprocess import Popen
 from tasks.models import Task
 from workflows.models import Workflow
@@ -21,28 +31,7 @@ blueprint = Blueprint(
     static_folder='static'
 )
 
-styles = {}
-
-for subtype in node_subtypes:
-    point_style = Style()
-    point_style.labelstyle.color = Color.blue
-    path_icon = join(
-        blueprint.root_path,
-        'static',
-        'images',
-        'default',
-        '{}.gif'.format(subtype)
-    )
-    point_style.iconstyle.icon.href = path_icon
-    styles[subtype] = point_style
-
-for subtype, cls in link_class.items():
-    line_style = Style()
-    # we convert the RGB color to a KML color,
-    # i.e #RRGGBB to #AABBGGRR
-    kml_color = "#ff" + cls.color[-2:] + cls.color[3:5] + cls.color[1:3]
-    line_style.linestyle.color = kml_color
-    styles[subtype] = line_style
+styles = create_styles(blueprint.root_path)
 
 
 @blueprint.route('/<view_type>_view', methods=['GET', 'POST'])
@@ -151,11 +140,11 @@ def putty_connection():
         current_user.password
     )
     Popen(ssh_connection.split())
-    return dumps({'success': True}), 200, {'ContentType': 'application/json'}
+    return jsonify({})
 
 
 @blueprint.route('/selection', methods=['POST'])
 @login_required
 def selection():
     session['selection'] = list(set(request.form.getlist('selection[]')))
-    return dumps({'success': True}), 200, {'ContentType': 'application/json'}
+    return jsonify({})

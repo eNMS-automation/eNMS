72d28f6fe91cc2dc96b71d8b1b2dfcc2bf3eddcb
minto <minto@hotmail.fr>
2018-05-28 22:17:54 +0200

    scheduling.js apart

---
+++ source/base/static/enms/scheduling.js
@@ -0,0 +1,22 @@
+var dates = ['start_date', 'end_date'];
+var today = new Date();
+for (var i = 0; i < dates.length; i++) {
+  $('#' + dates[i]).datetimepicker({
+    format: 'DD/MM/YYYY HH:mm:ss',
+    widgetPositioning: {
+      horizontal: 'left',
+      vertical: 'bottom'
+    },
+    useCurrent: false,
+  });
+  $('#' + dates[i]).data("DateTimePicker").minDate(today);
+}
+
+function schedule(){
+  $('#wizard').smartWizard({
+    onFinish: scheduleScript
+  });
+  $('.buttonNext').addClass('btn btn-success');
+  $('.buttonPrevious').addClass('btn btn-primary');
+  $('.buttonFinish').addClass('btn btn-default');
+}

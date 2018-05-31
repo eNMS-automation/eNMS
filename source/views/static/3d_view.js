53c74f5c654ad5aa904ac03e26680ca28e607832
minto <minto@hotmail.fr>
2018-05-31 20:58:15 +0200

    update js geo 2

--- source/views/static/3d_view.js
+++ source/views/static/3d_view.js
@@ -1,7 +1,13 @@
-var options = {sky:true, atmosphere: true};
+var options = {sky: true, atmosphere: true};
 var map = new WE.map('earth_div', options);
 var current_layer = WE.tileLayer(layers['gm']);
 current_layer.addTo(map);
+
+function switch_layer(layer) {
+  current_layer.removeFrom(map);
+  current_layer = WE.tileLayer(layers[layer]);
+  current_layer.addTo(map);
+}
 
 for (i = 0; i < nodes.length; i++) { 
   var node = nodes[i]
@@ -15,47 +21,46 @@ for (i = 0; i < nodes.length; i++) {
     showObjectModal('node', '${node.id}');
   });
 
-  marker.node_id = {{ obj.id }};
+  marker.node_id = node.id;
   markers_array.push(marker);
 }
-{% for obj, properties in link_table.items() %}  
+
+for (i = 0; i < links.length; i++) {
+  var link = links[i] 
   var polygonSD = WE.polygon(
   [
-    [{{ obj['source']['latitude'] }}, {{ obj['source']['longitude'] }}],
-    [{{ obj['destination']['latitude'] }}, {{ obj['destination']['longitude'] }}],
-    [{{ obj['source']['latitude'] }}, {{ obj['source']['longitude'] }}]
-  ], {color: '{{ obj.color }}', opacity: 20}
+    [link.source_properties.latitude, link.source_properties.longitude],
+    [link.destination_properties.latitude, link.destination_properties.longitude],
+    [link.source_properties.latitude, link.source_properties.longitude]
+  ], {color: link.color, opacity: 20}
   ).addTo(map);
   var polygonDS = WE.polygon(
   [
-    [{{ obj['destination']['latitude'] }}, {{ obj['destination']['longitude'] }}],
-    [{{ obj['source']['latitude'] }}, {{ obj['source']['longitude'] }}],
-    [{{ obj['destination']['latitude'] }}, {{ obj['destination']['longitude'] }}]
-  ], {color: '{{ obj.color }}', opacity: 20}
+    [link.destination_properties.latitude, link.destination_properties.longitude],
+    [link.source_properties.latitude, link.source_properties.longitude],
+    [link.destination_properties.latitude, link.destination_properties.longitude]
+  ], {color: link.color, opacity: 20}
   ).addTo(map);
 
-  polygonSD.link_id = polygonDS.link_id = {{ obj.id }};
+  polygonSD.link_id = polygonDS.link_id = link.id;
   polyline_array.push(polygonSD, polygonDS);
-
-{% endfor %}
-{% endif %}
+}
 
 // when a filter is selected, apply it
 $('#select-filters').on('change', function() {
-var filterName = this.value;
-$.ajax({
-  type: "POST",
-  url: `/objects/pool_objects/${this.value}`,
-  dataType: "json",
-  success: function(objects){
-    for (i = 0; i < markers_array.length; i++) {
-      if (objects['nodes'].includes(markers_array[i].node_id)) {
-        markers_array[i].addTo(map);
-      } else {
-        markers_array[i].removeFrom(map);
+  var filterName = this.value;
+  $.ajax({
+    type: "POST",
+    url: `/objects/pool_objects/${this.value}`,
+    dataType: "json",
+    success: function(objects){
+      for (i = 0; i < markers_array.length; i++) {
+        if (objects['nodes'].includes(markers_array[i].node_id)) {
+          markers_array[i].addTo(map);
+        } else {
+          markers_array[i].removeFrom(map);
+        }
       }
-    }
-    {% if view == 'glearth' %}
       for (i = 0; i < polyline_array.length; i++) {
         try { polyline_array[i].destroy(); }
         catch(err) {};
@@ -85,25 +90,7 @@ $.ajax({
         polygonSD.link_id = polygonDS.link_id = obj_id;
         polyline_array.push(polygonSD, polygonDS);
       }
-    {% else %}
-      for (i = 0; i < polyline_array.length; i++) {
-          if (objects['links'][0].includes(polyline_array[i].link_id)) {
-            polyline_array[i].addTo(map);
-          } else {
-            polyline_array[i].removeFrom(map);
-          }
+      alertify.notify(`Filter '${filterName}' applied`, 'success', 5);
     }
-    {% endif %}
-    alertify.notify(`Filter '${filterName}' applied`, 'success', 5);
-  }
+  });
 });
-});
-{% if view != 'glearth' %}
-
-{% else %}
-function switch_layer(layer){
-current_layer.removeFrom(map);
-current_layer = WE.tileLayer(layers[layer]);
-current_layer.addTo(map);
-}
-{% endif %}

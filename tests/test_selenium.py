fbe6242b1d851a321b96832c00c755e98fe4011a
minto <minto@hotmail.fr>
2018-06-16 15:34:55 +0200

    add selenium chromedriver + selenium test

---
+++ tests/test_selenium.py
@@ -0,0 +1,14 @@
+from tests.test_base import urls
+
+
+# this test uses selenium to check, for all pages of gentelella, that there
+# are no severe entry in chromium logs.
+# it is renamed selenium_test instead of test_selenium to be ignored by pytest
+# (and therefore Travis CI) for now.
+def selenium_test(selenium_client):
+    for blueprint, pages in urls.items():
+        for page in pages:
+            selenium_client.get('http://127.0.0.1:5000' + blueprint + page)
+            for entry in selenium_client.get_log('browser'):
+                print(entry, blueprint, page)
+                assert entry['level'] != 'SEVERE'

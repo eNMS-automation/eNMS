7d3a78f4b93d17c4cbcad8e3b32e870c577db1cd
minto <minto@hotmail.fr>
2018-06-17 08:22:53 +0200

    update geo vie with correct icon name

--- eNMS/views/static/2dView.js
+++ eNMS/views/static/2dView.js
@@ -14,13 +14,13 @@ function switch_layer(layer){
 
 for (var i = 0; i < subtypes.length; i++) {
   window[`icon_${subtypes[i]}`] = L.icon({
-    iconUrl: `static/images/default/${subtypes[i]}.gif`,
+    iconUrl: `static/images/default/${subtypes[i].toLowerCase()}.gif`,
     iconSize: [18, 12], // size of the icon
     iconAnchor: [9, 6], // point of the icon which will correspond to marker's location
     popupAnchor: [8, -5] // point from which the popup should open relative to the iconAnchor
     });
   window[`icon_selected_${subtypes[i]}`] = L.icon({
-  iconUrl: `static/images/selected/${subtypes[i]}.gif`,
+  iconUrl: `static/images/selected/${subtypes[i].toLowerCase()}.gif`,
   iconSize: [18, 12], // size of the icon
   iconAnchor: [9, 6], // point of the icon which will correspond to marker's location
   popupAnchor: [8, -5] // point from which the popup should open relative to the iconAnchor

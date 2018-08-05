5908902a78e4e84c0dfe07ca787b97828b46b314
minto <minto@hotmail.fr>
2018-08-05 13:00:08 +0200

    fix travis coveralls

--- eNMS/views/static/views.js
+++ eNMS/views/static/views.js
@@ -1,26 +1,9 @@
-function scheduleTask() {
-  if ($("#scheduling-form").parsley().validate()) {
-    $.ajax({
-      type: "POST",
-      url: "/tasks/scheduler",
-      dataType: "json",
-      data: $("#scheduling-form").serialize(),
-      success: function() {
-      alertify.notify('Task scheduled.', 'success', 5);
-      }
-    });
-    $("#scheduling").modal('hide');
-  } else {
-    alertify.notify('Some fields are missing.', 'error', 5);
-  }
-}
-
 function connectToDevice(name) {
   var name = $(`#node-name`).val();
   $.ajax({
     type: "POST",
     url: `/views/connect_to_${name}`,
-    success: function(msg){
+    success: function(msg) {
       alertify.notify(`Connection to ${name}.`, 'success', 5);
     }
   });

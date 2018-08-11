1d4bbe51f80349d95cf20fac1e6c5f2fa5bba885
minto <minto@hotmail.fr>
2018-08-11 22:38:02 +0200

    javascript linting views part 6

--- eNMS/views/static/logicalView.js
+++ eNMS/views/static/logicalView.js
@@ -1,75 +1,82 @@
-var selected_nodes = [];
+/*
+global
+alertify: false
+d3: false
+graph: false
+partial: false
+showModal: false
+showObjectModal: false
+*/
+
+let selectedNodes = [];
 
 function sendSelection() {
-  $("#nodes").val(selected_nodes.map(s => s[1].real_id));
+  $('#nodes').val(selectedNodes.map((s) => s[1].real_id));
 }
 
 // selection function
 function selectNode(d) {
-  // we stop the propagation up the DOM tree so that the 
+  // we stop the propagation up the DOM tree so that the
   // unselectAll event bound to the canvas is not triggered
   d3.event.stopPropagation();
-  // add both the HTML and graph elements in the selected_nodes array
-  selected_nodes.push([this, d]);
+  // add both the HTML and graph elements in the selectedNodes array
+  selectedNodes.push([this, d]);
   d3.select(this)
     .select('image')
-    .attr("xlink:href", d.selected_img);
+    .attr('xlink:href', d.selected_img);
   sendSelection();
-};
+}
 
 // selection function
 function showNodeProperties(d) {
   showObjectModal('node', d.real_id);
-};
+}
 
 function showLinkProperties(d) {
   showObjectModal('link', d.real_id);
-};
+}
 
 function unselectAll() {
   d3.event.preventDefault();
-  for (var i = 0; i < selected_nodes.length; i++) {
-    d3.select(selected_nodes[i][0])
+  for (let i = 0; i < selectedNodes.length; i++) {
+    d3.select(selectedNodes[i][0])
       .select('image')
-      .attr("xlink:href", selected_nodes[i][1].img);
+      .attr('xlink:href', selectedNodes[i][1].img);
   }
-  selected_nodes = [];
+  selectedNodes = [];
   sendSelection();
-};
-
-var div = d3.select("body").append("div")	
-  .attr("class", "tooltip")				
-
-var width = 960,
-    height = 500
-
-var zoom = d3.behavior.zoom()
-  .scaleExtent([1, 50])
-  .on("zoom", zoomed);
-
-var svg = d3.select("#logical_view").append("svg")
-  .attr("width", width)
-  .attr("height", height)
-  .on("click", unselectAll)
-  .call(zoom)
-  .on("mousedown.zoom", null);
-
-
-function rect(x, y, w, h) {
-  return "M"+[x,y]+" l"+[w,0]+" l"+[0,h]+" l"+[-w,0]+"z";
 }
 
-var selection = svg.append("path")
-  .attr("class", "selection")
-  .attr("visibility", "hidden");
+const width = 960;
+const height = 500;
 
-var startSelection = function(start) {
-  selection.attr("d", rect(start[0], start[0], 0, 0))
-    .attr("visibility", "visible");
+let zoom = d3.behavior.zoom()
+  .scaleExtent([1, 50])
+  .on('zoom', zoomed);
+
+let svg = d3.select('#logical_view').append('svg')
+  .attr('width', width)
+  .attr('height', height)
+  .on('click', unselectAll)
+  .call(zoom)
+  .on('mousedown.zoom', null);
+
+function rect(x, y, w, h) {
+  return 'M'+[x, y]+' l'+[w, 0]+' l'+[0, h]+' l'+[-w, 0]+'z';
 };
 
-var moveSelection = function(start, moved) {
-  selection.attr("d", rect(
+let selection = svg.append('path')
+  .attr('class', 'selection')
+  .attr('visibility', 'hidden');
+
+let startSelection = function(start) {
+  selection
+    .attr('d', rect(start[0], start[0], 0, 0))
+    .attr('visibility', 'visible');
+};
+
+let moveSelection = function(start, moved) {
+  selection.attr('d', rect(
     start[0],
     start[1],
     moved[0] - start[0],
@@ -77,72 +84,70 @@ var moveSelection = function(start, moved) {
   ));
 };
 
-var endSelection = function(start, end) {
-    var min_x = Math.min(start[0], end[0]);
-        max_x = Math.max(start[0], end[0]);
-        min_y = Math.min(start[1], end[1]);
-        max_y = Math.max(start[1], end[1]);
-    console.log(start, end, min_x, max_x, min_y, max_y);
-  selection.attr("visibility", "hidden");
+let endSelection = function(start, end) {
+  const minX = Math.min(start[0], end[0]);
+  const maxX = Math.max(start[0], end[0]);
+  const minY = Math.min(start[1], end[1]);
+  const maxY = Math.max(start[1], end[1]);
+  selection.attr('visibility', 'hidden');
   // select all nodes in the rectangle
   d3.selectAll('.node')  //here's how you get all the nodes
     .each(function(d) {                    
-      if (min_x <= d.x && d.x <= max_x && min_y <= d.y && d.y <= max_y) {
-        selected_nodes.push([this, d]);
+      if (minX <= d.x && d.x <= maxX && minY <= d.y && d.y <= maxY) {
+        selectedNodes.push([this, d]);
         d3.select(this)
           .select('image')
-          .attr("xlink:href", d.selected_img);
+          .attr('xlink:href', d.selected_img);
       }
     });
   sendSelection();
 };
 
-svg.on("mousedown", function() {
+svg.on('mousedown', function() {
   // same binding as leaflet: shift + left-click button for selection
   if (!d3.event.shiftKey || ((d3.event.which !== 1) && (d3.event.button !== 1))) {
     return false;
   }
-  
-  var subject = d3.select(window);
+  let subject = d3.select(window);
       parent = this.parentNode;
       start = d3.mouse(parent);
   startSelection(start);
   subject
-    .on("mousemove.selection", function() {
+    .on('mousemove.selection', function() {
       moveSelection(start, d3.mouse(parent));
     })
-    .on("mouseup.selection", function() {
+    .on('mouseup.selection', function() {
       endSelection(start, d3.mouse(parent));
       subject
-        .on("mousemove.selection", null)
-        .on("mouseup.selection", null);
+        .on('mousemove.selection', null)
+        .on('mouseup.selection', null);
     });
 });
 
-svg.on("touchstart", function() {
-  var subject = d3.select(this);
+svg.on('touchstart', function() {
+  let subject = d3.select(this);
       parent = this.parentNode;
       id = d3.event.changedTouches[0].identifier;
       start = d3.touch(parent, id)
       pos;
     startSelection(start);
     subject
-      .on("touchmove."+id, function() {
+      .on('touchmove.'+id, function() {
         if (pos = d3.touch(parent, id)) {
           moveSelection(start, pos);
         }
       })
-      .on("touchend."+id, function() {
+      .on('touchend.'+id, function() {
         if (pos = d3.touch(parent, id)) {
           endSelection(start, pos);
           subject
-            .on("touchmove."+id, null)
-            .on("touchend."+id, null);
+            .on('touchmove.'+id, null)
+            .on('touchend.'+id, null);
         }
       });
 });
 
-var force = d3.layout.force()
+let force = d3.layout.force()
   .gravity(0.2)
   .distance(10)
   .charge(-100)
@@ -153,49 +158,49 @@ force
   .links(graph.links)
   .start();
 
-var container = svg.append("g");
+let container = svg.append('g');
 
-var link = container.selectAll(".link")
+let link = container.selectAll('.link')
   .data(graph.links)
-  .enter().append("line")
-  .attr("class", "link")
-  .on("dblclick", showLinkProperties);
+  .enter().append('line')
+  .attr('class', 'link')
+  .on('dblclick', showLinkProperties);
 
-var node = container.selectAll(".node")
+let node = container.selectAll('.node')
   .data(graph.nodes)
-  .enter().append("g")
-  .attr("class", "node")
+  .enter().append('g')
+  .attr('class', 'node')
   .call(force.drag)
-  .on("click", selectNode)
-  .on("dblclick", showNodeProperties);
+  .on('click', selectNode)
+  .on('dblclick', showNodeProperties);
 
 function zoomed() {
   container.attr(
-    "transform",
-    "translate(" + d3.event.translate + ")scale(" + d3.event.scale + ")"
+    'transform',
+    'translate(' + d3.event.translate + ')scale(' + d3.event.scale + ')'
   );
 }
 
-var images = node.append("image")
-  .attr("xlink:href", function(d) { return d.img; })
-  .attr("x", -8)
-  .attr("y", -8)
-  .attr("width", 16)
-  .attr("height", 16)
+let images = node.append('image')
+  .attr('xlink:href', function(d) { return d.img; })
+  .attr('x', -8)
+  .attr('y', -8)
+  .attr('width', 16)
+  .attr('height', 16)
 
-var label = node.append("text")
-  .attr("dx", 8)
-  .attr("dy", ".35em")
+let label = node.append('text')
+  .attr('dx', 8)
+  .attr('dy', '.35em')
   .text(function(d) { return d.name });
 
-force.on("tick", function() {
+force.on('tick', function() {
   link
-    .attr("x1", function(d) { return d.source.x; })
-    .attr("y1", function(d) { return d.source.y; })
-    .attr("x2", function(d) { return d.target.x; })
-    .attr("y2", function(d) { return d.target.y; });
-  node.attr("transform", function(d) {
-    return "translate(" + d.x + "," + d.y + ")";
+    .attr('x1', function(d) { return d.source.x; })
+    .attr('y1', function(d) { return d.source.y; })
+    .attr('x2', function(d) { return d.target.x; })
+    .attr('y2', function(d) { return d.target.y; });
+  node.attr('transform', function(d) {
+    return 'translate(' + d.x + ',' + d.y + ')';
   });
 });
 
@@ -203,32 +208,32 @@ force.on("tick", function() {
 $('#select-filters').on('change', function() {
     console.log(this.value);
   $.ajax({
-    type: "POST",
+    type: 'POST',
     url: `/objects/pool_objects/${this.value}`,
-    dataType: "json",
+    dataType: 'json',
     success: function(objects){
-      var nodes_id = objects.nodes.map(n => n.id);
-      var links_id = objects.links.map(l => l.id);
-      node.style("visibility", function(d) {      
-        return nodes_id.includes(d.real_id.toString()) ? "visible" : "hidden";
+      let nodes_id = objects.nodes.map(n => n.id);
+      let links_id = objects.links.map(l => l.id);
+      node.style('visibility', function(d) {      
+        return nodes_id.includes(d.real_id.toString()) ? 'visible' : 'hidden';
       });
-      link.style("visibility", function(d) {  
-        return links_id.includes(d.real_id.toString()) ? "visible" : "hidden";
+      link.style('visibility', function(d) {  
+        return links_id.includes(d.real_id.toString()) ? 'visible' : 'hidden';
       });
       alertify.notify(`Filter applied.`, 'success', 5);
     }
   });
 });
 
-var action = {
+let action = {
   'Parameters': partial(showModal, 'filters'),
   'Add new task': partial(showModal, 'scheduling'),
 }
 
-$("#logical_view").contextMenu({
-  menuSelector: "#contextMenu",
+$('#logical_view').contextMenu({
+  menuSelector: '#contextMenu',
   menuSelected: function (invokedOn, selectedMenu) {
-    var row = selectedMenu.text();
+    let row = selectedMenu.text();
     action[row]();
   }
 });

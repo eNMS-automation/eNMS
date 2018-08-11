c4595bfdef700e39e641461eec47529a3e1a27b8
minto <minto@hotmail.fr>
2018-08-12 00:22:33 +0200

    object javascript linting part 1

--- eNMS/objects/static/objectManagement.js
+++ eNMS/objects/static/objectManagement.js
@@ -1,44 +1,55 @@
-var nodeTable = $('#node-table').DataTable();
-var linkTable = $('#link-table').DataTable();
+/*
+global
+links: false
+link_fields: false
+nodes: false
+node_fields: false
+*/
+
+const nodeTable = $('#node-table').DataTable(); // eslint-disable-line new-cap
+const linkTable = $('#link-table').DataTable(); // eslint-disable-line new-cap
 
 function addObjectToTable(mode, type, properties) {
-  var values = [];
-  var table = type == 'node' ? nodeTable : linkTable;
-  var fields = type == 'node' ? node_fields : link_fields;
-  for (var i = 0; i < fields.length; i++) {
-    var truncate = ["longitude", "latitude"].includes(fields[i]);
-    value = truncate ? parseFloat(properties[fields[i]]).toFixed(2) : properties[fields[i]]
+  let values = [];
+  const table = type == 'node' ? nodeTable : linkTable;
+  const fields = type == 'node' ? node_fields : link_fields;
+  for (let i = 0; i < fields.length; i++) {
+    if (['longitude', 'latitude'].includes(fields[i])) {
+      value = parseFloat(properties[fields[i]]).toFixed(2);
+    } else {
+      properties[fields[i]];
+    }
     values.push(`${value}`);
   }
   values.push(
     `<button type="button" class="btn btn-info btn-xs"
     onclick="showObjectModal('${type}', '${properties.id}')">Edit</button>`,
     `<button type="button" class="btn btn-danger btn-xs"
-    onclick="deleteObject('${type}', '${properties.id}')">Delete</button>`,
+    onclick="deleteObject('${type}', '${properties.id}')">Delete</button>`
   );
   if (mode == 'edit') {
     table.row($(`#${type}-${properties.id}`)).data(values);
   } else {
-    var rowNode = table.row.add(values).draw(false).node();
-    $(rowNode).attr("id", `${type}-${properties.id}`);
+    rowNode = table.row.add(values).draw(false).node();
+    $(rowNode).attr('id', `${type}-${properties.id}`);
   }
 }
 
 (function() {
-  for (var i = 0; i < nodes.length; i++) {
+  for (let i = 0; i < nodes.length; i++) {
     addObjectToTable('create', 'node', nodes[i]);
   }
-  for (var i = 0; i < links.length; i++) {
+  for (let i = 0; i < links.length; i++) {
     addObjectToTable('create', 'link', links[i]);
   }
 })();
 
-document.getElementById("file").onchange = function() {
-  $("#form").submit();
+document.getElementById('file').onchange = function() {
+  $('#form').submit();
 };
 
 function showModal(type) {
   $('#title').text(`Add a new ${type}`);
-  $(`#edit-${type}-form`).trigger("reset");
+  $(`#edit-${type}-form`).trigger('reset');
   $(`#edit-${type}`).modal('show');
 }

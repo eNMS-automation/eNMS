2c96c0986e062f07484c6819da0be81e3b782089
minto <minto@hotmail.fr>
2018-08-12 06:34:19 +0200

    base js linting part 2

--- eNMS/base/static/enms/contextMenu.js
+++ eNMS/base/static/enms/contextMenu.js
@@ -1,43 +1,52 @@
-(function ($, window) {
-  $.fn.contextMenu = function (settings) {
-    return this.each(function () {
+(function($, window) {
+  $.fn.contextMenu = function(settings) {
+    return this.each(function() {
       // Open context menu
-      $(this).on("contextmenu", function (e) {
+      $(this).on('contextmenu', function(e) {
         // return native menu if pressing control
-        if (e.ctrlKey) return;
-        //open menu
-        var $menu = $(settings.menuSelector)
-          .data("invokedOn", $(e.target))
+        if (e.ctrlKey) {
+          return;
+        }
+        const $menu = $(settings.menuSelector)
+          .data('invokedOn', $(e.target))
           .show()
           .css({
-            position: "absolute",
+            position: 'absolute',
             left: getMenuPosition(e.clientX, 'width', 'scrollLeft'),
-            top: getMenuPosition(e.clientY, 'height', 'scrollTop')
+            top: getMenuPosition(e.clientY, 'height', 'scrollTop'),
           })
           .off('click')
-          .on('click', 'a', function (e) {
+          .on('click', 'a', function(e) {
             $menu.hide();
-            var $invokedOn = $menu.data("invokedOn");
-            var $selectedMenu = $(e.target);
+            const $invokedOn = $menu.data('invokedOn');
+            const $selectedMenu = $(e.target);
             settings.menuSelected.call(this, $invokedOn, $selectedMenu);
           });
         return false;
       });
-      //make sure menu closes on any click
-      $('body').click(function () {
+      // make sure menu closes on any click
+      $('body').click(function() {
         $(settings.menuSelector).hide();
       });
     });
 
+    /**
+    * Get menu position.
+    * @param {mouse} mouse
+    * @param {direction} direction
+    * @param {scrollDir} scrollDir
+    * @return {position}
+    */
     function getMenuPosition(mouse, direction, scrollDir) {
-      var win = $(window)[direction](),
-          scroll = $(window)[scrollDir](),
-          menu = $(settings.menuSelector)[direction](),
-          position = mouse + scroll;
+      const win = $(window)[direction]();
+      const scroll = $(window)[scrollDir]();
+      const menu = $(settings.menuSelector)[direction]();
+      let position = mouse + scroll;
       // opening menu would pass the side of the page
-      if (mouse + menu > win && menu < mouse) 
+      if (mouse + menu > win && menu < mouse) {
         position -= menu;
-        return position;
-    }    
+      }
+      return position;
+    }
   };
 })(jQuery, window);

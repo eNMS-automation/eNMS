915ed74f8377fb5cf04cb668ea17443e17ab291e
minto <minto@hotmail.fr>
2018-08-26 09:31:33 +0200

    debug mode by default

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ from sys import exit
 from config import config_dict
 from eNMS import create_app, db
 
-get_config_mode = environ.get('ENMS_CONFIG_MODE', 'Production')
+get_config_mode = environ.get('ENMS_CONFIG_MODE', 'Debug')
 
 try:
     config_mode = config_dict[get_config_mode.capitalize()]

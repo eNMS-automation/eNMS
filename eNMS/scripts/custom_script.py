84a1828150df71657d2656e3f358a764b45ec36e
minto <minto@hotmail.fr>
2018-09-04 17:03:58 +0200

    custom script reorg part 3

--- eNMS/scripts/custom_script.py
+++ eNMS/scripts/custom_script.py
@@ -1,10 +1,10 @@
+from importlib.util import spec_from_file_location, module_from_spec
 from pathlib import Path
 from sqlalchemy import Boolean, Column, exc, ForeignKey, Integer, String
 
 from nornir.core import Nornir
 from nornir.core.inventory import Inventory
 from nornir.plugins.tasks import networking
-
 
 from eNMS import db
 from eNMS.scripts.models import Script, type_to_class
@@ -34,43 +34,14 @@ class CustomScript(Script):
 type_to_class['custom_script'] = CustomScript
 
 
-# 
-# 
-# def nornir_ping_job(args):
-#     # Script that uses Nornir to ping a device
-#     task, node, results = args
-#     nornir_inventory = {node.name: {'nornir_ip': node.ip_address}}
-#     external = Nornir(inventory=Inventory(nornir_inventory), dry_run=True)
-#     ping_result = external.run(networking.tcp_ping, ports=[23, 443])
-#     return {
-#         'success': all(res for res in ping_result[node.name].result.keys()),
-#         'logs': str(ping_result[node.name].result)
-#     }
-# 
-# 
-# nornir_ping_parameters = {
-#     'name': 'nornir ping 23 443',
-#     'node_multiprocessing': True,
-#     'description': 'Uses Nornir to ping',
-#     'vendor': 'none',
-#     'operating_system': 'all',
-#     'job_name': 'nornir_ping_job'
-# }
-
-import importlib
-import importlib.util
-from inspect import getmembers
 def create_custom_scripts():
     path_scripts = Path.cwd() / 'eNMS' / 'scripts' / 'custom_scripts'
     for file in path_scripts.glob('**/*.py'):
-        if file == '__init__.py':
-            print('pass')
-            pass
-        spec = importlib.util.spec_from_file_location(str(file), file)
-        mod = importlib.util.module_from_spec(spec)
-        spec.loader.exec_module(mod)
+        spec = spec_from_file_location(str(file), file)
+        script_module = module_from_spec(spec)
+        spec.loader.exec_module(script_module)
         try:
-            custom_script = CustomScript(**getattr(mod, 'parameters'))
+            custom_script = CustomScript(**getattr(script_module, 'parameters'))
             db.session.add(custom_script)
             db.session.commit()
         except exc.IntegrityError:
---
+++ eNMS/scripts/custom_scripts/nornir/nornir_script.py
@@ -0,0 +1,20 @@
+def nornir_ping_job(args):
+    # Script that uses Nornir to ping a device
+    task, node, results = args
+    nornir_inventory = {node.name: {'nornir_ip': node.ip_address}}
+    external = Nornir(inventory=Inventory(nornir_inventory), dry_run=True)
+    ping_result = external.run(networking.tcp_ping, ports=[23, 443])
+    return {
+        'success': all(res for res in ping_result[node.name].result.keys()),
+        'logs': str(ping_result[node.name].result)
+    }
+
+
+nornir_ping_parameters = {
+    'name': 'nornir ping 23 443',
+    'node_multiprocessing': True,
+    'description': 'Uses Nornir to ping',
+    'vendor': 'none',
+    'operating_system': 'all',
+    'job_name': 'nornir_ping_job'
+}

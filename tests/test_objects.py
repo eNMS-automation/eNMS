79e3b47bb0d65959a18ef89686cc8537f2e4c839
minto <minto@hotmail.fr>
2018-09-05 15:41:41 +0200

    refactor models part 33

--- tests/test_objects.py
+++ tests/test_objects.py
@@ -1,8 +1,9 @@
 from os.path import join
 from werkzeug.datastructures import ImmutableMultiDict
 
+from eNMS.base.helpers import retrieve
 from eNMS.base.properties import node_subtypes, link_subtypes
-from eNMS.objects.models import Node, Link
+from eNMS.objects.models import Node, Link, Pool
 
 from tests.test_base import check_blueprints
 
@@ -41,25 +42,25 @@ def define_link(subtype, source, destination, name):
     ])
 
 
-# def test_manual_object_creation(user_client):
-#     # we create two nodes per type
-#     for subtype in node_subtypes:
-#         for description in ('desc1', 'desc2'):
-#             obj_dict = define_node(subtype, description)
-#             user_client.post('/objects/edit_object', data=obj_dict)
-#     # for each type of link, we select the first 3 nodes in the node row
-#     # and create a link between each pair of nodes (including loopback links)
-#     for subtype in link_subtypes:
-#         nodes = Node.query.all()
-#         for source in nodes[:3]:
-#             for destination in nodes[:3]:
-#                 name = f'{subtype}: {source.name} to {destination.name}'
-#                 obj_dict = define_link(subtype, source.id, destination.id, name)
-#                 user_client.post('/objects/edit_object', data=obj_dict)
-#     # - exactly 16 nodes in total
-#     assert len(Node.query.all()) == 16
-#     # - exactly 6*9 = 54 links in total
-#     assert len(Link.query.all()) == 54
+def test_manual_object_creation(user_client):
+    # we create two nodes per type
+    for subtype in node_subtypes:
+        for description in ('desc1', 'desc2'):
+            obj_dict = define_node(subtype, description)
+            user_client.post('/objects/edit_object', data=obj_dict)
+    # for each type of link, we select the first 3 nodes in the node row
+    # and create a link between each pair of nodes (including loopback links)
+    for subtype in link_subtypes:
+        nodes = Node.query.all()
+        for source in nodes[:3]:
+            for destination in nodes[:3]:
+                name = f'{subtype}: {source.name} to {destination.name}'
+                obj_dict = define_link(subtype, source.id, destination.id, name)
+                user_client.post('/objects/edit_object', data=obj_dict)
+    # - exactly 16 nodes in total
+    assert len(Node.query.all()) == 16
+    # - exactly 6*9 = 54 links in total
+    assert len(Link.query.all()) == 54
 
 
 def create_from_file(client, file):
@@ -82,35 +83,28 @@ def test_object_creation_type(user_client):
     assert not Link.query.all()
 
 
-# @check_blueprints('', '/objects', '/views')
-# def test_object_creation_all_type(user_client):
-#     create_from_file(user_client, 'all_types.xls')
-#     assert len(Node.query.all()) == 27
-#     assert len(Link.query.all()) == 49
-
-
 nodes = ['router' + str(i) for i in range(5, 20)]
 links = ['link' + str(i) for i in range(4, 15)]
 
 
-# @check_blueprints('', '/objects', '/views')
-# def test_node_deletion(user_client):
-#     create_from_file(user_client, 'europe.xls')
-#     for node_name in nodes:
-#         node = retrieve(Node, name=node_name)
-#         user_client.post(f'/objects/delete/node/{node.id}')
-#     assert len(Node.query.all()) == 18
-#     assert len(Link.query.all()) == 18
-# 
-# 
-# @check_blueprints('', '/objects', '/views')
-# def test_link_deletion(user_client):
-#     create_from_file(user_client, 'europe.xls')
-#     for link_name in links:
-#         link = retrieve(Link, name=link_name)
-#         user_client.post(f'/objects/delete/link/{link.id}')
-#     assert len(Node.query.all()) == 33
-#     assert len(Link.query.all()) == 38
+@check_blueprints('', '/objects', '/views')
+def test_node_deletion(user_client):
+    create_from_file(user_client, 'europe.xls')
+    for node_name in nodes:
+        node = retrieve(Node, name=node_name)
+        user_client.post(f'/objects/delete/node/{node.id}')
+    assert len(Node.query.all()) == 18
+    assert len(Link.query.all()) == 18
+
+
+@check_blueprints('', '/objects', '/views')
+def test_link_deletion(user_client):
+    create_from_file(user_client, 'europe.xls')
+    for link_name in links:
+        link = retrieve(Link, name=link_name)
+        user_client.post(f'/objects/delete/link/{link.id}')
+    assert len(Node.query.all()) == 33
+    assert len(Link.query.all()) == 38
 
 
 pool1 = ImmutableMultiDict([
@@ -129,17 +123,17 @@ pool2 = ImmutableMultiDict([
 ])
 
 
-# @check_blueprints('', '/objects', '/views')
-# def test_pool_management(user_client):
-#     create_from_file(user_client, 'europe.xls')
-#     user_client.post('/objects/process_pool', data=pool1)
-#     user_client.post('/objects/process_pool', data=pool2)
-#     p1, p2 = retrieve(Pool, name='pool1'), retrieve(Pool, name='pool2')
-#     assert len(p1.nodes) == 21
-#     assert len(p1.links) == 20
-#     assert len(p2.nodes) == 12
-#     assert len(p2.links) == 4
-#     assert len(Pool.query.all()) == 5
-#     user_client.post(f'/objects/delete_pool/{p1.id}')
-#     user_client.post(f'/objects/delete_pool/{p2.id}')
-#     assert len(Pool.query.all()) == 3
+@check_blueprints('', '/objects', '/views')
+def test_pool_management(user_client):
+    create_from_file(user_client, 'europe.xls')
+    user_client.post('/objects/process_pool', data=pool1)
+    user_client.post('/objects/process_pool', data=pool2)
+    p1, p2 = retrieve(Pool, name='pool1'), retrieve(Pool, name='pool2')
+    assert len(p1.nodes) == 21
+    assert len(p1.links) == 20
+    assert len(p2.nodes) == 12
+    assert len(p2.links) == 4
+    assert len(Pool.query.all()) == 5
+    user_client.post(f'/objects/delete_pool/{p1.id}')
+    user_client.post(f'/objects/delete_pool/{p2.id}')
+    assert len(Pool.query.all()) == 3

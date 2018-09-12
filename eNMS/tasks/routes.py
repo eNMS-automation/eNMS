3fb8ef2b4b2fb35de2bdc45bf606032381518646
minto <minto@hotmail.fr>
2018-09-12 11:23:02 +0200

    update

--- eNMS/tasks/routes.py
+++ eNMS/tasks/routes.py
@@ -101,7 +101,7 @@ def get_task(task_id):
 @blueprint.route('/show_logs/<task_id>', methods=['POST'])
 @login_required
 def show_logs(task_id):
-    return jsonify(dumps(retrieve(Task, id=task_id).logs, indent=2))
+    return jsonify(dumps(retrieve(Task, id=task_id).logs, indent=4))
 
 
 @blueprint.route('/get_diff/<task_id>/<v1>/<v2>/<n1>/<n2>', methods=['POST'])

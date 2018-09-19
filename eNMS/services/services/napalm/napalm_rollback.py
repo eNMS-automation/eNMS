18c2300ed0c29d8ea1c147440a5deb86d65df299
minto <minto@hotmail.fr>
2018-09-19 21:17:23 +0200

    update services 48

---
+++ eNMS/services/services/napalm/napalm_rollback.py
@@ -0,0 +1,45 @@
+from netmiko.ssh_dispatcher import CLASS_MAPPER
+from sqlalchemy import Column, Float, ForeignKey, Integer, String
+
+from eNMS.services.custom_service import CustomService, service_classes
+from eNMS.services.models import multiprocessing
+
+netmiko_drivers = sorted(
+    driver for driver in CLASS_MAPPER
+    if 'telnet' not in driver and 'ssh' not in driver
+)
+
+
+class NapalmRollbackService(CustomService):
+
+    __tablename__ = 'NapalmRollbackService'
+
+    id = Column(Integer, ForeignKey('CustomService.id'), primary_key=True)
+    vendor = Column(String)
+    operating_system = Column(String)
+    content = Column(String)
+    driver = Column(String)
+    global_delay_factor = Column(Float, default=1.)
+    device_multiprocessing = True
+
+    driver_values = [(driver, driver) for driver in netmiko_drivers]
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'napalm_rollback_service',
+    }
+
+    @multiprocessing
+    def job(self, task, device, results, incoming_payload):
+    try:
+        napalm_driver = napalm_connection(device)
+        napalm_driver.open()
+        napalm_driver.rollback()
+        napalm_driver.close()
+        result, success = 'Rollback successful', True
+    except Exception as e:
+        result = f'Napalm rollback did not work because of {e}'
+        success = False
+    return success, result, incoming_payload
+
+
+service_classes['Napalm Rollback Service'] = NapalmRollbackService

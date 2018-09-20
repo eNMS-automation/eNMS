b19bc3a81926125d737d2b132d324c034b451142
minto <minto@hotmail.fr>
2018-09-20 07:55:58 +0200

    gotty update 2

--- config.py
+++ config.py
@@ -24,14 +24,12 @@ class Config(object):
         }
     }
 
-    # GoTTY (webSSH connections)
+    # goTTY (webSSH connections)
     # Default: 20 ports reserved from 8080 to 8099)
     # eNMS will use these 20 ports as GoTTY WebSSH terminal
     GOTTY_ALLOWED_PORTS = list(range(8080, 8100))
-    
     # 'sshpass' must be installed on the server for the authentication
     GOTTY_AUTHENTICATION = True
-    
     # In production, it is likely that the web server (e.g nginx) allows
     # only one port. In that case, the web server can be configured to 
     # redirect the requests to another port, as GoTTY needs its own port to
@@ -40,10 +38,14 @@ class Config(object):
     # location /terminal1 {
     # proxy_pass  http://127.0.0.1:8080;
     # } 
-
     GOTTY_PORT_REDIRECTION = environ.get('GOTTY_PORT_REDIRECTION', False)
-
     GOTTY_ALLOWED_URLS = [f'terminal{i}' for i in range(3)]
+    # By default, each new client that tries to connect to a GoTTY terminal
+    # will have its own SSH session to the target device.
+    # If the port multiplexing option is enabled, clients will all share the
+    # same SSH session instead (they will actually share the same terminal
+    # with tmux)
+    GOTTY_MULTIPLEXING = environ.get('GOTTY_PORT_REDIRECTION', False)
 
 
 class DebugConfig(Config):
@@ -53,9 +55,13 @@ class DebugConfig(Config):
 
 class ProductionConfig(Config):
     DEBUG = False
+    # In production, the secret MUST be provided as an environment variable.
     SECRET_KEY = environ.get('ENMS_SECRET_KEY')
 
     # Vault
+    # In production, all credentials (hashes, usernames and passwords) are
+    # stored in a vault.
+    # There MUST be a Vault configured to use eNMS in production mode.
     VAULT_ADDR = environ.get('VAULT_ADDR')
     VAULT_TOKEN = environ.get('VAULT_TOKEN')
 

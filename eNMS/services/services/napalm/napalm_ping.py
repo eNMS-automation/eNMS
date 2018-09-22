961223fb32c41256d713be54962b8ffda1cbadec
minto <minto@hotmail.fr>
2018-09-22 18:32:33 +0200

    napalm ping part 1

---
+++ eNMS/services/services/napalm/napalm_ping.py
@@ -0,0 +1,44 @@
+from multiprocessing.pool import ThreadPool
+from sqlalchemy import Column, ForeignKey, Integer
+
+from eNMS.services.helpers import napalm_connection
+from eNMS.services.models import Service, service_classes
+
+
+class NapalmRollbackService(Service):
+
+    __tablename__ = 'NapalmRollbackService'
+
+    id = Column(Integer, ForeignKey('Service.id'), primary_key=True)
+    destination = Column(String)
+    source = Column(String)
+    vrf = Column(String)
+
+    __mapper_args__ = {
+        'polymorphic_identity': 'napalm_rollback_service',
+    }
+
+    def job(self, task, incoming_payload):
+        targets = task.compute_targets()
+        results = {'success': True}
+        pool = ThreadPool(processes=len(targets))
+        pool.map(self.device_job, [(device, results) for device in targets])
+        pool.close()
+        pool.join()
+        return results
+
+    def device_job(self, args):
+        device, results = args
+        try:
+            napalm_driver = napalm_connection(device)
+            napalm_driver.open()
+            napalm_driver.rollback()
+            napalm_driver.close()
+            result, success = 'Rollback successful', True
+        except Exception as e:
+            result, success = f'task failed ({e})', False
+            results['success'] = False
+        results[device.name] = {'success': success, 'result': result}
+
+
+service_classes['Napalm Rollback Service'] = NapalmRollbackService

2016597f543ef45ca343a963030d8db11598ce39
minto <minto@hotmail.fr>
2018-09-29 10:45:49 +0200

    refactor gotty connection system p2

--- eNMS/objects/static/objectCommonFunctions.js
+++ eNMS/objects/static/objectCommonFunctions.js
@@ -150,3 +150,36 @@ function importTopology(objType) { // eslint-disable-line no-unused-vars
     });
   }
 }
+
+/**
+ * Open new tab at the provided URL.
+ * @param {url} url - URL.
+ */
+function openUrl(url) {
+  let win = window.open(url, '_blank');
+  win.focus();
+}
+
+/**
+ * Start an SSH session to the device.
+ */
+function SshConnection(id) { // eslint-disable-line no-unused-vars
+  $.ajax({
+    type: 'POST',
+    url: `/views/connection/${id}`,
+    success: function(result) {
+      const protocol = window.location.protocol;
+      const hostname = window.location.hostname;
+      setTimeout(function () {
+        if (result.redirection) {
+          openUrl(`${protocol}//${hostname}/terminal${result.port}/`);
+        } else {
+          openUrl(`${protocol}//${hostname}:${result.port}`);
+        }
+      }, 500);
+      const message = `Connection to ${result.device} on port ${result.port}.`;
+      alertify.notify(message, 'success', 15);
+    },
+  });
+}
+

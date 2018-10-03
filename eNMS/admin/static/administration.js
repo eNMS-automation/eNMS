8ab9fac309523cef8c61ed0597d287363a0186da
minto <minto@hotmail.fr>
2018-10-03 09:05:10 +0200

    refactor admin panel part 2

--- eNMS/admin/static/administration.js
+++ eNMS/admin/static/administration.js
@@ -66,3 +66,35 @@ function queryNetbox() { // eslint-disable-line no-unused-vars
     });
   }
 }
+
+/**
+ * Save geographical parameters.
+ */
+function saveGeographicalParameters() { // eslint-disable-line no-unused-vars
+  if ($('#geographical-parameters-form').parsley().validate()) {
+    $.ajax({
+      type: 'POST',
+      url: '/admin/save_geographical_parameters',
+      data: $('#geographical-parameters-form').serialize(),
+      success: function() {
+        alertify.notify('Geographical parameters saved.', 'success', 5);
+      },
+    });
+  }
+}
+
+/**
+ * Save GoTTY parameters.
+ */
+function saveGottyParameters() { // eslint-disable-line no-unused-vars
+  if ($('#gotty-parameters-form').parsley().validate()) {
+    $.ajax({
+      type: 'POST',
+      url: '/admin/save_gotty_parameters',
+      data: $('#gotty-parameters-form').serialize(),
+      success: function() {
+        alertify.notify('GoTTY parameters saved.', 'success', 5);
+      },
+    });
+  }
+}

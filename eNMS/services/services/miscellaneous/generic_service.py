c700f6af8b052e8489bf6c06f4ed74a4d968c36f
minto <minto@hotmail.fr>
2018-10-06 00:58:03 +0200

    update services

--- eNMS/services/services/miscellaneous/generic_service.py
+++ eNMS/services/services/miscellaneous/generic_service.py
@@ -17,11 +17,22 @@ class GenericService(Service):
         return getattr(self, self.name)(task, incoming_payload)
 
     def process_payload1(self, task, payload):
-        config = payload['task_service_napalm_getter_get_config']['success']
-        intf = payload['task_service_napalm_getter_get_interfaces']['success']
-        facts = payload['task_service_napalm_getter_get_facts']['success']
-        success = config and intf and facts
-        return {'success': success, 'result': ''}
+        int_r8 = payload['task_service_napalm_getter_get_interfaces']['router8']
+        result_int_r8 = int_r8['result']['get_interfaces']
+        speed_Fa0 = result_int_r8['FastEthernet0/0']['speed']
+        speed_Fa1 = result_int_r8['FastEthernet0/1']['speed']
+        same_speed = speed_Fa0 == speed_Fa1
+        
+        facts_r8 = payload['task_service_napalm_getter_get_facts']['success']
+        result_facts_r8 = facts_r8['result']['get_facts']
+        uptime_less_than_50000 = result_facts_r8['uptime'] < 50000
+        return {
+            'success': success,
+            'result': {
+                'same_speed_Fa0_Fa1': same_speed,
+                'uptime_less_5000': uptime_less_than_50000
+            }
+        }
 
 
 service_classes['Generic Service'] = GenericService

d2699dea1687a8ce334bd547222966f197cf6151
minto <minto@hotmail.fr>
2018-10-10 08:38:44 +0200

    refactoring worflow as graph of service p40

--- eNMS/services/forms.py
+++ eNMS/services/forms.py
@@ -13,3 +13,18 @@ class ServiceForm(FlaskForm):
 class CompareLogsForm(FlaskForm):
     first_version = SelectField(choices=())
     second_version = SelectField(choices=())
+
+
+class AddJobForm(FlaskForm):
+    job = SelectField()
+
+
+class WorkflowCreationForm(FlaskForm):
+    name = TextField()
+    description = TextField()
+    vendor = TextField()
+    operating_system = TextField()
+
+
+class WorkflowEditorForm(FlaskForm):
+    workflow = SelectField()
--- eNMS/workflows/forms.py
+++ eNMS/workflows/forms.py
@@ -2,16 +2,4 @@ from flask_wtf import FlaskForm
 from wtforms import SelectField, TextField
 
 
-class AddJobForm(FlaskForm):
-    job = SelectField()
 
-
-class WorkflowCreationForm(FlaskForm):
-    name = TextField()
-    description = TextField()
-    vendor = TextField()
-    operating_system = TextField()
-
-
-class WorkflowEditorForm(FlaskForm):
-    workflow = SelectField()

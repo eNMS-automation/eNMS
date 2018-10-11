1a7c7f812ef38288e6b430b8fd6cbc628cf11636
minto <minto@hotmail.fr>
2018-10-11 10:47:24 +0200

    refactoring worflow as graph of service p97

--- eNMS/schedule/static/scheduleTask.js
+++
@@ -1,6 +0,0 @@
-/*
-global
-alertify: false
-*/
-
-
--- eNMS/schedule/static/taskManagement.js
+++ eNMS/schedule/static/taskManagement.js
@@ -39,27 +39,40 @@ function addTask(mode, properties) {
 }
 
 /**
+ * Open pool modal for creation.
+ */
+function showTaskModal() { // eslint-disable-line no-unused-vars
+
+}
+
+/**
  * Show the task modal for task.
  * @param {id} id - Task id.
  */
 function showTaskModal(id) { // eslint-disable-line no-unused-vars
-  $.ajax({
-    type: 'POST',
-    url: `/schedule/get/${id}`,
-    dataType: 'json',
-    success: function(properties) {
-      console.log(properties);
-      if (!properties) {
-        alertify.notify('HTTP Error 403 – Forbidden', 'error', 5);
-      } else {
-        for (const [property, value] of Object.entries(properties)) {
-          $(`#${property}`).val(value);
+  if (!id) {
+    $('#title').text('Create a new task');
+    $('#task-modal-form').trigger('reset');
+    $('#task-modal').modal('show');
+  } else {
+    $.ajax({
+      type: 'POST',
+      url: `/schedule/get/${id}`,
+      dataType: 'json',
+      success: function(properties) {
+        console.log(properties);
+        if (!properties) {
+          alertify.notify('HTTP Error 403 – Forbidden', 'error', 5);
+        } else {
+          for (const [property, value] of Object.entries(properties)) {
+            $(`#${property}`).val(value);
+          }
+          $('#job').val(properties.job.id);
         }
-        $('#job').val(properties.job.id);
-      }
-      $('#title').text(`Edit task '${properties.name}'`);
-    },
-  });
+        $('#title').text(`Edit task '${properties.name}'`);
+      },
+    });
+  }
   $('#task-modal').modal('show');
 }
 

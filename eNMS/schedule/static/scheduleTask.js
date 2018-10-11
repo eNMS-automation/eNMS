e2b6041c7295d7e1f09231b2a76d2fb0c77030cb
minto <minto@hotmail.fr>
2018-10-11 10:37:47 +0200

    refactoring worflow as graph of service p95

--- eNMS/schedule/static/scheduleTask.js
+++ eNMS/schedule/static/scheduleTask.js
@@ -23,17 +23,19 @@ for (let i = 0; i < dates.length; i++) {
  * Schedule a task.
  */
 function scheduleTask() {
-  if ($('#scheduling-form').parsley().validate()) {
+  if ($('#task-modal-form').parsley().validate()) {
     $.ajax({
       type: 'POST',
       url: '/schedule/scheduler',
       dataType: 'json',
-      data: $('#scheduling-form').serialize(),
+      data: $('#task-modal-form').serialize(),
       success: function(result) {
+        console.log(taskManagement);
         if (!result) {
           alertify.notify('HTTP Error 403 – Forbidden', 'error', 5);
         } else {
-          $('#scheduling').modal('hide');
+          $('#task-modal').modal('hide');
+          addTask(mode, properties)
           alertify.notify(`Task '${result.name}' scheduled.`, 'success', 5);
         }
       },
--- eNMS/schedule/static/taskManagement.js
+++ eNMS/schedule/static/taskManagement.js
@@ -5,6 +5,7 @@ tasks: false
 */
 
 const table = $('#table').DataTable(); // eslint-disable-line new-cap
+const taskManagement = true;
 
 /**
  * Add a task to the datatable.
@@ -36,6 +37,94 @@ function addTask(mode, properties) {
     $(rowNode).attr('id', `${properties.id}`);
   }
 }
+
+/**
+ * Show the task modal for task.
+ * @param {id} id - Task id.
+ */
+function showTaskModal(id) { // eslint-disable-line no-unused-vars
+  $.ajax({
+    type: 'POST',
+    url: `/schedule/get/${id}`,
+    dataType: 'json',
+    success: function(properties) {
+      console.log(properties);
+      if (!properties) {
+        alertify.notify('HTTP Error 403 – Forbidden', 'error', 5);
+      } else {
+        for (const [property, value] of Object.entries(properties)) {
+          $(`#${property}`).val(value);
+        }
+        $('#job').val(properties.job.id);
+      }
+    },
+  });
+  $('#task-modal').modal('show');
+}
+
+/**
+ * Delete a task.
+ * @param {id} id - Task id.
+ */
+function deleteTask(id) { // eslint-disable-line no-unused-vars
+  $.ajax({
+    type: 'POST',
+    url: `/schedule/delete_task/${id}`,
+    success: function(result) {
+      if (!result) {
+        alertify.notify('HTTP Error 403 – Forbidden', 'error', 5);
+      } else {
+        table.row($(`#${id}`)).remove().draw(false);
+        alertify.notify('Task successfully deleted.', 'success', 5);
+      }
+    },
+  });
+}
+
+/**
+ * Pause a task.
+ * @param {id} id - Task id.
+ */
+function pauseTask(id) { // eslint-disable-line no-unused-vars
+  $.ajax({
+    type: 'POST',
+    url: `/schedule/pause_task/${id}`,
+    success: function(result) {
+      if (!result) {
+        alertify.notify('HTTP Error 403 – Forbidden', 'error', 5);
+      } else {
+        $(`#pause-resume-${id}`).attr(
+          'onclick',
+          `resumeTask('${id}')`
+        ).text('Resume');
+        alertify.notify('Task paused.', 'success', 5);
+      }
+    },
+  });
+}
+
+/**
+ * Resume a task.
+ * @param {id} id - Task id.
+ */
+function resumeTask(id) { // eslint-disable-line no-unused-vars
+  $.ajax({
+    type: 'POST',
+    url: `/schedule/resume_task/${id}`,
+    success: function(result) {
+      if (!result) {
+        alertify.notify('HTTP Error 403 – Forbidden', 'error', 5);
+      } else {
+        $(`#pause-resume-${id}`).attr(
+          'onclick',
+          `pauseTask('${id}')`
+        ).text('Pause');
+        alertify.notify('Task resumed.', 'success', 5);
+      }
+    },
+  });
+}
+
 
 (function() {
   if (typeof tasks !== 'undefined') {

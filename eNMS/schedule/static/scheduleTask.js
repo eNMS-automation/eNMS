71f34025189b25b14a7be8ade8be27d859b84d44
minto <minto@hotmail.fr>
2018-10-11 10:40:45 +0200

    refactoring worflow as graph of service p96

--- eNMS/schedule/static/scheduleTask.js
+++ eNMS/schedule/static/scheduleTask.js
@@ -3,44 +3,4 @@ global
 alertify: false
 */
 
-const dates = ['start_date', 'end_date'];
-const today = new Date();
-for (let i = 0; i < dates.length; i++) {
-  $('#' + dates[i]).datetimepicker({
-    format: 'DD/MM/YYYY HH:mm:ss',
-    widgetPositioning: {
-      horizontal: 'left',
-      vertical: 'bottom',
-    },
-    useCurrent: false,
-  });
-  if ($('#' + dates[i]).length) {
-    $('#' + dates[i]).data('DateTimePicker').minDate(today);
-  }
-}
 
-/**
- * Schedule a task.
- */
-function scheduleTask() {
-  if ($('#task-modal-form').parsley().validate()) {
-    $.ajax({
-      type: 'POST',
-      url: '/schedule/scheduler',
-      dataType: 'json',
-      data: $('#task-modal-form').serialize(),
-      success: function(result) {
-        console.log(taskManagement);
-        if (!result) {
-          alertify.notify('HTTP Error 403 – Forbidden', 'error', 5);
-        } else {
-          $('#task-modal').modal('hide');
-          addTask(mode, properties)
-          alertify.notify(`Task '${result.name}' scheduled.`, 'success', 5);
-        }
-      },
-    });
-  } else {
-    alertify.notify('Some fields are missing.', 'error', 5);
-  }
-}
--- eNMS/schedule/static/taskManagement.js
+++ eNMS/schedule/static/taskManagement.js
@@ -57,9 +57,36 @@ function showTaskModal(id) { // eslint-disable-line no-unused-vars
         }
         $('#job').val(properties.job.id);
       }
+      $('#title').text(`Edit task '${properties.name}'`);
     },
   });
   $('#task-modal').modal('show');
+}
+
+/**
+ * Schedule a task.
+ */
+function scheduleTask() {
+  if ($('#task-modal-form').parsley().validate()) {
+    $.ajax({
+      type: 'POST',
+      url: '/schedule/scheduler',
+      dataType: 'json',
+      data: $('#task-modal-form').serialize(),
+      success: function(result) {
+        console.log(taskManagement);
+        if (!result) {
+          alertify.notify('HTTP Error 403 – Forbidden', 'error', 5);
+        } else {
+          $('#task-modal').modal('hide');
+          addTask(mode, properties)
+          alertify.notify(`Task '${result.name}' scheduled.`, 'success', 5);
+        }
+      },
+    });
+  } else {
+    alertify.notify('Some fields are missing.', 'error', 5);
+  }
 }
 
 /**
@@ -125,11 +152,26 @@ function resumeTask(id) { // eslint-disable-line no-unused-vars
   });
 }
 
-
 (function() {
   if (typeof tasks !== 'undefined') {
     for (let i = 0; i < tasks.length; i++) {
       addTask('create', tasks[i]);
+    }
+  }
+
+  const dates = ['start_date', 'end_date'];
+  const today = new Date();
+  for (let i = 0; i < dates.length; i++) {
+    $('#' + dates[i]).datetimepicker({
+      format: 'DD/MM/YYYY HH:mm:ss',
+      widgetPositioning: {
+        horizontal: 'left',
+        vertical: 'bottom',
+      },
+      useCurrent: false,
+    });
+    if ($('#' + dates[i]).length) {
+      $('#' + dates[i]).data('DateTimePicker').minDate(today);
     }
   }
 })();

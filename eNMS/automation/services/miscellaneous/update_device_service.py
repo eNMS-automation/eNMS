4dd09deea2afaaf474546d25cf893e85b9f9c1fb
minto <minto@hotmail.fr>
2018-10-15 23:56:24 +0200

    renaming update inventory service

--- eNMS/automation/services/miscellaneous/update_device_service.py
+++ eNMS/automation/services/miscellaneous/update_device_service.py
@@ -4,15 +4,15 @@ from sqlalchemy.ext.mutable import MutableDict
 from eNMS.automation.models import Service, service_classes
 
 
-class UpdateDeviceService(Service):
+class UpdateInventoryService(Service):
 
-    __tablename__ = 'UpdateDeviceService'
+    __tablename__ = 'UpdateInventoryService'
 
     id = Column(Integer, ForeignKey('Service.id'), primary_key=True)
     update_dictionnary = Column(MutableDict.as_mutable(PickleType), default={})
 
     __mapper_args__ = {
-        'polymorphic_identity': 'update_device_service',
+        'polymorphic_identity': 'update_inventory_service',
     }
 
     def job(self, device, payload):
@@ -21,4 +21,4 @@ class UpdateDeviceService(Service):
         return {'success': True, 'result': 'properties updated'}
 
 
-service_classes['update_device_service'] = UpdateDeviceService
+service_classes['update_inventory_service'] = UpdateInventoryService

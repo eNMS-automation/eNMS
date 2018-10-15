58efea8243c03971bd60701e6038139a46d7cc6e
minto <minto@hotmail.fr>
2018-10-15 13:33:43 +0200

    logs blueprint p9

--- eNMS/base/routes.py
+++ eNMS/base/routes.py
@@ -36,37 +36,6 @@ def dashboard():
     )
 
 
-@blueprint.route('/log_management')
-@login_required
-def log_management():
-    log_filtering_form = LogFilteringForm(request.form)
-    return render_template(
-        'log_management.html',
-        log_filtering_form=log_filtering_form,
-        names=pretty_names,
-        fields=('source', 'content'),
-        logs=Log.serialize()
-    )
-
-
-@blueprint.route('/filter_logs', methods=['POST'])
-@login_required
-def filter_logs():
-    logs = [log for log in Log.serialize() if all(
-        # if the regex property is not in the request, the
-        # regex box is unticked and we only check that the values of the
-        # filters are contained in the values of the log
-        request.form[prop] in str(val) if not prop + 'regex' in request.form
-        # if it is ticked, we use re.search to check that the value
-        # of the device property matches the regular expression,
-        # providing that the property field in the form is not empty
-        # (empty field <==> property ignored)
-        else search(request.form[prop], str(val)) for prop, val in log.items()
-        if prop in request.form and request.form[prop]
-    )]
-    return jsonify(logs)
-
-
 @blueprint.route('/counters/<property>/<type>', methods=['POST'])
 @login_required
 def get_counters(property, type):
@@ -74,16 +43,6 @@ def get_counters(property, type):
     if property in reverse_pretty_names:
         property = reverse_pretty_names[property]
     return jsonify(Counter(map(lambda o: str(getattr(o, property)), objects)))
-
-
-@blueprint.route('/delete_log/<log_id>', methods=['POST'])
-@login_required
-@permission_required('Edit logs', redirect=False)
-def delete_log(log_id):
-    log = retrieve(Log, id=log_id)
-    db.session.delete(log)
-    db.session.commit()
-    return jsonify({'success': True})
 
 
 def shutdown_server():
--- eNMS/logs/routes.py
+++ eNMS/logs/routes.py
@@ -6,6 +6,48 @@ from eNMS.base.custom_base import factory
 from eNMS.base.helpers import permission_required, retrieve
 
 
+@blueprint.route('/log_management')
+@login_required
+def log_management():
+    log_filtering_form = LogFilteringForm(request.form)
+    return render_template(
+        'log_management.html',
+        log_filtering_form=log_filtering_form,
+        names=pretty_names,
+        fields=('source', 'content'),
+        logs=Log.serialize()
+    )
+
+
+@blueprint.route('/delete_log/<log_id>', methods=['POST'])
+@login_required
+@permission_required('Edit logs', redirect=False)
+def delete_log(log_id):
+    log = retrieve(Log, id=log_id)
+    db.session.delete(log)
+    db.session.commit()
+    return jsonify({'success': True})
+
+
+@blueprint.route('/filter_logs', methods=['POST'])
+@login_required
+def filter_logs():
+    logs = [log for log in Log.serialize() if all(
+        # if the regex property is not in the request, the
+        # regex box is unticked and we only check that the values of the
+        # filters are contained in the values of the log
+        request.form[prop] in str(val) if not prop + 'regex' in request.form
+        # if it is ticked, we use re.search to check that the value
+        # of the device property matches the regular expression,
+        # providing that the property field in the form is not empty
+        # (empty field <==> property ignored)
+        else search(request.form[prop], str(val)) for prop, val in log.items()
+        if prop in request.form and request.form[prop]
+    )]
+    return jsonify(logs)
+
+
+
 @blueprint.route('/syslog_automation')
 @login_required
 def syslog_automation():

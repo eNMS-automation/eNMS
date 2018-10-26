5ace53247700adb46f3eb6aa1e770df7e63edbbd
minto <minto@hotmail.fr>
2018-10-26 11:48:13 +0200

    form refactoring p1

--- eNMS/automation/forms.py
+++ eNMS/automation/forms.py
@@ -9,18 +9,20 @@ from wtforms import (
 )
 
 
-class ServiceForm(FlaskForm):
+class JobForm(FlaskForm)
     id = HiddenField()
     name = TextField()
     description = TextField()
     devices = SelectMultipleField(choices=())
     pools = SelectMultipleField(choices=())
-    waiting_time = IntegerField(default=0)
+    waiting_time = IntegerField('Waiting time (in seconds)', default=0)
     number_of_retry = IntegerField(default=1)
     time_between_retries = IntegerField(
         'Time between retries (in seconds)',
         default=10
     )
+    vendor = TextField()
+    operating_system = TextField()
 
 
 class CompareLogsForm(FlaskForm):
@@ -32,21 +34,8 @@ class AddJobForm(FlaskForm):
     job = SelectField()
 
 
-class WorkflowCreationForm(FlaskForm):
-    id = HiddenField()
-    name = TextField()
-    description = TextField()
+class WorkflowForm(JobForm):
     multiprocessing = BooleanField('ggg')
-    number_of_retry = IntegerField(default=1)
-    waiting_time = IntegerField('Waiting time (in seconds)', default=0)
-    time_between_retries = IntegerField(
-        'Time between retries (in seconds)',
-        default=10
-    )
-    vendor = TextField()
-    operating_system = TextField()
-    devices = SelectMultipleField(choices=())
-    pools = SelectMultipleField(choices=())
 
 
 class WorkflowBuilderForm(FlaskForm):

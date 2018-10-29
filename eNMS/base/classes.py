f7b55e71429838992d17578de7569ddcd7e6da25
minto <minto@hotmail.fr>
2018-10-29 14:10:09 +0100

    import export page refactoring p4

--- eNMS/base/classes.py
+++ eNMS/base/classes.py
@@ -1,16 +1,1 @@
-from flask import abort, jsonify
-from flask_login import current_user, login_required
-from functools import wraps
-from importlib.util import spec_from_file_location, module_from_spec
-from pathlib import Path
-from sqlalchemy import Boolean, Float, Integer, PickleType
-
-from eNMS import db
-from eNMS.base.properties import (
-    property_types,
-    boolean_properties,
-    service_import_properties,
-    service_properties
-)
-
 classes, service_classes = {}, {}

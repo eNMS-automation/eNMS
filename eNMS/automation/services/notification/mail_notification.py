7d00db2633c636a4e0be9ed0957e5052c2676e75
minto <minto@hotmail.fr>
2018-11-01 11:40:41 +0100

    mail notification service p3

--- eNMS/automation/services/notification/mail_notification.py
+++ eNMS/automation/services/notification/mail_notification.py
@@ -1,3 +1,4 @@
+from flask_mail import Message
 from netmiko import file_transfer
 from sqlalchemy import Boolean, Column, Float, ForeignKey, Integer, String
 
@@ -11,14 +12,26 @@ class MailNotificationService(Service):
     __tablename__ = 'MailNotificationService'
 
     id = Column(Integer, ForeignKey('Service.id'), primary_key=True)
+    title = Column(String)
+    sender = Column(String)
+    recipients = Column(String)
+    body = Column(String)
+    body_textarea = True
     multiprocessing = False
 
     __mapper_args__ = {
         'polymorphic_identity': 'mail_notification_service',
     }
 
-    def job(self, device, payload):
-        return {'success': True, 'result': 'result'}
+    def job(self, payload):
+        message = Message(
+            self.title,
+            sender=self.sender,
+            recipients=self.recipients.split(','),
+            body=self.body
+        )
+        mail.send(message)
+        return {'success': True, 'result': message}
 
 
 service_classes['mail_notification_service'] = MailNotificationService

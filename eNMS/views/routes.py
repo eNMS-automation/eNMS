c6db1a11e266fa2c68d59f89412b632e71714cd6
minto <minto@hotmail.fr>
2018-11-04 09:42:49 +0100

    refactor view p2

--- eNMS/views/routes.py
+++ eNMS/views/routes.py
@@ -25,15 +25,13 @@ from eNMS.views.forms import GoogleEarthForm, ViewOptionsForm
 @get(bp, '/<view_type>_view', 'Views Section', ['GET', 'POST'])
 def view(view_type):
     devices = fetch_all('Device')
-    view = request.form.get(
-        'view',
-        ('3D', ('2D', 'Cluster'))[len(devices) > 2000][len(devices) > 50]
-    )
     return render_template(
         f'{view_type}_view.html',
         pools=fetch_all('Pool'),
         parameters=get_one('Parameters').serialized,
-        view=view,
+        view=request.form.get(
+            'view',('3D', ('2D', '2DC'))[len(d) > 2000][len(d) > 50]
+        ),
         view_options_form=ViewOptionsForm(request.form),
         google_earth_form=GoogleEarthForm(request.form),
         add_device_form=AddDevice(request.form),

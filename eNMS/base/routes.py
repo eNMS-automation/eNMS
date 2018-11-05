b94c5c4ee16bf590e28d6c2ea6213a0a16032dd3
minto <minto@hotmail.fr>
2018-11-05 11:30:48 +0100

    refactor routes p2

--- eNMS/base/routes.py
+++ eNMS/base/routes.py
@@ -17,21 +17,6 @@ def site_root():
     return redirect(url_for('admin_blueprint.login'))
 
 
-@post(bp, '/update/<cls>', 'Edit Admin Section')
-def update_instance():
-    return jsonify(factory('User', **request.form).serialized)
-
-
-@post(bp, '/get/<cls>/<user_id>', 'Admin Section')
-def get_instance(user_id):
-    return jsonify(fetch('User', id=user_id).serialized)
-
-
-@post(bp, '/delete/<cls>/<user_id>', 'Edit Admin Section')
-def delete_instance(user_id):
-    return jsonify(delete('User', id=user_id))
-
-
 @get(bp, '/dashboard')
 def dashboard():
     return render_template(
@@ -49,6 +34,21 @@ def get_counters(property, type):
     if property in reverse_pretty_names:
         property = reverse_pretty_names[property]
     return jsonify(Counter(map(lambda o: str(getattr(o, property)), objects)))
+
+
+@post(bp, '/get/<cls>/<user_id>', 'View')
+def get_instance(user_id):
+    return jsonify(fetch('User', id=user_id).serialized)
+
+
+@post(bp, '/update/<cls>', 'Editing')
+def update_instance():
+    return jsonify(factory('User', **request.form).serialized)
+
+
+@post(bp, '/delete/<cls>/<user_id>', 'Editing')
+def delete_instance(user_id):
+    return jsonify(delete('User', id=user_id))
 
 
 @post(bp, '/shutdown', 'Admin')

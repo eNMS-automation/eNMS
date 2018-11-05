7dbe33f4d5b9abcc3be376fc3eb6569d190e5cce
minto <minto@hotmail.fr>
2018-11-05 22:47:02 +0100

    refactor js p9

--- eNMS/logs/static/logAutomation.js
+++ eNMS/logs/static/logAutomation.js
@@ -9,26 +9,6 @@ logRules: false
 
 const table = $('#table').DataTable(); // eslint-disable-line new-cap
 
-/**
- * Add log rule to datatable or edit line.
- * @param {properties} properties - Properties of the log rule.
- * @param {mode} mode - Create or edit.
- */
-function addLogRule(properties, mode) {
-  let values = [];
-  for (let i = 0; i < fields.length; i++) {
-    values.push(`${properties[fields[i]]}`);
-  }
-
-
-  if (mode == 'edited') {
-    table.row($(`#${properties.id}`)).data(values);
-  } else {
-    const rowNode = table.row.add(values).draw(false).node();
-    $(rowNode).attr('id', `${properties.id}`);
-  }
-}
-
 function tableActions(values, logrule) {
   values.push(
     `<button type="button" class="btn btn-info btn-xs"
@@ -40,28 +20,6 @@ function tableActions(values, logrule) {
 
 (function() {
   for (let i = 0; i < logRules.length; i++) {
-    addLogRule(logRules[i], 'create');
+    addInstance('create', 'logrule', logRules[i]);
   }
 })();
-
-
-/**
- * Display log rule modal for editing.
- * @param {id} id - Id of the log rule.
- */
-function showLogRuleModal(id) { // eslint-disable-line no-unused-vars
-  call(`/get/logrule/${id}`, function(logRule) {
-    for (const [property, value] of Object.entries(logRule)) {
-      if (property.includes('regex')) {
-        $(`#${property}`).prop('checked', value);
-      } else {
-        $(`#${property}`).val(value);
-      }
-      $('#jobs').val(logRule.jobs.map((j) => j.id));
-      $('#title').text(`Edit log rule properties`);
-    }
-    $('#edit').modal('show');
-  });
-}
-
-

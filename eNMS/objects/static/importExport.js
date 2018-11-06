e03769e26783802c9ed9c18756dd8fdda8dd204b
minto <minto@hotmail.fr>
2018-11-06 11:08:30 +0100

    refactor migrations p6

--- eNMS/objects/static/importExport.js
+++ eNMS/objects/static/importExport.js
@@ -62,25 +62,6 @@ function importTopology() { // eslint-disable-line no-unused-vars
   }
 }
 
-/**
- * Export all for migration.
- */
-function migrationExport() { // eslint-disable-line no-unused-vars
-  alertify.notify('Export initiated.', 'success', 5);
-  fCall('/objects/migration_export', '#import-export-form', function() {
-    alertify.notify('Export successful.', 'success', 5);
-  });
-}
-
-/**
- * Export all for migration.
- */
-function migrationImport() { // eslint-disable-line no-unused-vars
-  fCall('/objects/migration_import', '#import-export-form', function() {
-    alertify.notify('Import successful.', 'success', 5);
-  });
-}
-
 (function() {
   doc('https://enms.readthedocs.io/en/latest/inventory/objects.html');
 })();

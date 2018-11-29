baff9727a758176a5d8f56a33b54760568f6cba9
minto <minto@hotmail.fr>
2018-11-29 13:17:54 +0100

    import merge mechanism p2

--- eNMS/admin/forms.py
+++ eNMS/admin/forms.py
@@ -95,11 +95,11 @@ class MigrationsForm(FlaskForm):
     empty_database_before_import = BooleanField()
     export_choices = [(p, p) for p in import_properties]
     import_export_types = SelectMultipleField(choices=export_choices)
-    conflict_choices = (
-        ('ignore', 'Ignore imported object'),
-        ('create', 'Create with a new ID')
+    merge_choices = (
+        ('update', 'Update existing objects'),
+        ('create', 'Create new objects')
     )
-    conflict = SelectField(choices=conflict_choices)
+    merge_mechanism = SelectField(choices=merge_choices)
 
 
 class LogsForm(FlaskForm):
--- eNMS/admin/helpers.py
+++ eNMS/admin/helpers.py
@@ -20,19 +20,15 @@ def migrate_import(path_app, request):
     status = 'Import successful.'
     if request.get('empty_database_before_import', False):
         delete_all(*request['import_export_types'])
-    conflict = request.get('conflict', 'ignore')
+    merge_mechanism = request.get('merge_mechanism', 'update')
     for cls in request['import_export_types']:
         path = path_app / 'migrations' / request['name'] / f'{cls}.yaml'
         with open(path, 'r') as migration_file:
             for obj in load(migration_file):
                 obj_cls = obj.pop('type') if cls == 'Service' else cls
                 try:
-                    instance = fetch(obj_cls, id=obj['id'])
-                    if instance:
-                        if conflict == 'ignore':
-                            continue
-                        else:
-                            obj.pop('id')
+                    if merge_mechanism == 'create':
+                        obj.pop('id')
                     factory(obj_cls, **obj)
                 except Exception as e:
                     info(f'{str(obj)} could not be imported ({str(e)})')

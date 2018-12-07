6a9f0618ac1b260e41a9dc2cccd5774eb1faa37f
minto <minto@hotmail.fr>
2018-12-07 21:57:25 +0100

    remove selenium stuff

--- tests/conftest.py
+++ tests/conftest.py
@@ -31,38 +31,3 @@ def user_client():
     with app.app_context():
         client.post('/admin/login', data=login)
         yield client
-
-
-@fixture
-def selenium_client():
-    app = create_app(Path.cwd(), config_dict['Debug'])
-    app_context = app.app_context()
-    app_context.push()
-    db.session.close()
-    db.drop_all()
-    options = Options()
-    options.add_argument('--headless')
-    client = None
-    try:
-        client = webdriver.Chrome(
-            './tests/chromedriver',
-            chrome_options=options
-        )
-    except Exception:
-        pass
-    if client:
-        t = Thread(
-            target=app.run,
-            kwargs={
-                'host': '0.0.0.0',
-                'port': 5000,
-                'use_reloader': False
-            }
-        )
-        t.daemon = True
-        t.start()
-        sleep(1)
-        yield client
-        client.get('http://0.0.0.0:5000/shutdown')
-        client.quit()
-    app_context.pop()

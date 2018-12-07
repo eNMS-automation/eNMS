873da12a5277fe88980931b03d8778b629a10088
minto <minto@hotmail.fr>
2018-12-07 21:57:37 +0100

    remove selenium stuff p2

--- tests/test_selenium.py
+++
@@ -1,9 +0,0 @@
-from tests.test_base import urls
-
-
-def selenium_test(selenium_client):
-    for blueprint, pages in urls.items():
-        for page in pages:
-            selenium_client.get('http://127.0.0.1:5000' + blueprint + page)
-            for entry in selenium_client.get_log('browser'):
-                assert entry['level'] != 'SEVERE'

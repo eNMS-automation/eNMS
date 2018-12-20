fbdd7f2dafc271d8bf38ebfffac7424630c2d7da
minto <minto@hotmail.fr>
2018-12-20 22:06:17 +0100

    tests for workflows p6

--- tests/test_workflows.py
+++ tests/test_workflows.py
@@ -4,68 +4,68 @@ from requests.auth import HTTPBasicAuth
 from tests.test_base import check_blueprints
 
 
-# @check_blueprints('/automation')
-# def test_payload_transfer_workflow(user_client):
-#     result = post(
-#         'http://192.168.105.2:5000/rest/run_job',
-#         json={'name': 'payload_transfer_workflow', 'async': 0},
-#         auth=HTTPBasicAuth('admin', 'admin')
-#     ).json()
-#     assert result['success'] and len(result) == 3
-#     post(
-#         'http://192.168.105.2:5000/rest/object/Workflow',
-#         json={'name': 'payload_transfer_workflow', 'multiprocessing': True},
-#         auth=HTTPBasicAuth('admin', 'admin')
-#     )
-#     result = post(
-#         'http://192.168.105.2:5000/rest/run_job',
-#         json={'name': 'payload_transfer_workflow', 'async': 0},
-#         auth=HTTPBasicAuth('admin', 'admin')
-#     ).json()
-#     assert result['success'] and len(result) == 3
-#     post(
-#         'http://192.168.105.2:5000/rest/object/Workflow',
-#         json={'name': 'payload_transfer_workflow', 'devices': []},
-#         auth=HTTPBasicAuth('admin', 'admin')
-#     )
-#     result = post(
-#         'http://192.168.105.2:5000/rest/run_job',
-#         json={'name': 'payload_transfer_workflow', 'async': 0},
-#         auth=HTTPBasicAuth('admin', 'admin')
-#     ).json()
-#     assert result['success'] and len(result) == 10
+@check_blueprints('/automation')
+def test_payload_transfer_workflow(user_client):
+    result = post(
+        'http://192.168.105.2:5000/rest/run_job',
+        json={'name': 'payload_transfer_workflow', 'async': 0},
+        auth=HTTPBasicAuth('admin', 'admin')
+    ).json()
+    assert result['success'] and len(result) == 3
+    post(
+        'http://192.168.105.2:5000/rest/object/Workflow',
+        json={'name': 'payload_transfer_workflow', 'multiprocessing': True},
+        auth=HTTPBasicAuth('admin', 'admin')
+    )
+    result = post(
+        'http://192.168.105.2:5000/rest/run_job',
+        json={'name': 'payload_transfer_workflow', 'async': 0},
+        auth=HTTPBasicAuth('admin', 'admin')
+    ).json()
+    assert result['success'] and len(result) == 3
+    post(
+        'http://192.168.105.2:5000/rest/object/Workflow',
+        json={'name': 'payload_transfer_workflow', 'devices': []},
+        auth=HTTPBasicAuth('admin', 'admin')
+    )
+    result = post(
+        'http://192.168.105.2:5000/rest/run_job',
+        json={'name': 'payload_transfer_workflow', 'async': 0},
+        auth=HTTPBasicAuth('admin', 'admin')
+    ).json()
+    assert result['success'] and len(result) == 10
 
 
-# @check_blueprints('/automation')
-# def test_netmiko_workflow(user_client):
-#     result = post(
-#         'http://192.168.105.2:5000/rest/run_job',
-#         json={'name': 'Netmiko_VRF_workflow', 'async': 0},
-#         auth=HTTPBasicAuth('admin', 'admin')
-#     ).json()
-#     assert result['success'] and len(result) == 3
-#     post(
-#         'http://192.168.105.2:5000/rest/object/Workflow',
-#         json={'name': 'Netmiko_VRF_workflow', 'multiprocessing': True},
-#         auth=HTTPBasicAuth('admin', 'admin')
-#     )
-#     result = post(
-#         'http://192.168.105.2:5000/rest/run_job',
-#         json={'name': 'Netmiko_VRF_workflow', 'async': 0},
-#         auth=HTTPBasicAuth('admin', 'admin')
-#     ).json()
-#     assert result['success'] and len(result) == 3
-#     post(
-#         'http://192.168.105.2:5000/rest/object/Workflow',
-#         json={'name': 'Netmiko_VRF_workflow', 'devices': []},
-#         auth=HTTPBasicAuth('admin', 'admin')
-#     )
-#     result = post(
-#         'http://192.168.105.2:5000/rest/run_job',
-#         json={'name': 'Netmiko_VRF_workflow', 'async': 0},
-#         auth=HTTPBasicAuth('admin', 'admin')
-#     ).json()
-#     assert result['success'] and len(result) == 8
+@check_blueprints('/automation')
+def test_netmiko_workflow(user_client):
+    result = post(
+        'http://192.168.105.2:5000/rest/run_job',
+        json={'name': 'Netmiko_VRF_workflow', 'async': 0},
+        auth=HTTPBasicAuth('admin', 'admin')
+    ).json()
+    assert result['success'] and len(result) == 3
+    post(
+        'http://192.168.105.2:5000/rest/object/Workflow',
+        json={'name': 'Netmiko_VRF_workflow', 'multiprocessing': True},
+        auth=HTTPBasicAuth('admin', 'admin')
+    )
+    result = post(
+        'http://192.168.105.2:5000/rest/run_job',
+        json={'name': 'Netmiko_VRF_workflow', 'async': 0},
+        auth=HTTPBasicAuth('admin', 'admin')
+    ).json()
+    assert result['success'] and len(result) == 3
+    post(
+        'http://192.168.105.2:5000/rest/object/Workflow',
+        json={'name': 'Netmiko_VRF_workflow', 'devices': []},
+        auth=HTTPBasicAuth('admin', 'admin')
+    )
+    result = post(
+        'http://192.168.105.2:5000/rest/run_job',
+        json={'name': 'Netmiko_VRF_workflow', 'async': 0},
+        auth=HTTPBasicAuth('admin', 'admin')
+    ).json()
+    assert result['success'] and len(result) == 8
 
 
 @check_blueprints('/automation')

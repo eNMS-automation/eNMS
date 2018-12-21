b239d626c73c620230d29cbcd10b534a0ca24a1c
minto <minto@hotmail.fr>
2018-12-21 09:11:14 +0100

    rest api update p1

--- tests/test_virtual_lab.py
+++ tests/test_virtual_lab.py
@@ -1,7 +1,17 @@
-from requests import post
+from requests import get, post
 from requests.auth import HTTPBasicAuth
 
 from tests.test_base import check_blueprints
+
+
+@check_blueprints('/inventory')
+def test_rest_api_basic(user_client):
+    result = post(
+        'http://192.168.105.2:5000/rest/instance/device/Washington',
+        json={'name': 'payload_transfer_workflow', 'async': 0},
+        auth=HTTPBasicAuth('admin', 'admin')
+    ).json()
+    print(result)
 
 
 @check_blueprints('/automation')
@@ -13,7 +23,7 @@ def test_payload_transfer_workflow(user_client):
     ).json()
     assert result['success'] and len(result) == 3
     post(
-        'http://192.168.105.2:5000/rest/object/Workflow',
+        'http://192.168.105.2:5000/rest/instance/Workflow',
         json={'name': 'payload_transfer_workflow', 'multiprocessing': True},
         auth=HTTPBasicAuth('admin', 'admin')
     )
@@ -24,7 +34,7 @@ def test_payload_transfer_workflow(user_client):
     ).json()
     assert result['success'] and len(result) == 3
     post(
-        'http://192.168.105.2:5000/rest/object/Workflow',
+        'http://192.168.105.2:5000/rest/instance/Workflow',
         json={'name': 'payload_transfer_workflow', 'devices': []},
         auth=HTTPBasicAuth('admin', 'admin')
     )
@@ -45,7 +55,7 @@ def test_netmiko_workflow(user_client):
     ).json()
     assert result['success'] and len(result) == 3
     post(
-        'http://192.168.105.2:5000/rest/object/Workflow',
+        'http://192.168.105.2:5000/rest/instance/Workflow',
         json={'name': 'Netmiko_VRF_workflow', 'multiprocessing': True},
         auth=HTTPBasicAuth('admin', 'admin')
     )
@@ -56,7 +66,7 @@ def test_netmiko_workflow(user_client):
     ).json()
     assert result['success'] and len(result) == 3
     post(
-        'http://192.168.105.2:5000/rest/object/Workflow',
+        'http://192.168.105.2:5000/rest/instance/Workflow',
         json={'name': 'Netmiko_VRF_workflow', 'devices': []},
         auth=HTTPBasicAuth('admin', 'admin')
     )
@@ -77,7 +87,7 @@ def test_napalm_workflow(user_client):
     ).json()
     assert result['success'] and len(result) == 3
     post(
-        'http://192.168.105.2:5000/rest/object/Workflow',
+        'http://192.168.105.2:5000/rest/instance/Workflow',
         json={'name': 'Napalm_VRF_workflow', 'multiprocessing': True},
         auth=HTTPBasicAuth('admin', 'admin')
     )
@@ -88,7 +98,7 @@ def test_napalm_workflow(user_client):
     ).json()
     assert result['success'] and len(result) == 3
     post(
-        'http://192.168.105.2:5000/rest/object/Workflow',
+        'http://192.168.105.2:5000/rest/instance/Workflow',
         json={'name': 'Napalm_VRF_workflow', 'devices': []},
         auth=HTTPBasicAuth('admin', 'admin')
     )

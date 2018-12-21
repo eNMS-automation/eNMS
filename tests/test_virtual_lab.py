b25938f4349086639acbec3206d91cb5e73922eb
minto <minto@hotmail.fr>
2018-12-21 09:32:21 +0100

    add new test for rest api p3

--- tests/test_virtual_lab.py
+++ tests/test_virtual_lab.py
@@ -8,50 +8,58 @@ from tests.test_base import check_blueprints
 
 @check_blueprints('/inventory')
 def test_rest_api_basic(user_client):
-    assert len(fetch_all('Device')) == 28
-    post(
-        'http://192.168.105.2:5000/rest/instance/device',
-        json={'name': 'new_router', 'model': 'Cisco'},
-        auth=HTTPBasicAuth('admin', 'admin')
-    )
-    assert len(fetch_all('Device')) == 29
-    result = get(
-        'http://192.168.105.2:5000/rest/instance/device/Washington',
-        auth=HTTPBasicAuth('admin', 'admin')
-    ).json()
-    assert result['model'] == 'Arista' and len(result) == 14
-    post(
-        'http://192.168.105.2:5000/rest/instance/device',
-        json={'name': 'Washington', 'model': 'Cisco'},
-        auth=HTTPBasicAuth('admin', 'admin')
-    )
-    result = get(
-        'http://192.168.105.2:5000/rest/instance/device/Washington',
-        auth=HTTPBasicAuth('admin', 'admin')
-    ).json()
-    assert result['model'] == 'Cisco' and len(result) == 14
-    result = get(
-        'http://192.168.105.2:5000/rest/instance/service/get_facts',
-        auth=HTTPBasicAuth('admin', 'admin')
-    ).json()
-    assert result['description'] == 'Getter: get_facts' and len(result) == 24
-    put(
+    # assert len(fetch_all('Device')) == 28
+    # post(
+    #     'http://192.168.105.2:5000/rest/instance/device',
+    #     json={'name': 'new_router', 'model': 'Cisco'},
+    #     auth=HTTPBasicAuth('admin', 'admin')
+    # )
+    # assert len(fetch_all('Device')) == 29
+    # result = get(
+    #     'http://192.168.105.2:5000/rest/instance/device/Washington',
+    #     auth=HTTPBasicAuth('admin', 'admin')
+    # ).json()
+    # assert result['model'] == 'Arista' and len(result) == 14
+    # post(
+    #     'http://192.168.105.2:5000/rest/instance/device',
+    #     json={'name': 'Washington', 'model': 'Cisco'},
+    #     auth=HTTPBasicAuth('admin', 'admin')
+    # )
+    # result = get(
+    #     'http://192.168.105.2:5000/rest/instance/device/Washington',
+    #     auth=HTTPBasicAuth('admin', 'admin')
+    # ).json()
+    # assert result['model'] == 'Cisco' and len(result) == 14
+    # result = get(
+    #     'http://192.168.105.2:5000/rest/instance/service/get_facts',
+    #     auth=HTTPBasicAuth('admin', 'admin')
+    # ).json()
+    # assert result['description'] == 'Getter: get_facts' and len(result) == 24
+    # put(
+    #     'http://192.168.105.2:5000/rest/instance/service',
+    #     json={'name': 'get_facts', 'description': 'Get facts'},
+    #     auth=HTTPBasicAuth('admin', 'admin')
+    # )
+    # result = get(
+    #     'http://192.168.105.2:5000/rest/instance/service/get_facts',
+    #     auth=HTTPBasicAuth('admin', 'admin')
+    # ).json()
+    # assert result['description'] == 'Get facts' and len(result) == 24
+    assert len(fetch_all('Service')) == 21
+    result = post(
         'http://192.168.105.2:5000/rest/instance/service',
-        json={'name': 'get_facts', 'description': 'Get facts'},
-        auth=HTTPBasicAuth('admin', 'admin')
-    )
-    result = get(
-        'http://192.168.105.2:5000/rest/instance/service/get_facts',
+        json={'name': 'new_service', 'vendor': 'Cisco'},
         auth=HTTPBasicAuth('admin', 'admin')
     ).json()
-    assert result['description'] == 'Get facts' and len(result) == 24
-    assert len(fetch_all('Service')) == 28
+    assert result['vendor'] = 'Cisco'
+    assert len(fetch_all('Service')) == 22
+    assert len(fetch_all('Service')) == 4
     post(
         'http://192.168.105.2:5000/rest/instance/service',
         json={'name': 'new_service', 'model': 'Cisco'},
         auth=HTTPBasicAuth('admin', 'admin')
     )
-    assert len(fetch_all('Service')) == 29
+    assert len(fetch_all('Service')) == 5
 
 
 # @check_blueprints('/automation')

421d813d8c94f3c722571857a63df4cd1d70b5f7
minto <minto@hotmail.fr>
2018-12-21 09:29:43 +0100

    add new test for rest api

--- tests/test_virtual_lab.py
+++ tests/test_virtual_lab.py
@@ -1,18 +1,55 @@
-from requests import get, post
+from requests import get, post, put
 from requests.auth import HTTPBasicAuth
+
+from eNMS.base.helpers import fetch_all
 
 from tests.test_base import check_blueprints
 
 
+
+
+
 @check_blueprints('/inventory')
 def test_rest_api_basic(user_client):
+    assert len(fetch_all('Device')) == 44
+    post(
+        'http://192.168.105.2:5000/rest/instance/device',
+        json={'name': 'new_router', 'model': 'Cisco'},
+        auth=HTTPBasicAuth('admin', 'admin')
+    )
     result = get(
         'http://192.168.105.2:5000/rest/instance/device/Washington',
         auth=HTTPBasicAuth('admin', 'admin')
     ).json()
-    print(result)
+    assert result['model'] == 'Arista' and len(result) == 14
+    post(
+        'http://192.168.105.2:5000/rest/instance/device',
+        json={'name': 'Washington', 'model': 'Cisco'},
+        auth=HTTPBasicAuth('admin', 'admin')
+    )
+    result = get(
+        'http://192.168.105.2:5000/rest/instance/device/Washington',
+        auth=HTTPBasicAuth('admin', 'admin')
+    ).json()
+    assert result['model'] == 'Cisco' and len(result) == 14
+    result = get(
+        'http://192.168.105.2:5000/rest/instance/service/get_facts',
+        auth=HTTPBasicAuth('admin', 'admin')
+    ).json()
+    assert result['description'] == 'Getter: get_facts' and len(result) == 24
+    put(
+        'http://192.168.105.2:5000/rest/instance/service',
+        json={'name': 'get_facts', 'description': 'Get facts'},
+        auth=HTTPBasicAuth('admin', 'admin')
+    )
+    result = get(
+        'http://192.168.105.2:5000/rest/instance/service/get_facts',
+        auth=HTTPBasicAuth('admin', 'admin')
+    ).json()
+    assert result['description'] == 'Get facts' and len(result) == 24
+# {'id': 4, 'name': 'Washington', 'description': 'Washington', 'subtype': 'router', 'model': 'Arista', 'location': 'Washington', 'vendor': 'Arista', 'operating_system': 'eos', 'os_version': '15.5(3)M', 'ip_address': '192.168.105.5', 'longitude': -77.03637, 'latitude': 38.89511, 'port': 22, 'username': 'admin'}
 
-# 
+
 # @check_blueprints('/automation')
 # def test_payload_transfer_workflow(user_client):
 #     result = post(

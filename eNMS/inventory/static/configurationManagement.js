f9e7661a695c5b0e1bfe15bf1f7e04239b2213d4
minto <minto@hotmail.fr>
2018-12-22 11:28:53 +0100

    configuration management p3

---
+++ eNMS/inventory/static/configurationManagement.js
@@ -0,0 +1,31 @@
+/*
+global
+addInstance: false
+devices: false
+doc: false
+*/
+
+const table = $('#table').DataTable(); // eslint-disable-line
+
+/**
+ * Table Actions.
+ * @param {values} values - values array.
+ * @param {device} device - Device properties.
+ */
+function tableActions(values, device) { // eslint-disable-line no-unused-vars
+  values.push(
+    `<button type="button" class="btn btn-success btn-xs"
+    onclick="connectionParametersModal('${device.id}')">Connect</button>`,
+    `<button type="button" class="btn btn-primary btn-xs"
+    onclick="showTypeModal('device', '${device.id}')">Edit</button>`,
+    `<button type="button" class="btn btn-danger btn-xs"
+    onclick="confirmDeletion('device', '${device.id}')">Delete</button>`
+  );
+}
+
+(function() {
+  doc('https://enms.readthedocs.io/en/latest/inventory/objects.html');
+  for (let i = 0; i < devices.length; i++) {
+    addInstance('create', 'device', devices[i]);
+  }
+})();

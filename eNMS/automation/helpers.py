aa2e132d4f207ffa388634d19278e9511a9611f6
minto <minto@hotmail.fr>
2018-12-24 06:34:11 +0100

    refactor automation p4

--- eNMS/automation/helpers.py
+++ eNMS/automation/helpers.py
@@ -1,6 +1,4 @@
-from napalm import get_network_driver
 from napalm._SUPPORTED_DRIVERS import SUPPORTED_DRIVERS
-from netmiko import ConnectHandler
 from netmiko.ssh_dispatcher import CLASS_MAPPER, FILE_TRANSFER_MAP
 from re import compile
 
@@ -10,9 +8,6 @@ from eNMS.base.helpers import fetch
 NETMIKO_DRIVERS = sorted((driver, driver) for driver in CLASS_MAPPER)
 NETMIKO_SCP_DRIVERS = sorted((driver, driver) for driver in FILE_TRANSFER_MAP)
 NAPALM_DRIVERS = sorted((driver, driver) for driver in SUPPORTED_DRIVERS[1:])
-
-
-
 
 
 def substitute(data, variables):

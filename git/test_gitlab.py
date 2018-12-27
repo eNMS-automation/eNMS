567eb80e63fca5fbe0b360884cc0e5570fe766be
minto <minto@hotmail.fr>
2018-12-27 14:34:39 +0100

    update

---
+++ git/test_gitlab.py
@@ -0,0 +1,11 @@
+from git import Repo
+from git.exc import GitCommandError
+from pathlib import Path
+
+repo = Repo(str(Path.cwd() / 'configurations'))
+try:
+    repo.git.add(A=True)
+    repo.git.commit(m='commit all')
+except GitCommandError:
+    pass
+repo.remotes.origin.push()

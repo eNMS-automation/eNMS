f6dceb091b4248eea5eb76a8251ff3f66a448c56
minto <minto@hotmail.fr>
2019-01-25 14:12:19 +0100

    remove git env variables

--- config.py
+++ config.py
@@ -16,10 +16,6 @@ class Config(object):
     # Vault
     USE_VAULT = int(environ.get('USE_VAULT', False))
 
-    # Gitlab
-    GIT_USERNAME = environ.get('GIT_USERNAME')
-    GIT_PASSWORD = environ.get('GIT_PASSWORD')
-
     # LDAP
     LDAP_SERVER = environ.get('LDAP_SERVER')
     LDAP_USERDN = environ.get('LDAP_USERDN')

e57c07d66afa9ff00d2cb770f0187cacb85262d9
minto <minto@hotmail.fr>
2019-01-27 13:38:14 +0100

    per column search p7

--- eNMS/inventory/static/deviceManagement.js
+++ eNMS/inventory/static/deviceManagement.js
@@ -6,10 +6,12 @@ devices: false
 doc: false
 */
 
+const toExclude = ['Edit', 'Duplicate', 'Delete', Automation', 'Connect'];
+
 $('#table thead tr').clone(true).appendTo('#table thead');
 $('#table thead tr:eq(1) th').each(function(i) {
   var title = $(this).text();
-  if (!['Edit', 'Duplicate', 'Delete'].includes(title)) {
+  if (!.includes(title)) {
     $(this).html('<input type="text" style="width: 100%;"/>');
     $('input', this).on('keyup change', function() {
       if (table.column(i).search() !== this.value) {
@@ -22,7 +24,7 @@ $('#table thead tr:eq(1) th').each(function(i) {
 const table = $('#table').DataTable({
   'processing': true,
   'serverSide': true,
-  'ajax': '/server_side_processing'
+  'ajax': '/server_side_processing',
 }); // eslint-disable-line
 
     // Apply the search

1994302af9db0de02bccfc2023c03bbd24bf0f20
minto <minto@hotmail.fr>
2019-01-27 11:19:40 +0100

    fix project auto generator with ip address uniqueness

--- tests/scripts/generate_topology.py
+++ tests/scripts/generate_topology.py
@@ -11,7 +11,12 @@ style1 = easyxf(num_format_str='#,##0.00')
 
 ws = wb.add_sheet('Device')
 
-for index, header in enumerate(('name', 'longitude', 'latitude', 'subtype')):
+for index, header in enumerate((
+    'name',
+    'longitude',
+    'latitude',
+    'subtype'
+)):
     ws.write(0, index, header)
 
 for i in range(1, 1000):
@@ -19,5 +24,6 @@ for i in range(1, 1000):
     ws.write(i, 1, uniform(-40., 40.))
     ws.write(i, 2, uniform(-40., 40.))
     ws.write(i, 3, 'router')
+    ws.write(i, 4, 'router')
 
 wb.save(Path.cwd() / 'test.xls')

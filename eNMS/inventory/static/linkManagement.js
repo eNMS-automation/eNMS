2716123ebdc2f14a1906d7ad68d388f4d33dbabe
minto <minto@hotmail.fr>
2019-01-27 12:20:53 +0100

    per column search p3

--- eNMS/inventory/static/linkManagement.js
+++ eNMS/inventory/static/linkManagement.js
@@ -9,15 +9,14 @@ $('#table thead tr').clone(true).appendTo( '#table thead' );
 $('#table thead tr:eq(1) th').each( function (i) {
   var title = $(this).text();
   $(this).html( '<input type="text" placeholder="Search '+title+'" />' );
-  
-        $( 'input', this ).on( 'keyup change', function () {
-            if ( table.column(i).search() !== this.value ) {
-                table
-                    .column(i)
-                    .search( this.value )
-                    .draw();
-            }
-        } );
+  $('input', this).on('keyup change', function () {
+    if (table.column(i).search() !== this.value ) {
+        table
+            .column(i)
+            .search( this.value )
+            .draw();
+    }
+  });
 });
 
 const table = $('#table').DataTable(); // eslint-disable-line

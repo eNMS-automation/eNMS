554f766024bbeb4e571fe778f461ad7bdfe38741
minto <minto@hotmail.fr>
2019-01-27 15:11:31 +0100

    server side processing p9

--- eNMS/inventory/static/linkManagement.js
+++ eNMS/inventory/static/linkManagement.js
@@ -20,26 +20,11 @@ $('#table thead tr:eq(1) th').each(function(i) {
 
 const table = $('#table').DataTable({
   ordering: false,
-  orderCellsTop: true,
-  fixedHeader: true
+  fixedHeader: true,
+  processing: true,
+  serverSide: true,
+  ajax: '/server_side_processing',
 }); // eslint-disable-line
-
-/**
- * Table Actions.
- * @param {values} values - values array.
- * @param {link} link - Link properties.
- */
-function tableActions(values, link) { // eslint-disable-line no-unused-vars
-  values.push(
-    `<button type="button" class="btn btn-primary btn-xs"
-    onclick="showTypeModal('link', '${link.id}')">Edit</button>`,
-    `<button type="button" class="btn btn-primary btn-xs"
-    onclick="showTypeModal('link', '${link.id}', true)">
-    Duplicate</button>`,
-    `<button type="button" class="btn btn-danger btn-xs"
-    onclick="confirmDeletion('link', '${link.id}')">Delete</button>`
-  );
-}
 
 (function() {
   doc('https://enms.readthedocs.io/en/latest/inventory/objects.html');

6efe343e79490bbc43b766af0334982aa3d9a86e
minto <minto@hotmail.fr>
2019-01-28 12:13:11 +0100

    server side processing p44

--- eNMS/scheduling/static/taskManagement.js
+++ eNMS/scheduling/static/taskManagement.js
@@ -11,38 +11,6 @@ const table = $('#table').DataTable(); // eslint-disable-line new-cap
 const taskManagement = true; // eslint-disable-line no-unused-vars
 
 /**
- * Add a task to the datatable.
- * @param {mode} mode - Create or edit.
- * @param {properties} properties - Properties of the task to add.
- */
-function addTask(mode, properties) {
-  let values = [];
-  for (let i = 0; i < fields.length; i++) {
-    if (fields[i] != 'recurrent') {
-      values.push(`${properties[fields[i]]}`);
-    }
-  }
-  const status = properties.status == 'Active' ? 'pause' : 'resume';
-  values.push(
-    `<button id="pause-resume-${properties.id}" type="button"
-    class="btn btn-success btn-xs" onclick="${status}Task('${properties.id}')">
-    ${status.charAt(0).toUpperCase() + status.substr(1)}</button>`,
-    `<button type="button" class="btn btn-primary btn-xs"
-    onclick="showTaskModal('${properties.id}')">Edit</button>`,
-    `<button type="button" class="btn btn-primary btn-xs"
-    onclick="showTaskModal('${properties.id}', true)">Duplicate</button>`,
-    `<button type="button" class="btn btn-danger btn-xs"
-    onclick="confirmDeletion('task', '${properties.id}')">Delete</button>`
-  );
-  if (mode == 'edit') {
-    table.row($(`#${properties.id}`)).data(values);
-  } else {
-    const rowNode = table.row.add(values).draw(false).node();
-    $(rowNode).attr('id', `${properties.id}`);
-  }
-}
-
-/**
  * Start or shutdown the scheduler.
  * @param {action} action - Pause or resume.
  */
@@ -78,19 +46,6 @@ function resumeTask(id) { // eslint-disable-line no-unused-vars
       `pauseTask('${id}')`
     ).text('Pause');
     alertify.notify('Task resumed.', 'success', 5);
-  });
-}
-
-/**
- * Get Task Status.
- */
-function getStatus() {
-  call(`/scheduling/get_status`, function(status) {
-    for (let i = 0; i < status.length; i++) {
-      const col = table.column('#status');
-      table.cell(i, col).data(status[i]).draw(false);
-    }
-    setTimeout(getStatus, 5000);
   });
 }
 

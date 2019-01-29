d114c5ac7220db35cae9fa3a3685c681a7cabd8f
minto <minto@hotmail.fr>
2019-01-29 09:07:34 +0100

    add all parameters as environment variables p4

--- eNMS/admin/models.py
+++ eNMS/admin/models.py
@@ -50,17 +50,17 @@ class Parameters(Base):
     __tablename__ = type = 'Parameters'
     id = Column(Integer, primary_key=True)
     name = Column(String, default='default', unique=True)
-    cluster_scan_subnet = Column(String, default='192.168.105.0/24')
-    cluster_scan_protocol = Column(String, default='http')
-    cluster_scan_timeout = Column(Float, default=0.05)
-    default_longitude = Column(Float, default=-96.)
-    default_latitude = Column(Float, default=33.)
-    default_zoom_level = Column(Integer, default=5)
-    default_view = Column(String, default='2D')
+    cluster_scan_subnet = Column(String)
+    cluster_scan_protocol = Column(String)
+    cluster_scan_timeout = Column(Float)
+    default_longitude = Column(Float)
+    default_latitude = Column(Float)
+    default_zoom_level = Column(Integer)
+    default_view = Column(String)
     git_repository_configurations = Column(String)
-    git_repository_automation = Column(String, default='')
-    gotty_start_port = Column(Integer, default=9000)
-    gotty_end_port = Column(Integer, default=9100)
+    git_repository_automation = Column(String)
+    gotty_start_port = Column(Integer)
+    gotty_end_port = Column(Integer)
     gotty_port_index = Column(Integer, default=-1)
     opennms_rest_api = Column(
         String,
@@ -71,14 +71,11 @@ class Parameters(Base):
         default='https://demo.opennms.org/opennms/rest/nodes'
     )
     opennms_login = Column(String, default='demo')
-    mail_sender = Column(String, default='enms@enms.fr')
-    mail_recipients = Column(String, default='antoinefourmy@gmail.com')
-    mattermost_url = Column(
-        String,
-        default='http://192.168.105.2:8065/hooks/dnuajekqp78jjbbajo8ewuw4ih'
-    )
-    mattermost_channel = Column(String, default='')
-    mattermost_verify_certificate = Column(Boolean, default=True)
+    mail_sender = Column(String)
+    mail_recipients = Column(String)
+    mattermost_url = Column(String)
+    mattermost_channel = Column(String)
+    mattermost_verify_certificate = Column(Boolean)
     slack_token = Column(String)
     slack_channel = Column(String)
     pool_id = Column(Integer, ForeignKey('Pool.id'))

062054a3cab0d7738769cdc5d52ff12260356246
minto <minto@hotmail.fr>
2019-02-13 09:42:10 +0100

    mypy p40

--- eNMS/inventory/routes.py
+++ eNMS/inventory/routes.py
@@ -6,6 +6,7 @@ from flask_login import current_user
 from pynetbox import api as netbox_api
 from requests import get as http_get
 from subprocess import Popen
+from typing import Dict, List
 
 from eNMS.main import db
 from eNMS.base.helpers import factory, fetch, fetch_all, get, get_one, objectify, post
@@ -123,50 +124,50 @@ def connection(device_id: int) -> dict:
     }
 
 
-@post(bp, "/save_device_jobs/<device_id>", "Edit")
+@post(bp, "/save_device_jobs/<int:device_id>", "Edit")
 def save_device_jobs(device_id: int) -> bool:
     fetch("Device", id=device_id).jobs = objectify("Job", request.form["jobs"])
     db.session.commit()
     return True
 
 
-@post(bp, "/save_pool_objects/<id>", "Edit")
-def save_pool_objects(id):
-    pool = fetch("Pool", id=id)
+@post(bp, "/save_pool_objects/<int:device_id>", "Edit")
+def save_pool_objects(device_id: int) -> dict:
+    pool = fetch("Pool", id=device_id)
     pool.devices = objectify("Device", request.form["devices"])
     pool.links = objectify("Link", request.form["links"])
     db.session.commit()
     return pool.serialized
 
 
-@post(bp, "/pool_objects/<pool_id>", "View")
-def filter_pool_objects(pool_id):
+@post(bp, "/pool_objects/<int:pool_id>", "View")
+def filter_pool_objects(pool_id: int) -> dict[str, List[dict]]:
     return fetch("Pool", id=pool_id).filter_objects()
 
 
 @post(bp, "/update_pool/<pool>", "Edit")
-def update_pools(pool):
+def update_pools(pool: str) -> bool:
     if pool == "all":
         for pool in fetch_all("Pool"):
             pool.compute_pool()
     else:
-        fetch("Pool", id=pool).compute_pool()
+        fetch("Pool", id=int(pool)).compute_pool()
     db.session.commit()
     return True
 
 
 @post(bp, "/import_topology", "Edit")
-def import_topology():
+def import_topology() -> str:
     return object_import(request.form, request.files["file"])
 
 
 @post(bp, "/export_topology", "View")
-def export_topology():
+def export_topology() -> bool:
     return object_export(request.form, app.path)
 
 
 @post(bp, "/query_opennms", "Edit")
-def query_opennms():
+def query_opennms() -> bool:
     parameters = get_one("Parameters")
     login, password = parameters.opennms_login, request.form["password"]
     parameters.update(**request.form)
@@ -206,7 +207,7 @@ def query_opennms():
 
 
 @post(bp, "/query_netbox", "Edit")
-def query_netbox():
+def query_netbox() -> bool:
     nb = netbox_api(request.form["netbox_address"], token=request.form["netbox_token"])
     for device in nb.dcim.devices.all():
         device_ip = device.primary_ip4 or device.primary_ip6
@@ -224,7 +225,7 @@ def query_netbox():
 
 
 @post(bp, "/query_librenms", "Edit")
-def query_librenms():
+def query_librenms() -> bool:
     devices = http_get(
         f'{request.form["librenms_address"]}/api/v0/devices',
         headers={"X-Auth-Token": request.form["librenms_token"]},
@@ -244,13 +245,13 @@ def query_librenms():
     return True
 
 
-@post(bp, "/get_configurations/<id>", "View")
-def get_configurations(id):
-    return {str(k): v for k, v in fetch("Device", id=id).configurations.items()}
+@post(bp, "/get_configurations/<int:device_id>", "View")
+def get_configurations(device_id: int) -> dict:
+    return {str(k): v for k, v in fetch("Device", id=device_id).configurations.items()}
 
 
-@post(bp, "/get_diff/<device_id>/<v1>/<v2>", "View")
-def get_diff(device_id, v1, v2, n1=None, n2=None):
+@post(bp, "/get_diff/<int:device_id>/<v1>/<v2>", "View")
+def get_diff(device_id: int, v1: str, v2: str) -> dict:
     device = fetch("Device", id=device_id)
     v1, v2 = [datetime.strptime(d, "%Y-%m-%d %H:%M:%S.%f") for d in (v1, v2)]
     first = device.configurations[v1].splitlines()
@@ -259,15 +260,15 @@ def get_diff(device_id, v1, v2, n1=None, n2=None):
     return {"first": first, "second": second, "opcodes": opcodes}
 
 
-@post(bp, "/clear_configurations/<device_id>", "Edit")
-def clear_configurations(device_id):
+@post(bp, "/clear_configurations/<int:device_id>", "Edit")
+def clear_configurations(device_id: int) -> bool:
     fetch("Device", id=device_id).configurations = {}
     db.session.commit()
     return True
 
 
-@get(bp, "/get_raw_logs/<device_id>/<version>", "Edit")
-def get_raw_logs(device_id, version):
+@get(bp, "/get_raw_logs/<int:device_id>/<version>", "Edit")
+def get_raw_logs(device_id: int, version: str) -> str:
     device = fetch("Device", id=device_id)
     configurations = {str(k): v for k, v in device.configurations.items()}
     return f'<pre>{configurations.get(version, "")}</pre>'

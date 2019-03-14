27381b08b512d7b773e42541f9ad351a34cd36ed
minto <minto@hotmail.fr>
2019-03-14 22:39:27 +0100

    1 gunicorn worker by default

--- gunicorn.py
+++ gunicorn.py
@@ -1,7 +1,7 @@
 from os import environ
 
 bind = "0.0.0.0:5000"
-workers = 5
+workers = 1
 accesslog = environ.get("GUNICORN_ACCESS_LOG", "-")
 loglevel = environ.get("GUNICORN_LOG_LEVEL", "debug").lower()
 capture_output = True

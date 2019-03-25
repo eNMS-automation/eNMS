86d5f7bf17987eee706f0bf531940539e2e5891b
minto <minto@hotmail.fr>
2019-03-25 12:07:57 +0100

    split network / site view p4

--- eNMS/views/static/networkView.js
+++ eNMS/views/static/networkView.js
@@ -10,46 +10,6 @@ view: false
 WE: false
 */
 
-/**
- * Switch dimension.
- * @param {dimension} dimension - Dimension.
- */
-function switchView(newView) {
-  $('#map,#earth').css('visibility', 'visible');
-  deleteAll();
-  view = newView;
-  newDimension = newView.substring(0, 2);
-  if (dimension != newDimension) {
-    $('.flip-container').toggleClass('hover');
-    setTimeout(function() {
-      if (newDimension == '3D') {
-        $('#map').css('visibility', 'hidden');
-      } else {
-        $('#earth').css('visibility', 'hidden');
-      }
-    }, 1600);
-  }
-  dimension = newDimension;
-  $('#pool-filter').change();
-}
-
-/**
- * Change the tile layer.
- * @param {layer} layer - tile layer.
- */
-function switchLayer(layer) {
-  if (view == '2D' || view == '2DC') {
-    map.removeLayer(layer2D);
-    layer2D = L.tileLayer(layers[layer]);
-    map.addLayer(layer2D);
-  } else {
-    layer3D.removeFrom(earth);
-    layer3D = WE.tileLayer(layers[layer]);
-    layer3D.addTo(earth);
-  }
-  $('.dropdown-submenu a.menu-layer').next('ul').toggle();
-}
-
 const routerIcon = window['icon_router'];
 
 function updateView() {

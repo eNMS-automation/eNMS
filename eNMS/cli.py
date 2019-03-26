e89adcec7790e4cb73340ebdd33d9d155f6b0ce1
minto <minto@hotmail.fr>
2019-03-26 23:48:51 +0100

    CLI interface p4

--- eNMS/cli.py
+++ eNMS/cli.py
@@ -1,18 +1,18 @@
-from click import argument, echo
+from click import argument, echo, option
 from flask import Flask
 from flask.cli import with_appcontext
 from json import loads
 
 
-from eNMS.base.functions import factory, fetch, str_dict
+from eNMS.base.functions import delete, factory, fetch, str_dict
 
 
 def configure_cli(app: Flask) -> None:
-    @app.cli.command()
+    @app.cli.command(name="fetch")
     @argument("table")
     @argument("name")
-    def get(table, name):
-        # example: flask get device Washington
+    def cli_fetch(table, name):
+        # example: flask fetch device Washington
         echo(str_dict(fetch(table, name=name).get_properties()))
 
     @app.cli.command()
@@ -21,3 +21,30 @@ def configure_cli(app: Flask) -> None:
     def update(table, properties):
         # example: flask update device '{"name": "Aserver", "description": "test"}'
         echo(str_dict(factory(table, **loads(properties)).get_properties()))
+
+    @app.cli.command(name="delete")
+    @argument("table")
+    @argument("name")
+    def cli_delete(table, name):
+        # example: flask delete device Washington
+        echo(str_dict(delete(table, name=name)))
+
+    @app.cli.command()
+    @argument("name")
+    @option("--devices")
+    @option("--payload")
+    def start(name, devices, payload):
+        print(devices, payload)
+        # example: flask start service_name
+        # example 2: flask start service_name device1,device2,device3
+        if devices:
+            devices = {fetch("Device", name=name) for name in devices.split(",")}
+        else:
+            devices = set()
+        if payload:
+            payload = loads(properties)
+        echo(
+            str_dict(fetch("Job", name=name).try_run(targets=targets, payload=payload))[
+                0
+            ]
+        )

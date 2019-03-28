e1e7b828660895ed9550f80f7f1ad4f722cebc8d
minto <minto@hotmail.fr>
2019-03-28 09:51:48 +0100

    dashboard + object curly spacing in eslint

--- eNMS/base/static/dashboard.js
+++ eNMS/base/static/dashboard.js
@@ -9,42 +9,48 @@ echarts: false
 
 const theme = {
   color: [
-    '#26B99A', '#34495E', '#BDC3C7', '#3498DB',
-    '#9B59B6', '#8abb6f', '#759c6a', '#bfd3b7',
+    "#26B99A",
+    "#34495E",
+    "#BDC3C7",
+    "#3498DB",
+    "#9B59B6",
+    "#8abb6f",
+    "#759c6a",
+    "#bfd3b7",
   ],
   title: {
     itemGap: 8,
     textStyle: {
-      fontWeight: 'normal',
-      color: '#408829',
+      fontWeight: "normal",
+      color: "#408829",
     },
   },
   dataRange: {
-    color: ['#1f610a', '#97b58d'],
+    color: ["#1f610a", "#97b58d"],
   },
   toolbox: {
-    color: ['#408829', '#408829', '#408829', '#408829'],
+    color: ["#408829", "#408829", "#408829", "#408829"],
   },
   tooltip: {
-    backgroundColor: 'rgba(0,0,0,0.5)',
+    backgroundColor: "rgba(0,0,0,0.5)",
     axisPointer: {
-      type: 'line',
+      type: "line",
       lineStyle: {
-        color: '#408829',
-        type: 'dashed',
+        color: "#408829",
+        type: "dashed",
       },
       crossStyle: {
-        color: '#408829',
+        color: "#408829",
       },
       shadowStyle: {
-        color: 'rgba(200,200,200,0.3)',
+        color: "rgba(200,200,200,0.3)",
       },
     },
   },
   dataZoom: {
-    dataBackgroundColor: '#eee',
-    fillerColor: 'rgba(64,136,41,0.2)',
-    handleColor: '#408829',
+    dataBackgroundColor: "#eee",
+    fillerColor: "rgba(64,136,41,0.2)",
+    handleColor: "#408829",
   },
   grid: {
     borderWidth: 0,
@@ -52,51 +58,51 @@ const theme = {
   categoryAxis: {
     axisLine: {
       lineStyle: {
-        color: '#408829',
+        color: "#408829",
       },
     },
     splitLine: {
       lineStyle: {
-        color: ['#eee'],
+        color: ["#eee"],
       },
     },
   },
   valueAxis: {
     axisLine: {
       lineStyle: {
-        color: '#408829',
+        color: "#408829",
       },
     },
     splitArea: {
       show: true,
       areaStyle: {
-        color: ['rgba(250,250,250,0.1)', 'rgba(200,200,200,0.1)'],
+        color: ["rgba(250,250,250,0.1)", "rgba(200,200,200,0.1)"],
       },
     },
     splitLine: {
       lineStyle: {
-        color: ['#eee'],
+        color: ["#eee"],
       },
     },
   },
   timeline: {
     lineStyle: {
-      color: '#408829',
+      color: "#408829",
     },
     controlStyle: {
-      normal: {color: '#408829'},
-      emphasis: {color: '#408829'},
+      normal: { color: "#408829" },
+      emphasis: { color: "#408829" },
     },
   },
   k: {
     itemStyle: {
       normal: {
-        color: '#68a54a',
-        color0: '#a9cba2',
+        color: "#68a54a",
+        color0: "#a9cba2",
         lineStyle: {
           width: 1,
-          color: '#408829',
-          color0: '#86b379',
+          color: "#408829",
+          color0: "#86b379",
         },
       },
     },
@@ -105,21 +111,21 @@ const theme = {
     itemStyle: {
       normal: {
         areaStyle: {
-          color: '#ddd',
+          color: "#ddd",
         },
         label: {
           textStyle: {
-            color: '#c12e34',
+            color: "#c12e34",
           },
         },
       },
       emphasis: {
         areaStyle: {
-          color: '#99d2dd',
+          color: "#99d2dd",
         },
         label: {
           textStyle: {
-            color: '#c12e34',
+            color: "#c12e34",
           },
         },
       },
@@ -129,7 +135,7 @@ const theme = {
     itemStyle: {
       normal: {
         linkStyle: {
-          strokeColor: '#408829',
+          strokeColor: "#408829",
         },
       },
     },
@@ -137,43 +143,39 @@ const theme = {
   chord: {
     padding: 4,
     itemStyle: {
-    normal: {
-      lineStyle: {
-        width: 1,
-        color: 'rgba(128, 128, 128, 0.5)',
-      },
-      chordStyle: {
+      normal: {
         lineStyle: {
           width: 1,
-          color: 'rgba(128, 128, 128, 0.5)',
+          color: "rgba(128, 128, 128, 0.5)",
+        },
+        chordStyle: {
+          lineStyle: {
+            width: 1,
+            color: "rgba(128, 128, 128, 0.5)",
+          },
         },
       },
-    },
-    emphasis: {
-      lineStyle: {
-        width: 1,
-        color: 'rgba(128, 128, 128, 0.5)',
-      },
-      chordStyle: {
+      emphasis: {
         lineStyle: {
           width: 1,
-          color: 'rgba(128, 128, 128, 0.5)',
+          color: "rgba(128, 128, 128, 0.5)",
+        },
+        chordStyle: {
+          lineStyle: {
+            width: 1,
+            color: "rgba(128, 128, 128, 0.5)",
+          },
         },
       },
     },
   },
-},
-gauge: {
+  gauge: {
     startAngle: 225,
     endAngle: -45,
     axisLine: {
       show: true,
       lineStyle: {
-        color: [
-          [0.2, '#86b379'],
-          [0.8, '#68a54a'],
-          [1, '#408829'],
-        ],
+        color: [[0.2, "#86b379"], [0.8, "#68a54a"], [1, "#408829"]],
         width: 8,
       },
     },
@@ -181,37 +183,37 @@ gauge: {
       splitNumber: 10,
       length: 12,
       lineStyle: {
-        color: 'auto',
+        color: "auto",
       },
     },
     axisLabel: {
       textStyle: {
-        color: 'auto',
+        color: "auto",
       },
     },
     splitLine: {
       length: 18,
       lineStyle: {
-        color: 'auto',
+        color: "auto",
       },
     },
     pointer: {
-      length: '90%',
-      color: 'auto',
+      length: "90%",
+      color: "auto",
     },
     title: {
       textStyle: {
-        color: '#333',
+        color: "#333",
       },
     },
     detail: {
       textStyle: {
-        color: 'auto',
+        color: "auto",
       },
     },
   },
   textStyle: {
-    fontFamily: 'Arial, Verdana, sans-serif',
+    fontFamily: "Arial, Verdana, sans-serif",
   },
 };
 
@@ -233,66 +235,68 @@ function drawDiagrams(objects, type) {
   const link = echarts.init(document.getElementById(type), theme);
   link.setOption({
     tooltip: {
-      trigger: 'item',
-      formatter: '{a} <br/>{b} : {c} ({d}%)',
+      trigger: "item",
+      formatter: "{a} <br/>{b} : {c} ({d}%)",
     },
     calculable: true,
     legend: {
-      x: 'center',
-      y: 'bottom',
+      x: "center",
+      y: "bottom",
       data: legend,
     },
     toolbox: {
       show: true,
       feature: {
         magicType: {
-            show: true,
-            type: ['pie', 'funnel'],
-            option: {
+          show: true,
+          type: ["pie", "funnel"],
+          option: {
             funnel: {
-                x: '25%',
-                width: '50%',
-                funnelAlign: 'center',
-                max: 1548,
+              x: "25%",
+              width: "50%",
+              funnelAlign: "center",
+              max: 1548,
             },
           },
         },
         restore: {
-            show: true,
-            title: 'Restore',
+          show: true,
+          title: "Restore",
         },
         saveAsImage: {
-            show: true,
-            title: 'Save Image',
+          show: true,
+          title: "Save Image",
         },
       },
     },
-    series: [{
-      name: type,
-      type: 'pie',
-      radius: ['35%', '55%'],
-      itemStyle: {
-        normal: {
-          label: {
-            show: true,
+    series: [
+      {
+        name: type,
+        type: "pie",
+        radius: ["35%", "55%"],
+        itemStyle: {
+          normal: {
+            label: {
+              show: true,
+            },
+            labelLine: {
+              show: true,
+            },
           },
-          labelLine: {
-            show: true,
-          },
-        },
-        emphasis: {
-          label: {
-            show: true,
-            position: 'center',
-            textStyle: {
-              fontSize: '14',
-              fontWeight: 'normal',
+          emphasis: {
+            label: {
+              show: true,
+              position: "center",
+              textStyle: {
+                fontSize: "14",
+                fontWeight: "normal",
+              },
             },
           },
         },
+        data: data,
       },
-    data: data,
-    }],
+    ],
   });
 }
 
@@ -303,7 +307,7 @@ $.each(defaultProperties, function(type, property) {
 });
 
 $.each(counters, function(type, _) {
-  $(`#${type}-properties`).on('change', function() {
+  $(`#${type}-properties`).on("change", function() {
     call(`/counters/${this.value}/${type}`, function(objects) {
       drawDiagrams(objects, type);
     });
@@ -311,5 +315,5 @@ $.each(counters, function(type, _) {
 });
 
 (function() {
-  doc('https://enms.readthedocs.io/en/latest/base/introduction.html');
+  doc("https://enms.readthedocs.io/en/latest/base/introduction.html");
 })();

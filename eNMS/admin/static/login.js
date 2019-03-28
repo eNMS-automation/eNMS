8626b110567c5e80aeb66b533a519e9acdbbac0d
minto <minto@hotmail.fr>
2019-03-28 07:46:51 +0100

    prettier user management + module type

--- eNMS/admin/static/login.js
+++ eNMS/admin/static/login.js
@@ -7,13 +7,13 @@ fCall: false
 /**
  * Create a new account.
  */
-function signUp() { // eslint-disable-line no-unused-vars
-  fCall('/admin/create_new_user', '#create-user-form', function(result) {
-    if (result == 'duplicate') {
-      alertify.notify('Cannot create new user: duplicate entry.', 'error', 5);
+export function signUp() {
+  fCall("/admin/create_new_user", "#create-user-form", function(result) {
+    if (result == "duplicate") {
+      alertify.notify("Cannot create new user: duplicate entry.", "error", 5);
     } else {
-      alertify.notify('New user created.', 'success', 5);
-      document.getElementById('login-button').click();
+      alertify.notify("New user created.", "success", 5);
+      document.getElementById("login-button").click();
     }
   });
 }
--- eNMS/admin/static/userManagement.js
+++ eNMS/admin/static/userManagement.js
@@ -5,11 +5,14 @@ doc: false
 initTable: false
 */
 
-let table = initTable( // eslint-disable-line no-unused-vars
-  'user', 'user', ['Edit', 'Duplicate', 'Delete']
+export const table = initTable(
+  // eslint-disable-line no-unused-vars
+  "user",
+  "user",
+  ["Edit", "Duplicate", "Delete"]
 );
 
 (function() {
-  doc('https://enms.readthedocs.io/en/latest/security/access.html');
-  convertSelect('#user-permissions', '#user-pools');
+  doc("https://enms.readthedocs.io/en/latest/security/access.html");
+  convertSelect("#user-permissions", "#user-pools");
 })();

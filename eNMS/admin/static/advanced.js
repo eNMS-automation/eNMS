e8c3b41725edce8f8763cbcc378f407b56519c77
minto <minto@hotmail.fr>
2019-03-28 07:36:23 +0100

    add arrowParens option to prettier and fix advanced.js

--- eNMS/admin/static/advanced.js
+++ eNMS/admin/static/advanced.js
@@ -12,9 +12,9 @@ folders: false
  * Export all for migration.
  */
 export function migrationsExport() {
-  alertify.notify('Export initiated.', 'success', 5);
-  fCall('/admin/migration_export', '#import-export-form', function() {
-    alertify.notify('Export successful.', 'success', 5);
+  alertify.notify("Export initiated.", "success", 5);
+  fCall("/admin/migration_export", "#import-export-form", function() {
+    alertify.notify("Export successful.", "success", 5);
   });
 }
 
@@ -22,9 +22,9 @@ export function migrationsExport() {
  * Import all for migration.
  */
 export function migrationsImport() {
-  alertify.notify('Import initiated.', 'success', 5);
-  fCall('/admin/migration_import', '#import-export-form', function(result) {
-    alertify.notify(result, 'success', 5);
+  alertify.notify("Import initiated.", "success", 5);
+  fCall("/admin/migration_import", "#import-export-form", function(result) {
+    alertify.notify(result, "success", 5);
   });
 }
 
@@ -32,9 +32,9 @@ export function migrationsImport() {
  * Database Helpers.
  */
 export function databaseHelpers() {
-  alertify.notify('Starting to delete...', 'success', 5);
-  fCall('/admin/database_helpers', '#database-helpers-form', function(result) {
-    alertify.notify('Deletion done.', 'success', 5);
+  alertify.notify("Starting to delete...", "success", 5);
+  fCall("/admin/database_helpers", "#database-helpers-form", function(result) {
+    alertify.notify("Deletion done.", "success", 5);
   });
 }
 
@@ -42,8 +42,8 @@ export function databaseHelpers() {
  * Reset Status.
  */
 export function resetStatus() {
-  call('/admin/reset_status', function(result) {
-    alertify.notify('Reset successful.', 'success', 5);
+  call("/admin/reset_status", function(result) {
+    alertify.notify("Reset successful.", "success", 5);
   });
 }
 
@@ -51,8 +51,8 @@ export function resetStatus() {
  * Git Action.
  */
 export function getGitContent() {
-  call('/admin/get_git_content', function(result) {
-    alertify.notify('Action successful.', 'success', 5);
+  call("/admin/get_git_content", function(result) {
+    alertify.notify("Action successful.", "success", 5);
   });
 }
 
@@ -62,23 +62,22 @@ export function getGitContent() {
  */
 export function scheduler(action) {
   call(`/admin/scheduler/${action}`, function() {
-      alertify.notify(`Scheduler ${action}d.`, 'success', 5);
-    }
-  );
+    alertify.notify(`Scheduler ${action}d.`, "success", 5);
+  });
 }
 
 (function() {
-  convertSelect('#import_export_types', '#deletion_types');
-  $('#clear_logs_date').datetimepicker({
-    format: 'DD/MM/YYYY HH:mm:ss',
+  convertSelect("#import_export_types", "#deletion_types");
+  $("#clear_logs_date").datetimepicker({
+    format: "DD/MM/YYYY HH:mm:ss",
     widgetPositioning: {
-      horizontal: 'left',
-      vertical: 'bottom',
+      horizontal: "left",
+      vertical: "bottom",
     },
     useCurrent: false,
   });
   folders.forEach((f) => {
-    $('#versions').append(`<option value='${f}'></option>`);
+    $("#versions").append(`<option value='${f}'></option>`);
   });
-  doc('https://enms.readthedocs.io/en/latest/base/migrations.html');
+  doc("https://enms.readthedocs.io/en/latest/base/migrations.html");
 })();

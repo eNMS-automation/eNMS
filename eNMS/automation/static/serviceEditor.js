b0638d2b6355a598266e4fc5613f5dee1c674cfb
minto <minto@hotmail.fr>
2019-03-29 09:54:58 +0100

    prettier + linting service editor

--- eNMS/automation/static/serviceEditor.js
+++ eNMS/automation/static/serviceEditor.js
@@ -12,16 +12,16 @@ workflowBuilder: false;
 */
 
 (function() {
-  convertSelect('#service-devices', '#service-pools');
+  convertSelect("#service-devices", "#service-pools");
   for (let i = 0; i < servicesClasses.length; i++) {
     const cls = servicesClasses[i];
-    $('#service-type').append(`<option value='${cls}'>${cls}</option>`);
+    $("#service-type").append(`<option value='${cls}'>${cls}</option>`);
   }
-  $('#service-type').change(function() {
+  $("#service-type").change(function() {
     editService();
   });
-  $('#edit-service').on('hidden.bs.modal', function() {
-    $('#service-type').prop('disabled', false);
+  $("#edit-service").on("hidden.bs.modal", function() {
+    $("#service-type").prop("disabled", false);
   });
   editService();
 })();
@@ -32,16 +32,16 @@ workflowBuilder: false;
  * @param {duplicate} duplicate - Duplicate.
  */
 function editService(id, duplicate) {
-  const url = `/automation/get_service/${id || $('#service-type').val()}`;
-  if (id) $('#service-type').prop('disabled', true);
+  const url = `/automation/get_service/${id || $("#service-type").val()}`;
+  if (id) $("#service-type").prop("disabled", true);
   call(url, function(r) {
-    for (const type of ['boolean', 'list']) {
+    for (const type of ["boolean", "list"]) {
       const fields = $(`#service-${type}_fields`);
-      const prop = type == 'boolean' ? r.boolean_properties : r.list_properties;
+      const prop = type == "boolean" ? r.boolean_properties : r.list_properties;
       fields.val(`${fields.val()},${prop}`);
     }
-    $('#html-form').html(r.form);
-    if (r.service) processInstance('service', r.service, duplicate);
+    $("#html-form").html(r.form);
+    if (r.service) processInstance("service", r.service, duplicate);
   });
 }
 
@@ -49,18 +49,18 @@ function editService(id, duplicate) {
  * Save a service.
  * @param {run} run - Run after saving.
  */
-function saveService(run=false) { // eslint-disable-line no-unused-vars
-  fCall(
-    `/update/${$('#service-type').val()}`,
-    '#edit-service-form',
-    function(service) {
-      saveInstance('service', service, !run);
-      if (typeof workflowBuilder !== 'undefined') {
-        nodes.update({id: service.id, label: service.name});
-      }
-      if (run) {
-        runJob(service.id);
-      }
+// eslint-disable-next-line
+function saveService(run = false) {
+  // eslint-disable-line no-unused-vars
+  fCall(`/update/${$("#service-type").val()}`, "#edit-service-form", function(
+    service
+  ) {
+    saveInstance("service", service, !run);
+    if (typeof workflowBuilder !== "undefined") {
+      nodes.update({ id: service.id, label: service.name });
     }
-  );
+    if (run) {
+      runJob(service.id);
+    }
+  });
 }

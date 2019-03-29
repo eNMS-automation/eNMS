bd348415dabe78d6929749dcd9d4cef60d3d268b
minto <minto@hotmail.fr>
2019-03-29 10:21:46 +0100

    prettier + linting object management

--- eNMS/inventory/static/deviceManagement.js
+++ eNMS/inventory/static/deviceManagement.js
@@ -5,15 +5,18 @@ doc: false
 initTable: false
 */
 
-let table = initTable( // eslint-disable-line no-unused-vars
-  'device', 'device', ['Edit', 'Duplicate', 'Delete', 'Automation', 'Connect']
+let table = initTable(
+  // eslint-disable-line no-unused-vars
+  "device",
+  "device",
+  ["Edit", "Duplicate", "Delete", "Automation", "Connect"]
 );
 
 (function() {
-  doc('https://enms.readthedocs.io/en/latest/inventory/objects.html');
-  $('#restrict-pool').on('change', function() {
+  doc("https://enms.readthedocs.io/en/latest/inventory/objects.html");
+  $("#restrict-pool").on("change", function() {
     table.ajax.reload(null, false);
   });
-  convertSelect('#jobs', '#restrict-pool');
-  $('#restrict-pool').selectpicker('selectAll');
+  convertSelect("#jobs", "#restrict-pool");
+  $("#restrict-pool").selectpicker("selectAll");
 })();
--- eNMS/inventory/static/linkManagement.js
+++ eNMS/inventory/static/linkManagement.js
@@ -5,15 +5,18 @@ doc: false
 initTable: false
 */
 
-let table = initTable( // eslint-disable-line no-unused-vars
-  'link', 'link', ['Edit', 'Duplicate', 'Delete']
+let table = initTable(
+  // eslint-disable-line no-unused-vars
+  "link",
+  "link",
+  ["Edit", "Duplicate", "Delete"]
 );
 
 (function() {
-  doc('https://enms.readthedocs.io/en/latest/inventory/objects.html');
-  $('#restrict-pool').on('change', function() {
+  doc("https://enms.readthedocs.io/en/latest/inventory/objects.html");
+  $("#restrict-pool").on("change", function() {
     table.ajax.reload(null, false);
   });
-  convertSelect('#link-source', '#link-destination', '#restrict-pool');
-  $('#restrict-pool').selectpicker('selectAll');
+  convertSelect("#link-source", "#link-destination", "#restrict-pool");
+  $("#restrict-pool").selectpicker("selectAll");
 })();

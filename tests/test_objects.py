f8f975a9709b3812af8b682d754000eb4d039b2e
minto <minto@hotmail.fr>
2019-04-01 14:53:12 +0200

    fix tests for real

--- tests/test_objects.py
+++ tests/test_objects.py
@@ -133,7 +133,7 @@ def test_pool_management(user_client: FlaskClient) -> None:
     assert len(p1.links) == 20
     assert len(p2.devices) == 12
     assert len(p2.links) == 4
-    assert len(fetch_all("Pool")) == 5
+    assert len(fetch_all("Pool")) == 8
     user_client.post(f"/delete/pool/{p1.id}")
     user_client.post(f"/delete/pool/{p2.id}")
-    assert len(fetch_all("Pool")) == 3
+    assert len(fetch_all("Pool")) == 6

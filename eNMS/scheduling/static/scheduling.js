350632538b1eb5f6c3856a32b0978b2455c0311c
minto <minto@hotmail.fr>
2019-04-16 00:32:49 +0200

    improved filtering system p24

--- eNMS/scheduling/static/scheduling.js
+++ eNMS/scheduling/static/scheduling.js
@@ -1,3 +1,23 @@
+/*
+global
+alertify: false
+call: false
+doc: false
+initTable: false
+refreshTable: false
+*/
+
+// eslint-disable-next-line no-unused-vars
+let table = initTable("task", "task", [
+  "Next runtime",
+  "Time before next run",
+  "Action",
+  "Edit",
+  "Duplicate",
+  "Delete",
+]);
+const taskManagement = true; // eslint-disable-line no-unused-vars
+
 /**
  * Pause a task.
  * @param {id} id - Task id.
@@ -27,3 +47,8 @@ function resumeTask(id) {
     alertify.notify("Task resumed.", "success", 5);
   });
 }
+
+(function() {
+  doc("https://enms.readthedocs.io/en/latest/scheduling/task_management.html");
+  refreshTable(10000);
+})();

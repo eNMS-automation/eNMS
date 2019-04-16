fd9643d23b85944c796058e8f4294ab99547b3ae
minto <minto@hotmail.fr>
2019-04-16 09:41:52 +0200

    log blueprint refactoring p1

--- eNMS/logs/routes.py
+++ eNMS/logs/routes.py
@@ -6,11 +6,6 @@ from eNMS.logs import bp
 from eNMS.logs.forms import LogAutomationForm
 
 
-@get(bp, "/log_management", "View")
-def log_management() -> dict:
-    return dict(fields=log_public_properties, logs=serialize("Log"))
-
-
 @get(bp, "/log_automation", "View")
 def log_automation() -> dict:
     return dict(

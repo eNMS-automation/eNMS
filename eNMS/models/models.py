d887656ff92827d594696aed637a9e7c7387792c
minto <minto@hotmail.fr>
2019-04-18 01:35:01 +0200

    blueprint refactoring p41

--- eNMS/models/models.py
+++ eNMS/models/models.py
@@ -1,3 +1,4 @@
+from apscheduler.triggers.cron import CronTrigger
 from copy import deepcopy
 from datetime import datetime
 from flask import Flask
@@ -16,6 +17,7 @@ from re import compile, search
 from scp import SCPClient
 from sqlalchemy import (
     Boolean,
+    case,
     Column,
     Float,
     ForeignKey,
@@ -36,8 +38,9 @@ from typing import Any, Dict, List, Optional, Set, Tuple, Union
 from xmltodict import parse
 from yaml import load
 
-from eNMS.extensions import controller, db
+from eNMS.extensions import controller, db, scheduler
 from eNMS.functions import add_classes, fetch, fetch_all, session_scope
+from eNMS.helpers import scheduler_job
 from eNMS.associations import (
     job_device_table,
     job_log_rule_table,
@@ -1050,6 +1053,142 @@ class LogRule(Base):
             onclick="deleteInstance('logrule', '{self.id}')">
             Delete</button>""",
         ]
+
+
+class Task(Base):
+
+    __tablename__ = "Task"
+    type = "Task"
+    id = Column(Integer, primary_key=True)
+    aps_job_id = Column(String(255))
+    name = Column(String(255), unique=True)
+    description = Column(String(255))
+    creation_time = Column(String(255))
+    scheduling_mode = Column(String(255), default="standard")
+    periodic = Column(Boolean)
+    frequency = Column(Integer)
+    frequency_unit = Column(String(255), default="seconds")
+    start_date = Column(String(255))
+    end_date = Column(String(255))
+    crontab_expression = Column(String(255))
+    is_active = Column(Boolean, default=False)
+    job_id = Column(Integer, ForeignKey("Job.id"))
+    job = relationship("Job", back_populates="tasks")
+    job_name = association_proxy("job", "name")
+
+    def __init__(self, **kwargs: Any) -> None:
+        super().update(**kwargs)
+        self.creation_time = str(datetime.now())
+        self.aps_job_id = kwargs.get("aps_job_id", self.creation_time)
+        if self.is_active:
+            self.schedule()
+
+    def update(self, **kwargs: Any) -> None:
+        super().update(**kwargs)
+        if self.is_active:
+            self.schedule()
+
+    def generate_row(self, table: str) -> List[str]:
+        status = "Pause" if self.is_active else "Resume"
+        return [
+            f"""<button id="pause-resume-{self.id}" type="button"
+            class="btn btn-success btn-xs" onclick=
+            "{status.lower()}Task('{self.id}')">{status}</button>""",
+            f"""<button type="button" class="btn btn-primary btn-xs"
+            onclick="showTypePanel('task', '{self.id}')">Edit</button>""",
+            f"""<button type="button" class="btn btn-primary btn-xs"
+            onclick="showTypePanel('task', '{self.id}', true)">
+            Duplicate</button>""",
+            f"""<button type="button" class="btn btn-danger btn-xs"
+            onclick="confirmDeletion('task', '{self.id}')">
+            Delete</button>""",
+        ]
+
+    @hybrid_property
+    def status(self) -> str:
+        return "Active" if self.is_active else "Inactive"
+
+    @status.expression  # type: ignore
+    def status(cls) -> str:  # noqa: N805
+        return case([(cls.is_active, "Active")], else_="Inactive")
+
+    @property
+    def next_run_time(self) -> Optional[str]:
+        job = scheduler.get_job(self.aps_job_id)
+        if job and job.next_run_time:
+            return job.next_run_time.strftime("%Y-%m-%d %H:%M:%S")
+        return None
+
+    @property
+    def time_before_next_run(self) -> Optional[str]:
+        job = scheduler.get_job(self.aps_job_id)
+        if job and job.next_run_time:
+            delta = job.next_run_time.replace(tzinfo=None) - datetime.now()
+            hours, remainder = divmod(delta.seconds, 3600)
+            minutes, seconds = divmod(remainder, 60)
+            days = f"{delta.days} days, " if delta.days else ""
+            return f"{days}{hours}h:{minutes}m:{seconds}s"
+        return None
+
+    def aps_conversion(self, date: str) -> str:
+        dt: datetime = datetime.strptime(date, "%d/%m/%Y %H:%M:%S")
+        return datetime.strftime(dt, "%Y-%m-%d %H:%M:%S")
+
+    def aps_date(self, datetype: str) -> Optional[str]:
+        date = getattr(self, datetype)
+        return self.aps_conversion(date) if date else None
+
+    def pause(self) -> None:
+        scheduler.pause_job(self.aps_job_id)
+        self.is_active = False
+        db.session.commit()
+
+    def resume(self) -> None:
+        self.schedule()
+        scheduler.resume_job(self.aps_job_id)
+        self.is_active = True
+        db.session.commit()
+
+    def delete_task(self) -> None:
+        if scheduler.get_job(self.aps_job_id):
+            scheduler.remove_job(self.aps_job_id)
+        db.session.commit()
+
+    def kwargs(self) -> Tuple[dict, dict]:
+        default = {
+            "id": self.aps_job_id,
+            "func": scheduler_job,
+            "replace_existing": True,
+            "args": [self.job.id, self.aps_job_id],
+        }
+        if self.scheduling_mode == "cron":
+            self.periodic = True
+            trigger = {"trigger": CronTrigger.from_crontab(self.crontab_expression)}
+        elif self.frequency:
+            self.periodic = True
+            frequency_in_seconds = (
+                int(self.frequency)
+                * {"seconds": 1, "minutes": 60, "hours": 3600, "days": 86400}[
+                    self.frequency_unit
+                ]
+            )
+            trigger = {
+                "trigger": "interval",
+                "start_date": self.aps_date("start_date"),
+                "end_date": self.aps_date("end_date"),
+                "seconds": frequency_in_seconds,
+            }
+        else:
+            self.periodic = False
+            trigger = {"trigger": "date", "run_date": self.aps_date("start_date")}
+        return default, trigger
+
+    def schedule(self) -> None:
+        default, trigger = self.kwargs()
+        if not scheduler.get_job(self.aps_job_id):
+            scheduler.add_job(**{**default, **trigger})
+        else:
+            scheduler.reschedule_job(default.pop("id"), **trigger)
 
 
 add_classes(

1f57704a349aa0d825c5340909ff4f6a53540732
minto <minto@hotmail.fr>
2019-04-18 00:30:43 +0200

    blueprint refactoring p16

--- eNMS/forms.py
+++ eNMS/forms.py
@@ -30,8 +30,17 @@ from wtforms import (
     SelectMultipleField,
 )
 
-from eNMS.models.base_models import MultipleObjectField
-from eNMS.properties import import_properties, user_permissions
+from eNMS.automation.functions import NAPALM_DRIVERS, NETMIKO_DRIVERS
+from eNMS.models.base_models import MultipleObjectField, ObjectField
+from eNMS.properties import (
+    custom_properties,
+    pool_link_properties,
+    link_subtypes,
+    pool_device_properties,
+    device_subtypes,
+    import_properties,
+    user_permissions,
+)
 
 
 class LoginForm(FlaskForm):

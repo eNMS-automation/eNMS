dc338df93e21fec8af81092393f7633f0de8c53a
minto <minto@hotmail.fr>
2019-04-27 18:40:32 +0200

    fix login system p1

--- eNMS/controller/administration_controller.py
+++ eNMS/controller/administration_controller.py
@@ -46,56 +46,59 @@ class AdministrationController:
                     if datetime.strptime(date, "%Y-%m-%d-%H:%M:%S.%f") > clear_date
                 }
 
-    def check_credentials(self):
-        name, password = request.form["name"], request.form["password"]
-        try:
-            if request.form["authentication_method"] == "Local User":
-                user = fetch("User", name=name)
-                if user and password == user.password:
-                    login_user(user)
-                    return redirect(url_for("bp.get_route", page="dashboard"))
-            elif request.form["authentication_method"] == "LDAP Domain":
-                with Connection(
-                    ldap_client,
-                    user=f'{app.config["LDAP_USERDN"]}\\{name}',
-                    password=password,
-                    auto_bind=True,
-                    authentication=NTLM,
-                ) as connection:
-                    connection.search(
-                        app.config["LDAP_BASEDN"],
-                        f"(&(objectClass=person)(samaccountname={name}))",
-                        search_scope=SUBTREE,
-                        get_operational_attributes=True,
-                        attributes=["cn", "memberOf", "mail"],
-                    )
-                    json_response = loads(connection.response_to_json())["entries"][0]
-                    if json_response:
-                        user = {
-                            "name": name,
-                            "password": password,
-                            "email": json_response["attributes"].get("mail", ""),
-                        }
-                        if any(
-                            group in s
-                            for group in app.config["LDAP_ADMIN_GROUP"]
-                            for s in json_response["attributes"]["memberOf"]
-                        ):
-                            user["permissions"] = ["Admin"]
-                        new_user = factory("User", **user)
-                        login_user(new_user)
-                        return redirect(url_for("bp.get_route", page="dashboard"))
-            elif request.form["authentication_method"] == "TACACS":
-                if tacacs_client.authenticate(name, password).valid:
-                    user = factory("User", **{"name": name, "password": password})
-                    login_user(user)
-                    return redirect(url_for("bp.get_route", page="dashboard"))
-            abort(403)
-        except Exception as e:
-            info(f"Authentication failed ({str(e)})")
-            abort(403)
+    # def check_credentials(self):
 
-    def login() -> Union[Response, str]:
+    def login(self) -> Union[Response, str]:
+        if request.method == "POST":
+            name, password = request.form["name"], request.form["password"]
+            try:
+                if request.form["authentication_method"] == "Local User":
+                    user = fetch("User", name=name)
+                    if user and password == user.password:
+                        login_user(user)
+                        return redirect(url_for("bp.get_route", page="dashboard"))
+                elif request.form["authentication_method"] == "LDAP Domain":
+                    with Connection(
+                        ldap_client,
+                        user=f'{app.config["LDAP_USERDN"]}\\{name}',
+                        password=password,
+                        auto_bind=True,
+                        authentication=NTLM,
+                    ) as connection:
+                        connection.search(
+                            app.config["LDAP_BASEDN"],
+                            f"(&(objectClass=person)(samaccountname={name}))",
+                            search_scope=SUBTREE,
+                            get_operational_attributes=True,
+                            attributes=["cn", "memberOf", "mail"],
+                        )
+                        json_response = loads(connection.response_to_json())["entries"][
+                            0
+                        ]
+                        if json_response:
+                            user = {
+                                "name": name,
+                                "password": password,
+                                "email": json_response["attributes"].get("mail", ""),
+                            }
+                            if any(
+                                group in s
+                                for group in app.config["LDAP_ADMIN_GROUP"]
+                                for s in json_response["attributes"]["memberOf"]
+                            ):
+                                user["permissions"] = ["Admin"]
+                            new_user = factory("User", **user)
+                            login_user(new_user)
+                            return redirect(url_for("bp.get_route", page="dashboard"))
+                elif request.form["authentication_method"] == "TACACS":
+                    if tacacs_client.authenticate(name, password).valid:
+                        user = factory("User", **{"name": name, "password": password})
+                        login_user(user)
+                        return redirect(url_for("bp.get_route", page="dashboard"))
+                abort(403)
+            except Exception as e:
+                info(f"Authentication failed ({str(e)})")
+                abort(403)
         if not current_user.is_authenticated:
             login_form = LoginForm(request.form)
             authentication_methods = [("Local User",) * 2]
@@ -107,16 +110,16 @@ class AdministrationController:
             return render_template("login.html", login_form=login_form)
         return redirect(url_for("bp.get_route", page="dashboard"))
 
-    def logout() -> Response:
+    def logout(self) -> Response:
         logout_user()
-        return redirect(url_for("bp.get_route"), endpoint="login")
+        return redirect(url_for("bp.get_route", page="login"))
 
-    def save_parameters() -> bool:
+    def save_parameters(self) -> bool:
         parameters = get_one("Parameters")
         parameters.update(**request.form)
         parameters.trigger_active_parameters(app)
 
-    def scan_cluster() -> bool:
+    def scan_cluster(self) -> bool:
         parameters = get_one("Parameters")
         protocol = parameters.cluster_scan_protocol
         for ip_address in IPv4Network(parameters.cluster_scan_subnet):
--- eNMS/routes.py
+++ eNMS/routes.py
@@ -1,4 +1,4 @@
-from flask import jsonify, redirect, render_template, request, url_for
+from flask import current_app, jsonify, redirect, render_template, request, url_for
 from flask_login import current_user, login_required
 from flask.wrappers import Response
 from logging import info
@@ -9,36 +9,37 @@ from eNMS.modules import bp, db
 
 @bp.route("/")
 def site_root() -> Response:
-    return redirect(url_for("bp.get_route"), endpoint="login")
+    return redirect(url_for("bp.get_route", page="login"))
 
 
-@bp.route("/<endpoint>", methods=["GET"])
-@login_required
-def get_route(endpoint: str) -> Response:
-    func, *args = endpoint.split("-")
+@bp.route("/<page>")
+def get_route(page: str) -> Response:
+    if not current_user.is_authenticated and page != "login":
+        return current_app.login_manager.unauthorized()
+    func, *args = page.split("-")
     ctx = getattr(controller, func)(*args) or {}
     if not isinstance(ctx, dict):
         return ctx
-    ctx["endpoint"] = endpoint
+    ctx["endpoint"] = page
     info(
         f"User '{current_user.name}' ({request.remote_addr})"
-        f"calling the endpoint {endpoint} (GET)"
+        f"calling the endpoint {page} (GET)"
     )
     if func == "filtering":
         return jsonify(ctx)
-    return render_template(f"{ctx.pop('template', 'pages/' + endpoint)}.html", **ctx)
+    return render_template(f"{ctx.pop('template', 'pages/' + page)}.html", **ctx)
 
 
-@bp.route("/<endpoint>", methods=["POST"])
+@bp.route("/<page>", methods=["POST"])
 @login_required
-def post_route(endpoint: str) -> Response:
+def post_route(page: str) -> Response:
     data = {**request.form.to_dict(), **{"creator": current_user.id}}
     for property in data.get("list_fields", "").split(","):
         data[property] = request.form.getlist(property)
     for property in data.get("boolean_fields", "").split(","):
         data[property] = property in request.form
     request.form = data
-    func, *args = endpoint.split("-")
+    func, *args = page.split("-")
     info(
         f"User '{current_user.name}' ({request.remote_addr})"
         f" calling the endpoint {request.url} (POST)"
@@ -46,6 +47,9 @@ def post_route(endpoint: str) -> Response:
     # try:
     result = getattr(controller, func)(*args)
     db.session.commit()
-    return jsonify(result)
-    # except Exception as e:
-    # return jsonify({"error": str(e)})
+    if page == "login":
+        return result
+    else:
+        return jsonify(result)
+        # except Exception as e:
+        # return jsonify({"error": str(e)})

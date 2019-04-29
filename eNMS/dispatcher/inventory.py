72d9e399bde6ff69b5edc1b899fa9a7ddcdd33a3
minto <minto@hotmail.fr>
2019-04-29 18:49:58 +0200

    refactor import export dispatcher p2

--- eNMS/dispatcher/inventory.py
+++ eNMS/dispatcher/inventory.py
@@ -6,7 +6,22 @@ from sqlalchemy import and_
 from subprocess import Popen
 from typing import Union
 
-from eNMS.database import fetch, fetch_all, get_one, objectify
+from eNMS.database import (
+    delete_all,
+    export,
+    factory,
+    fetch,
+    fetch_all,
+    get_one,
+    objectify,
+)
+from eNMS.forms import (
+    GoogleEarthForm,
+    ImportExportForm,
+    LibreNmsForm,
+    NetboxForm,
+    OpenNmsForm,
+)
 from eNMS.models import classes
 from eNMS.modules import db
 from eNMS.properties import link_subtype_to_color, subtype_sizes, table_properties
@@ -50,6 +65,41 @@ class InventoryDispatcher:
             "server_addr": app.config["ENMS_SERVER_ADDR"],
         }
 
+    def counters(self, property: str, type: str) -> Counter:
+        property = reverse_pretty_names.get(property, property)
+        return Counter(str(getattr(instance, property)) for instance in fetch_all(type))
+
+    def export_topology() -> None:
+        workbook = Workbook()
+        filename = request.form["export_filename"]
+        if "." not in filename:
+            filename += ".xls"
+        for obj_type in ("Device", "Link"):
+            sheet = workbook.add_sheet(obj_type)
+            for index, property in enumerate(export_properties[obj_type]):
+                sheet.write(0, index, property)
+                for obj_index, obj in enumerate(fetch_all(obj_type), 1):
+                    sheet.write(obj_index, index, getattr(obj, property))
+        workbook.save(current_app.path / "projects" / filename)
+
+    def export_to_google_earth(self) -> None:
+        kml_file = Kml()
+        for device in fetch_all("Device"):
+            point = kml_file.newpoint(name=device.name)
+            point.coords = [(device.longitude, device.latitude)]
+            point.style = google_earth_styles[device.subtype]
+            point.style.labelstyle.scale = request.form["label_size"]
+        for link in fetch_all("Link"):
+            line = kml_file.newlinestring(name=link.name)
+            line.coords = [
+                (link.source.longitude, link.source.latitude),
+                (link.destination.longitude, link.destination.latitude),
+            ]
+            line.style = google_earth_styles[link.subtype]
+            line.style.linestyle.width = request.form["line_width"]
+        filepath = current_app.path / "google_earth" / f'{request.form["name"]}.kmz'
+        kml_file.save(filepath)
+
     def get_configurations(self, device_id: int) -> dict:
         return fetch("Device", id=device_id).get_configurations()
 
@@ -68,6 +118,113 @@ class InventoryDispatcher:
             if log.source == fetch("Device", id=device_id).ip_address
         ]
         return "\n".join(device_logs)
+
+    def import_export(self) -> dict:
+        return dict(
+            import_export_form=ImportExportForm(request.form),
+            librenms_form=LibreNmsForm(request.form),
+            netbox_form=NetboxForm(request.form),
+            opennms_form=OpenNmsForm(request.form),
+            google_earth_form=GoogleEarthForm(request.form),
+            parameters=get_one("Parameters"),
+        )
+
+    def import_from_netbox(self) -> None:
+        nb = netbox_api(
+            request.form["netbox_address"], token=request.form["netbox_token"]
+        )
+        for device in nb.dcim.devices.all():
+            device_ip = device.primary_ip4 or device.primary_ip6
+            factory(
+                "Device",
+                **{
+                    "name": device.name,
+                    "ip_address": str(device_ip).split("/")[0],
+                    "subtype": request.form["netbox_type"],
+                    "longitude": 0.0,
+                    "latitude": 0.0,
+                },
+            )
+
+    def import_from_librenms(self) -> None:
+        devices = http_get(
+            f'{request.form["librenms_address"]}/api/v0/devices',
+            headers={"X-Auth-Token": request.form["librenms_token"]},
+        ).json()["devices"]
+        for device in devices:
+            factory(
+                "Device",
+                **{
+                    "name": device["hostname"],
+                    "ip_address": device["ip"] or device["hostname"],
+                    "subtype": request.form["librenms_type"],
+                    "longitude": 0.0,
+                    "latitude": 0.0,
+                },
+            )
+
+    def import_from_opennms(self):
+        parameters = get_one("Parameters")
+        login, password = parameters.opennms_login, request.form["password"]
+        parameters.update(**request.form)
+        db.session.commit()
+        json_devices = http_get(
+            parameters.opennms_devices,
+            headers={"Accept": "application/json"},
+            auth=(login, password),
+        ).json()["node"]
+        devices = {
+            device["id"]: {
+                "name": device.get("label", device["id"]),
+                "description": device["assetRecord"].get("description", ""),
+                "location": device["assetRecord"].get("building", ""),
+                "vendor": device["assetRecord"].get("manufacturer", ""),
+                "model": device["assetRecord"].get("modelNumber", ""),
+                "operating_system": device.get("operatingSystem", ""),
+                "os_version": device["assetRecord"].get("sysDescription", ""),
+                "longitude": device["assetRecord"].get("longitude", 0.0),
+                "latitude": device["assetRecord"].get("latitude", 0.0),
+                "subtype": request.form["subtype"],
+            }
+            for device in json_devices
+        }
+        for device in list(devices):
+            link = http_get(
+                f"{parameters.opennms_rest_api}/nodes/{device}/ipinterfaces",
+                headers={"Accept": "application/json"},
+                auth=(login, password),
+            ).json()
+            for interface in link["ipInterface"]:
+                if interface["snmpPrimary"] == "P":
+                    devices[device]["ip_address"] = interface["ipAddress"]
+                    factory("Device", **devices[device])
+
+    def import_topology(self, request: dict, file: FileStorage) -> str:
+        if request["replace"]:
+            delete_all("Device")
+        result = "Topology successfully imported."
+        if controller.allowed_file(secure_filename(file.filename), {"xls", "xlsx"}):
+            book = open_workbook(file_contents=file.read())
+            for obj_type in ("Device", "Link"):
+                try:
+                    sheet = book.sheet_by_name(obj_type)
+                except XLRDError:
+                    continue
+                properties = sheet.row_values(0)
+                for row_index in range(1, sheet.nrows):
+                    values = dict(zip(properties, sheet.row_values(row_index)))
+                    values["dont_update_pools"] = True
+                    try:
+                        factory(obj_type, **values).serialized
+                    except Exception as e:
+                        info(f"{str(values)} could not be imported ({str(e)})")
+                        result = "Partial import (see logs)."
+                db.session.commit()
+        for pool in fetch_all("Pool"):
+            pool.compute_pool()
+        db.session.commit()
+        info("Inventory import: Done.")
+        return result
 
     def save_pool_objects(self, pool_id: int) -> dict:
         pool = fetch("Pool", id=pool_id)

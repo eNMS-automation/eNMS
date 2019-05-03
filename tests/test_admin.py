ac5521000866b88a3433eadc24dcc3accf84793b
minto <minto@hotmail.fr>
2019-05-03 11:38:18 +0200

    fix test user creation and deletion

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -1,22 +1,22 @@
 from flask.testing import FlaskClient
 
-from eNMS.functions import fetch, fetch_all
+from eNMS.database import fetch, fetch_all
 
-from tests.test_base import check_blueprints
+from tests.test_base import check_pages
 
 
-@check_blueprints("", "/admin")
+@check_pages("table-user")
 def test_user_management(user_client: FlaskClient) -> None:
     for user in ("user1", "user2", "user3"):
         dict_user = {
-            "list_fields": "permissions",
+            "form_type": "user",
             "name": user,
             "email": f"{user}@test.com",
             "permissions": ["Admin"],
             "password": user,
         }
-        user_client.post("/update/user", data=dict_user)
+        user_client.post("/update-user", data=dict_user)
     assert len(fetch_all("User")) == 4
     user1 = fetch("User", name="user1")
-    user_client.post("/delete/user/{}".format(user1.id))
+    user_client.post("/delete_instance-user-{}".format(user1.id))
     assert len(fetch_all("User")) == 3

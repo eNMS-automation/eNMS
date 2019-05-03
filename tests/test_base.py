c592f267689d9678caf16930664c8419843246ff
minto <minto@hotmail.fr>
2019-05-03 10:48:19 +0200

    refactor test base without blueprints

--- tests/test_base.py
+++ tests/test_base.py
@@ -1,32 +1,31 @@
 from logging import CRITICAL, disable
 from flask.testing import FlaskClient
-from typing import Callable, Dict
+from typing import Callable
 
 disable(CRITICAL)
 
-urls: Dict[str, tuple] = {
-    "": ("/", "/dashboard"),
-    "/admin": (
-        "/user_management",
-        "/login",
-        "/administration",
-        "/advanced",
-        "/instance_management",
-    ),
-    "/inventory": (
-        "/device_management",
-        "/configuration_management",
-        "/link_management",
-        "/pool_management",
-        "/import_export",
-    ),
-    "/views": ("/network_view", "/site_view"),
-    "/automation": ("/service_management", "/workflow_management", "/workflow_builder"),
-    "/scheduling": ("/task_management", "/calendar"),
-    "/logs": ("/log_management", "/log_automation"),
-}
+pages: tuple = (
+    "/administration",
+    "/calendar",
+    "/dashboard",
+    "/login",
+    "/table-configuration",
+    "/table-device",
+    "/table-link",
+    "/table-log",
+    "/table-logrule",
+    "/table-pool",
+    "/table-service",
+    "/table-user",
+    "/table-server",
+    "/table-task",
+    "/table-workflow",
+    "/view-network",
+    "/view-site",
+    "/workflow_builder",
+)
 
-free_access = {"/", "login"}
+free_access_pages = {"/", "/login"}
 
 
 def check_pages(*pages: str) -> Callable:
@@ -43,19 +42,15 @@ def check_pages(*pages: str) -> Callable:
 
 
 def test_authentication(base_client: FlaskClient) -> None:
-    for blueprint, pages in urls.items():
-        for page in pages:
-            page_url = blueprint + page
-            expected_code = 200 if page_url in free_access else 403
-            r = base_client.get(page_url, follow_redirects=True)
-            assert r.status_code == expected_code
+    for page in pages:
+        expected_code = 200 if page in free_access_pages else 403
+        r = base_client.get(page_url, follow_redirects=True)
+        assert r.status_code == expected_code
 
 
 def test_urls(user_client: FlaskClient) -> None:
-    for blueprint, pages in urls.items():
-        for page in pages:
-            page_url = blueprint + page
-            r = user_client.get(page_url, follow_redirects=True)
-            assert r.status_code == 200
-    r = user_client.get("/admin/logout", follow_redirects=True)
+    for pages in pages:
+        r = user_client.get(page, follow_redirects=True)
+        assert r.status_code == 200
+    r = user_client.get("/logout", follow_redirects=True)
     test_authentication(user_client)

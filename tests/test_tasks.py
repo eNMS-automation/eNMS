c110dc883c78d0a5a5aeb0b528c0bfd8ea51e268
minto <minto@hotmail.fr>
2019-05-03 13:13:18 +0200

    fix test task

--- tests/test_tasks.py
+++ tests/test_tasks.py
@@ -1,18 +1,24 @@
 from flask.testing import FlaskClient
 from werkzeug.datastructures import ImmutableMultiDict
 
-from eNMS.functions import fetch_all
+from eNMS.database import fetch_all
 
-from tests.test_base import check_blueprints
+from tests.test_base import check_pages
 from tests.test_objects import create_from_file
 
 
 instant_task = ImmutableMultiDict(
-    [("name", "instant_task"), ("start-task", "run-now"), ("job", "2")]
+    [
+        ("form_type", "task"),
+        ("name", "instant_task"),
+        ("start-task", "run-now"),
+        ("job", "2"),
+    ]
 )
 
 scheduled_task = ImmutableMultiDict(
     [
+        ("form_type", "task"),
         ("name", "scheduled_task"),
         ("start_date", "30/03/2018 19:10:13"),
         ("end_date", "06/04/2018 19:10:13"),
@@ -22,21 +28,10 @@ scheduled_task = ImmutableMultiDict(
 )
 
 
-@check_blueprints("/scheduling")
+@check_pages("table-task", "calendar")
 def test_netmiko_napalm_config(user_client: FlaskClient) -> None:
     create_from_file(user_client, "europe.xls")
-    user_client.post("/update/task", data=instant_task)
+    user_client.post("/update-task", data=instant_task)
     assert len(fetch_all("Task")) == 3
-    user_client.post("/update/task", data=scheduled_task)
+    user_client.post("/update-task", data=scheduled_task)
     assert len(fetch_all("Task")) == 4
-
-
-google_earth_dict = ImmutableMultiDict(
-    [
-        ("google earth", ""),
-        ("name", "test_google_earth"),
-        ("label_size", "1"),
-        ("line_width", "2"),
-        ("netmiko_service", ""),
-    ]
-)

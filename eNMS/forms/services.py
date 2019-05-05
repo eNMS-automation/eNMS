5d60bc9b5b6eb1f46687f0570b183eab1cf980a4
minto <minto@hotmail.fr>
2019-05-05 17:39:33 +0200

    add form property names p7

--- eNMS/forms/services.py
+++ eNMS/forms/services.py
@@ -10,14 +10,17 @@ class ValidationForm(FlaskForm, metaclass=metaform):
     form_type = HiddenField(default="service_validation")
     abstract_service = True
     validation_method = SelectField(
+        "Validation Method",
         choices=(
             ("text", "Validation by text match"),
             ("dict_equal", "Validation by dictionary equality"),
             ("dict_included", "Validation by dictionary inclusion"),
-        )
+        ),
     )
-    content_match = StringField(widget=TextArea(), render_kw={"rows": 5})
-    content_match_regex = BooleanField()
-    dict_match = DictField()
-    negative_logic = BooleanField()
-    delete_spaces_before_matching = BooleanField()
+    content_match = StringField(
+        "Content Match", widget=TextArea(), render_kw={"rows": 5}
+    )
+    content_match_regex = BooleanField("Match content with Regular Expression")
+    dict_match = DictField("Dictionary to Match Against")
+    negative_logic = BooleanField("Negative logic")
+    delete_spaces_before_matching = BooleanField("Delete Spaces before Matching")

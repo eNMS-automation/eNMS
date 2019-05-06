5fd5ed496556d9a9038cc05e349f5862998d52e7
minto <minto@hotmail.fr>
2019-05-07 01:02:00 +0200

    readd pool update code p2

--- eNMS/models/inventory.py
+++ eNMS/models/inventory.py
@@ -212,14 +212,13 @@ class Link(Object):
             {"source_id": kwargs["source"], "destination_id": kwargs["destination"]}
         )
         super().update(**kwargs)
-        """
         for pool in fetch_all("Pool"):
             if pool.never_update:
                 continue
             if pool.object_match(self):
                 pool.links.append(self)
             elif self in pool.links:
-                pool.links.remove(self) """
+                pool.links.remove(self)
 
     def generate_row(self, table: str) -> List[str]:
         return [

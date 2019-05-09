56ca9f557a0661dda0274e9a8a9544231710e707
minto <minto@hotmail.fr>
2019-05-09 11:27:02 +0200

    add webassets p1

--- eNMS/__init__.py
+++ eNMS/__init__.py
@@ -16,7 +16,7 @@ from eNMS.database.functions import fetch, get_one
 from eNMS.default import create_default
 from eNMS.examples import create_examples
 from eNMS.forms import form_properties, property_types
-from eNMS.extensions import bp, csrf, login_manager, mail_client, toolbar
+from eNMS.extensions import assets, bp, csrf, login_manager, mail_client, toolbar
 from eNMS.models.logging import SyslogServer
 from eNMS.models.administration import User
 from eNMS.properties import property_names
@@ -27,6 +27,7 @@ import eNMS.routes  # noqa: F401
 
 def register_modules(app: Flask) -> None:
     app.register_blueprint(bp)
+    assets.init_app(app)
     login_manager.init_app(app)
     mail_client.init_app(app)
     csrf.init_app(app)
@@ -83,6 +84,10 @@ def configure_errors(app: Flask) -> None:
     @app.errorhandler(404)
     def not_found_error(error: Any) -> Tuple[str, int]:
         return render_template("errors/page_404.html"), 404
+
+
+def configure_assets(app: Flask):
+    pass
 
 
 def configure_syslog_server() -> None:
--- eNMS/extensions.py
+++ eNMS/extensions.py
@@ -1,10 +1,12 @@
 from flask import Blueprint
+from flask_assets import Environment
 from flask_debugtoolbar import DebugToolbarExtension
 from flask_httpauth import HTTPBasicAuth
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_wtf.csrf import CSRFProtect
 
+assets = Environment()
 auth = HTTPBasicAuth()
 bp = Blueprint("bp", __name__, template_folder="templates")
 csrf = CSRFProtect()

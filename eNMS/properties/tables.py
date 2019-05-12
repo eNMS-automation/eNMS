2770b93b54e44fc23d29c753ae3848b7fec391c7
minto <minto@hotmail.fr>
2019-05-12 20:04:21 +0200

    refactor table properties p1

---
+++ eNMS/properties/tables.py
@@ -0,0 +1,128 @@
+from typing import Dict, List
+
+from eNMS.properties.properties import device_properties, object_common_properties
+
+device_table_properties: List[str] = object_common_properties + device_properties[:-3]
+
+link_table_properties: List[str] = object_common_properties[1:] + [
+    "source_name",
+    "destination_name",
+]
+
+pool_table_properties: List[str] = [
+    "name",
+    "description",
+    "never_update",
+    "longitude",
+    "latitude",
+    "object_number",
+]
+
+service_table_properties: List[str] = [
+    "name",
+    "type",
+    "description",
+    "creator",
+    "number_of_retries",
+    "time_between_retries",
+    "status",
+    "progress",
+]
+
+workflow_table_properties: List[str] = [
+    "name",
+    "description",
+    "creator",
+    "vendor",
+    "operating_system",
+    "number_of_retries",
+    "time_between_retries",
+    "status",
+    "progress",
+]
+
+user_table_properties: List[str] = ["name", "email"]
+
+server_table_properties = [
+    "name",
+    "description",
+    "ip_address",
+    "weight",
+    "status",
+    "cpu_load",
+]
+
+log_table_properties: List[str] = ["time", "origin", "severity", "content"]
+
+log_rule_table_properties: List[str] = ["name"] + log_table_properties
+
+task_table_properties: List[str] = [
+    "name",
+    "description",
+    "job_name",
+    "status",
+    "scheduling_mode",
+    "start_date",
+    "end_date",
+    "frequency",
+    "frequency_unit",
+    "crontab_expression",
+    "next_run_time",
+    "time_before_next_run",
+]
+
+table_properties: Dict[str, List[str]] = {
+    "configuration": device_configuration_properties,
+    "device": device_table_properties,
+    "server": server_table_properties,
+    "link": link_table_properties,
+    "log": log_table_properties,
+    "logrule": log_rule_table_properties,
+    "pool": pool_table_properties,
+    "service": service_table_properties,
+    "task": task_table_properties,
+    "user": user_table_properties,
+    "workflow": workflow_table_properties,
+}
+
+job_filtering_properties = {
+    "name",
+    "type",
+    "description",
+    "creator",
+    "max_processes",
+    "credentials",
+    "waiting_time",
+    "send_notification_method",
+    "mail_recipient",
+    "number_of_retries",
+    "time_between_retries",
+}
+
+filtering_properties: Dict[str, List[str]] = {
+    "configuration": device_configuration_properties + ["current_configuration"],
+    "device": device_table_properties + ["current_configuration"],
+    "server": server_table_properties,
+    "link": link_table_properties,
+    "log": log_table_properties,
+    "logrule": log_rule_table_properties,
+    "pool": pool_table_properties,
+    "service": job_filtering_properties,
+    "task": task_table_properties,
+    "user": user_table_properties,
+    "workflow": job_filtering_properties,
+}
+
+table_fixed_columns: Dict[str, List[str]] = {
+    "configuration": ["Configuration", "Download", "Edit"],
+    "device": ["Automation", "Connect", "Edit", "Duplicate", "Delete"],
+    "server": ["Edit", "Duplicate", "Delete"],
+    "link": ["Edit", "Duplicate", "Delete"],
+    "log": [],
+    "logrule": ["Edit", "Delete"],
+    "service": ["Logs", "Results", "Run", "Edit", "Duplicate", "Delete"],
+    "task": ["Action", "Edit", "Duplicate", "Delete"],
+    "user": ["Edit", "Duplicate", "Delete"],
+    "pool": ["Visualize", "Edit", "Update", "Duplicate", "Edit objects", "Delete"],
+    "workflow": ["Logs", "Results", "Run", "Edit", "Duplicate", "Delete"],
+}

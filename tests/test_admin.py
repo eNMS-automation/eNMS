f31125f9f6432286398fbca0bf12a5957610f138
afourmy <antoine.fourmy@gmail.com>
2019-05-24 20:07:44 +0200

    remove prettier hook and fix tests

--- tests/test_admin.py
+++ tests/test_admin.py
@@ -5,7 +5,7 @@ from eNMS.database.functions import fetch, fetch_all
 from tests.test_base import check_pages
 
 
-@check_pages("table-user")
+@check_pages("table/user")
 def test_user_management(user_client: FlaskClient) -> None:
     for user in ("user1", "user2", "user3"):
         dict_user = {
@@ -15,8 +15,8 @@ def test_user_management(user_client: FlaskClient) -> None:
             "permissions": ["Admin"],
             "password": user,
         }
-        user_client.post("/update-user", data=dict_user)
+        user_client.post("/update/user", data=dict_user)
     assert len(fetch_all("User")) == 4
     user1 = fetch("User", name="user1")
-    user_client.post("/delete_instance-user-{}".format(user1.id))
+    user_client.post("/delete_instance/user/{}".format(user1.id))
     assert len(fetch_all("User")) == 3

3159ea60aa1b7934cbc6cc5d5ec59289bd7b9dc8
afourmy <antoine.fourmy@gmail.com>
2019-06-05 17:34:33 +0200

    add python snippet service

---
+++ eNMS/services/miscellaneous/python_snippet_service.py
@@ -0,0 +1,118 @@
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, Text
+from typing import Any, Optional
+from wtforms import BooleanField, HiddenField, StringField
+from wtforms.widgets import TextArea
+
+from eNMS.forms.automation import ServiceForm
+from eNMS.models.automation import Service
+from eNMS.models.inventory import Device
+
+
+class PythonSnippetService(Service):
+
+    __tablename__ = "PythonSnippetService"
+
+    id = Column(Integer, ForeignKey("Service.id"), primary_key=True)
+    has_targets = Column(Boolean, default=False)
+    source_code = Column(Text)
+
+    __mapper_args__ = {"polymorphic_identity": "PythonSnippetService"}
+
+    def job(self, payload: dict, device: Optional[Device] = None) -> dict:
+
+        # Compile the Python code
+        try:
+            code_object = compile(self.source_code, "user_python_code", "exec")
+        except Exception as exc:
+            self.logs.append(f"Compile error: {str(exc)}")
+            return {"success": False, " result": {"step": "compile", "error": str(exc)}}
+
+        # Mechanism to save the result from within the snippet
+        _code_result_ = {}  # Set by save_result()
+
+        class TerminateException(Exception):  # Raised to terminate the script
+            pass
+
+        def save_result(success, result, **kwargs):
+            # Don't replace existing dictionary
+            _code_result_.update({"success": success, "result": result, **kwargs})
+            if kwargs.get("exit"):
+                raise TerminateException()
+
+        # Support for global and service variables
+        def get_var(name: str, service: Optional[str] = None):
+            if service:  # Access service local variable
+                svc_result = payload.get(service)
+                if not svc_result:
+                    raise Exception(
+                        f"get_var: Service not found in payload: '{service}'"
+                    )
+                return svc_result.get(name)
+            else:  # Access global variable
+                payload.setdefault("variables", {}).get(name)
+
+        def set_var(name: str, value: Any, service: Optional[str] = None):
+            if service:  # Access service local variable
+                svc_result = payload.get(service)
+                if not svc_result:
+                    raise Exception(
+                        f"set_var: Service not found in payload: '{service}'"
+                    )
+                svc_result[name] = value
+            else:  # Access global variable
+                payload.setdefault("variables", {})[name] = value
+
+        # Setup the environment
+        locals = {
+            "device": device,
+            "payload": payload,
+            "_code_result_": _code_result_,
+            "get_var": get_var,
+            "log": self.logs.append,
+            "save_result": save_result,
+            "set_var": set_var,
+        }
+
+        # Execute the snippet, allowing built-ins, and locals
+        try:
+            exec(code_object, {}, locals)
+        except TerminateException:
+            pass  # Ignore - clean exit from middle of snippet
+        except Exception as exc:
+            self.logs.append(f"Execution error: {str(exc)}")
+            return {
+                "success": False,
+                "result": {
+                    "step": "execute",
+                    "error": str(exc),
+                    "result": _code_result_,
+                },
+            }
+
+        if not _code_result_:
+            self.logs.append("Error: Result not set by user code on service instance")
+            _code_result_ = {
+                "success": False,
+                "result": {"error": "Result not set by user code on service instance"},
+            }
+        return _code_result_
+
+
+class PythonSnippetForm(ServiceForm):
+    form_type = HiddenField(default="PythonSnippetService")
+    has_targets = BooleanField()
+    source_code = StringField(
+        widget=TextArea(),
+        render_kw={"rows": 5},
+        default="""
+# The following input variables are available: payload, device
+# You must call save_result() to return a result:
+#    save_result(success=True, result={...})
+#    The result dict can return any data pertinent to service execution.
+#    Add exit=True to terminate execution in the middle of the python snippet.
+# You can log strings using the log() function:
+#    log("important message")
+
+result = {}
+save_result(success=True, result=result}""",
+    )

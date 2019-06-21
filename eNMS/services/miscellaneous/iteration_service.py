150929bceace08ef1ef56b57c1a2632cbecf52f8
afourmy <antoine.fourmy@gmail.com>
2019-06-21 17:58:45 +0200

    iteration service p3

--- eNMS/services/miscellaneous/iteration_service.py
+++ eNMS/services/miscellaneous/iteration_service.py
@@ -1,12 +1,15 @@
-from sqlalchemy import Column, ForeignKey, Integer, PickleType, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, PickleType, String
 from sqlalchemy.ext.mutable import MutableDict
 from sqlalchemy.orm import relationship
-from wtforms import HiddenField, SelectField
+from typing import Optional, Set
+from wtforms import BooleanField, HiddenField, SelectField, StringField
+from yaql import factory
 
 from eNMS.database import SMALL_STRING_LENGTH
 from eNMS.forms.automation import ServiceForm
 from eNMS.forms.fields import DictField, InstanceField
 from eNMS.models.automation import Service
+from eNMS.models.inventory import Device
 
 
 class IterationService(Service):
@@ -14,29 +17,39 @@ class IterationService(Service):
     __tablename__ = "IterationService"
 
     id = Column(Integer, ForeignKey("Service.id"), primary_key=True)
-    has_targets = False
+    has_targets = True
+    origin_of_targets = Column(String(SMALL_STRING_LENGTH), default="iteration")
     iterated_job_id = Column(Integer, ForeignKey("Job.id"))
     iterated_job = relationship(
         "Job", primaryjoin="Job.id == IterationService.iterated_job_id"
     )
-    origin_of_targets = Column(String(SMALL_STRING_LENGTH), default="iteration")
+    origin_of_values = Column(String(SMALL_STRING_LENGTH), default="iteration_values")
+    yaql_query = Column(String(SMALL_STRING_LENGTH), default="")
     iteration_values = Column(MutableDict.as_mutable(PickleType), default={})
+    per_device_values = Column(Boolean, default=False)
 
     __mapper_args__ = {"polymorphic_identity": "IterationService"}
 
-    def job(self, payload: dict) -> dict:
-        devices = (
-            self.compute_devices()
-            if self.origin_of_targets == "iteration"
-            else self.iterated_job.devices
-        )
-        payload[self.name] = self.iteration_values
-        results, _ = self.iterated_job.run(payload, devices, self)
-        return {
-            "Iterated devices": [device.name for device in devices],
-            "Iteration values": self.iteration_values,
-            **results,
-        }
+    def compute_devices(self, **kwargs) -> Set["Device"]:
+        if self.origin_of_targets == "iteration":
+            return super().compute_devices(**kwargs)
+        else:
+            return self.iterated_job.compute_devices(**kwargs)
+
+    def job(self, payload: dict, device: Optional[Device] = None) -> dict:
+        if self.origin_of_values == "iteration_values":
+            values = self.iteration_values
+        else:
+            engine = factory.YaqlFactory().create()
+            values = engine(self.yaql_query).evaluate(data=payload)
+        if self.per_device_values:
+            values = self.iteration_values[device.name]
+        else:
+            values = self.iteration_values
+        for value in values:
+            payload["iteration_value"] = value
+            results, _ = self.iterated_job.job(payload, device, self)
+        return {"Iteration values": values, **results}
 
 
 class IterationForm(ServiceForm):
@@ -48,4 +61,12 @@ class IterationForm(ServiceForm):
             ("iterated", "Use Targets from Iterated Service"),
         )
     )
+    origin_of_targets = SelectField(
+        choices=(
+            ("iteration_values", "Iteration Values Dictionary in this form"),
+            ("yaql", "YaQL Query on the Payload"),
+        )
+    )
     iteration_values = DictField()
+    yaql_query = StringField()
+    has_targets = BooleanField()

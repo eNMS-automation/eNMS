143712e025e50090d02ef9d381a6ed8a192e2611
afourmy <antoine.fourmy@gmail.com>
2019-06-23 06:23:56 +0200

    generic payload extraction service

--- eNMS/services/data_extraction/payload_regex_extraction.py
+++ eNMS/services/data_extraction/payload_regex_extraction.py
@@ -1,5 +1,7 @@
+from io import StringIO
 from re import findall
 from sqlalchemy import Boolean, Column, Float, ForeignKey, Integer, String, Text
+from textfsm import TextFSM
 from typing import Optional
 from wtforms import (
     BooleanField,
@@ -9,6 +11,7 @@ from wtforms import (
     SelectField,
     StringField,
 )
+from yaql import factory
 
 from eNMS.controller import controller
 from eNMS.database import LARGE_STRING_LENGTH, SMALL_STRING_LENGTH
@@ -17,51 +20,66 @@ from eNMS.models.automation import Job, Service
 from eNMS.models.inventory import Device
 
 
-class PayloadRegexExtractionService(Service):
+class PayloadExtractionService(Service):
 
-    __tablename__ = "PayloadRegexExtractionService"
+    __tablename__ = "PayloadExtractionService"
 
     id = Column(Integer, ForeignKey("Service.id"), primary_key=True)
     has_targets = True
     variable1 = Column(String(SMALL_STRING_LENGTH), default="")
-    regular_expression1 = Column(Text(LARGE_STRING_LENGTH), default="")
+    query1 = Column(String(SMALL_STRING_LENGTH), default="")
+    match_type1 = Column(String(SMALL_STRING_LENGTH), default="")
+    match1 = Column(Text(LARGE_STRING_LENGTH), default="")
     variable2 = Column(String(SMALL_STRING_LENGTH), default="")
-    regular_expression2 = Column(Text(LARGE_STRING_LENGTH), default="")
+    query2 = Column(String(SMALL_STRING_LENGTH), default="")
+    match_type2 = Column(String(SMALL_STRING_LENGTH), default="")
+    match2 = Column(Text(LARGE_STRING_LENGTH), default="")
     variable3 = Column(String(SMALL_STRING_LENGTH), default="")
-    regular_expression3 = Column(Text(LARGE_STRING_LENGTH), default="")
+    query3 = Column(String(SMALL_STRING_LENGTH), default="")
+    match_type3 = Column(String(SMALL_STRING_LENGTH), default="")
+    match3 = Column(Text(LARGE_STRING_LENGTH), default="")
 
-    __mapper_args__ = {"polymorphic_identity": "PayloadRegexExtractionService"}
+    __mapper_args__ = {"polymorphic_identity": "PayloadExtractionService"}
 
     def job(self, payload: dict, device: Device, parent: Optional[Job] = None) -> dict:
         result, success = {}, True
         for i in range(1, 4):
             variable = getattr(self, f"variable{i}")
-            regular_expression = getattr(self, f"regular_expression{i}")
             if not variable:
                 continue
-            command = self.sub(getattr(self, f"command{i}"), locals())
-            output = netmiko_handler.send_command(
-                command, delay_factor=self.delay_factor
-            )
-            match = findall(regular_expression, output)
-            if not match:
-                success = False
-            result[variable] = {
-                "command": command,
-                "regular_expression": regular_expression,
-                "output": output,
-                "value": match,
-            }
-        if not parent:
-            netmiko_handler.disconnect()
+            query = getattr(self, f"query{i}")
+            engine = factory.YaqlFactory().create()
+            value = engine(query).evaluate(data=payload)
+            match_type = getattr(self, f"match_type{i}")
+            match = getattr(self, f"match{i}")
+            result[variable] = {"query": query, "match_type": match_type}
+            if match_type == "none":
+                result["value"] = value
+            elif match_type == "regex":
+                result["value"] = findall(match, value)
+            else:
+                result["value"] = TextFSM(StringIO(match)).ParseText(value)
         return {"result": result, "success": True}
 
 
-class PayloadDataExtractionForm(ServiceForm):
-    form_type = HiddenField(default="PayloadRegexExtractionService")
+match_choices = (
+    ("none", "No post-processing"),
+    ("regex", "Regular Expression (findall)"),
+    ("textfsm", "TextFSM Template"),
+)
+
+
+class PayloadExtractionForm(ServiceForm):
+    form_type = HiddenField(default="PayloadExtractionService")
     variable1 = StringField()
-    regular_expression1 = StringField()
+    query1 = StringField()
+    match_type1 = SelectField(choices=match_choices)
+    match1 = StringField(widget=TextArea(), render_kw={"rows": 5})
     variable2 = StringField()
-    regular_expression2 = StringField()
+    query2 = StringField()
+    match_type2 = SelectField(choices=match_choices)
+    match2 = StringField(widget=TextArea(), render_kw={"rows": 5})
     variable3 = StringField()
-    regular_expression3 = StringField()
+    query3 = StringField()
+    match_type3 = SelectField(choices=match_choices)
+    match3 = StringField(widget=TextArea(), render_kw={"rows": 5})

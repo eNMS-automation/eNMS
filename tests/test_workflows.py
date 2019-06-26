6059c1bf6910ffe2fc179447ac1e85bb6c0b14b4
afourmy <antoine.fourmy@gmail.com>
2019-06-27 01:21:07 +0200

    fix napalm workflow

--- tests/test_workflows.py
+++ tests/test_workflows.py
@@ -1,12 +1,17 @@
 from flask.testing import FlaskClient
 
-# from eNMS.database.functions import fetch, fetch_all
+from eNMS.database.functions import fetch
 
 from tests.test_base import check_pages
 
 
 @check_pages("table/workflow")
-def test_workflow_creation(user_client: FlaskClient) -> None:
-    assert True
-    # for workflow in fetch_all("Workflow"):
-    #    assert netmiko_workflow.run()[0]["success"]
+def test_netmiko_workflow(user_client: FlaskClient) -> None:
+    workflow = fetch("Workflow", name="Netmiko_VRF_workflow")
+    assert workflow.run()[0]["success"]
+
+
+@check_pages("table/workflow")
+def test_extraction_validation_workflow(user_client: FlaskClient) -> None:
+    workflow = fetch("Workflow", name="payload_extraction_validation_worflow")
+    assert workflow.run()[0]["success"]

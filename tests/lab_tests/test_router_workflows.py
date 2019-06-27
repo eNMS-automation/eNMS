423d5bb84abbbbc1e43536e0ba3bc5550f2c6b4b
afourmy <antoine.fourmy@gmail.com>
2019-06-27 10:27:32 +0200

    split lab test from the rest

---
+++ tests/lab_tests/test_router_workflows.py
@@ -0,0 +1,35 @@
+from flask.testing import FlaskClient
+
+from eNMS.database.functions import fetch
+
+from tests.test_base import check_pages
+
+
+@check_pages("table/workflow")
+def test_configuration_workflow(user_client: FlaskClient) -> None:
+    workflow = fetch("Workflow", name="Configuration Management Workflow")
+    assert workflow.run()[0]["success"]
+
+
+@check_pages("table/workflow")
+def test_extraction_validation_workflow(user_client: FlaskClient) -> None:
+    workflow = fetch("Workflow", name="payload_extraction_validation_worflow")
+    assert workflow.run()[0]["success"]
+
+
+@check_pages("table/workflow")
+def test_netmiko_workflow(user_client: FlaskClient) -> None:
+    workflow = fetch("Workflow", name="Netmiko_VRF_workflow")
+    assert workflow.run()[0]["success"]
+
+
+@check_pages("table/workflow")
+def test_napalm_workflow(user_client: FlaskClient) -> None:
+    workflow = fetch("Workflow", name="Napalm_VRF_workflow")
+    assert workflow.run()[0]["success"]
+
+
+@check_pages("table/workflow")
+def test_payload_transfer_workflow(user_client: FlaskClient) -> None:
+    workflow = fetch("Workflow", name="payload_transfer_workflow")
+    assert workflow.run()[0]["success"]

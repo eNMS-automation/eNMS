a2666b30fe0c64b6dad54bdae43a43f68ac29f36
afourmy <antoine.fourmy@gmail.com>
2019-06-30 23:57:59 +0200

    dynamically load javascript for services

--- eNMS/static/base.js
+++ eNMS/static/base.js
@@ -129,6 +129,32 @@ $.ajaxSetup({
   },
 });
 
+const loadScript = (source, beforeEl, async = true, defer = true) => {
+  return new Promise((resolve, reject) => {
+    let script = document.createElement('script');
+    const prior = beforeEl || document.getElementsByTagName('script')[0];
+
+    script.async = async;
+    script.defer = defer;
+
+    function onloadHander(_, isAbort) {
+      if (isAbort || !script.readyState || /loaded|complete/.test(script.readyState)) {
+        script.onload = null;
+        script.onreadystatechange = null;
+        script = undefined;
+
+        if (isAbort) { reject(); } else { resolve(); }
+      }
+    }
+
+    script.onload = onloadHander;
+    script.onreadystatechange = onloadHander;
+
+    script.src = source;
+    prior.parentNode.insertBefore(script, prior);
+  });
+}
+
 /**
  * Open new tab at the provided URL.
  * @param {url} url - URL.
@@ -390,6 +416,11 @@ function showTypePanel(type, id, duplicate) {
       } else {
         panel.setHeaderTitle(`Create a New ${type}`);
       }
+      loadScript(`../static/services/${type}.js`).then(() => {
+        
+      }, () => {
+        console.log('fail to load script');
+      });
     },
     type,
     duplicate

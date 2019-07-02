ec9016f4cf21f5d796b50362ddd0cd576dc4a06e
afourmy <antoine.fourmy@gmail.com>
2019-07-02 22:53:28 +0200

    fix initial payload system

--- eNMS/concurrency.py
+++ eNMS/concurrency.py
@@ -14,7 +14,7 @@ def threaded_job(
     task = fetch("Task", allow_none=True, creation_time=aps_job_id or "")
     job = fetch("Job", id=job_id)
     origin = fetch("Job", allow_none=True, id=origin_id)
-    payload = payload or job.payload
+    payload = payload or job.initial_payload
     if targets:
         targets = {fetch("Device", id=device_id) for device_id in targets}
     job.run(targets=targets, payload=payload, task=task, origin=origin)

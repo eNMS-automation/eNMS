d637c95aab067dbafc7859e23884b0cae5688b36
afourmy <antoine.fourmy@gmail.com>
2019-07-03 12:00:00 +0200

    fix pretty names in the UI

--- eNMS/models/__init__.py
+++ eNMS/models/__init__.py
@@ -2,7 +2,6 @@ from collections import defaultdict
 from typing import Dict
 
 models: dict = {}
-relationships: defaultdict = defaultdict(dict)
-
 model_properties: defaultdict = defaultdict(list)
 property_types: Dict[str, str] = {}
+relationships: defaultdict = defaultdict(dict)

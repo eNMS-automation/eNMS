6425a5dbcf6778ee49025c99de47dc406d17fed4
afourmy <antoine.fourmy@gmail.com>
2019-07-04 18:46:26 +0200

    exclude edge from advanced search filtering

--- eNMS/forms/filtering.py
+++ eNMS/forms/filtering.py
@@ -14,6 +14,7 @@ def filtering_form_generator() -> None:
                 model.capitalize(), instance_type=relation["model"]
             )
             for model, relation in relationships[table_model].items()
+            if model != "edges"
         }
         type(
             f"{table.capitalize()}FilteringForm",

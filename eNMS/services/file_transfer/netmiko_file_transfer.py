0e84091693a226bc835e2add13940e9c99cfde90
afourmy <antoine.fourmy@gmail.com>
2019-08-07 22:47:58 +0200

    run getattr refacttoring p7

--- eNMS/services/file_transfer/netmiko_file_transfer.py
+++ eNMS/services/file_transfer/netmiko_file_transfer.py
@@ -35,8 +35,8 @@ class NetmikoFileTransferService(Service):
 
     def job(self, run: "Run", payload: dict, device: Device) -> dict:
         netmiko_connection = run.netmiko_connection(device)
-        source = run.sub(run["source_file"], locals())
-        destination = run.sub(run["destination_file"], locals())
+        source = run.sub(run.source_file, locals())
+        destination = run.sub(run.destination_file, locals())
         run.log("info", f"Transferring file {source} on {device.name}")
         transfer_dict = file_transfer(
             netmiko_connection,

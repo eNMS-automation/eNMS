2c8f03ec84347a5282dbf101e38a38557d7cfcde
afourmy <antoine.fourmy@gmail.com>
2019-08-19 00:58:26 +0200

    cascade delete

--- eNMS/models/inventory.py
+++ eNMS/models/inventory.py
@@ -85,7 +85,7 @@ class Device(CustomDevice):
     last_update = Column(SmallString, default="Never")
     last_runtime = Column(Float)
     jobs = relationship("Job", secondary=job_device_table, back_populates="devices")
-    results = relationship("Result", back_populates="device")
+    results = relationship("Result", back_populates="device", cascade="all, delete-orphan")
     tasks = relationship("Task", secondary=task_device_table, back_populates="devices")
     pools = relationship("Pool", secondary=pool_device_table, back_populates="devices")
 

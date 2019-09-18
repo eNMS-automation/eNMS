f07d70af9384216769786a4fb842f59fde0f4535
afourmy <antoine.fourmy@gmail.com>
2019-09-19 00:56:43 +0200

    add unix shell script service

--- eNMS/services/unix/unix_shell_script_service.py
+++ eNMS/services/unix/unix_shell_script_service.py
@@ -1,12 +1,12 @@
-from fabric import Connection
-from io import StringIO
-from sqlalchemy import Boolean, ForeignKey, Integer
-from wtforms import HiddenField, StringField, BooleanField
+from sqlalchemy import Boolean, Float, ForeignKey, Integer
+from wtforms import HiddenField, StringField, BooleanField, IntegerField, SelectField, FloatField
 from wtforms.widgets import TextArea
+from eNMS import app
 
-from eNMS.database.dialect import Column, LargeString
+from eNMS.database.dialect import Column, LargeString, SmallString
 from eNMS.forms.automation import ServiceForm
-from eNMS.forms.services import StringValidationForm
+from eNMS.forms.services import StringValidationForm, NetmikoForm
+from eNMS.forms.fields import SubstitutionField
 from eNMS.models.automation import Service
 from eNMS.models.execution import Run
 from eNMS.models.inventory import Device
@@ -24,55 +24,106 @@ class UnixShellScriptService(Service):
     negative_logic = Column(Boolean, default=False)
     delete_spaces_before_matching = Column(Boolean, default=False)
     privileged_mode = Column(Boolean, default=False)
+    driver = Column(SmallString)
+    use_device_driver = Column(Boolean, default=True)
+    fast_cli = Column(Boolean, default=False)
+    timeout = Column(Integer, default=10.0)
+    delay_factor = Column(Float, default=1.0)
+    global_delay_factor = Column(Float, default=1.0)
+    expect_string = Column(SmallString)
+    auto_find_prompt = Column(Boolean, default=True)
+    strip_prompt = Column(Boolean, default=True)
+    strip_command = Column(Boolean, default=True)
 
     __mapper_args__ = {"polymorphic_identity": "UnixShellScriptService"}
 
     def job(self, run: "Run", payload: dict, device: Device) -> dict:
-        username, password = run.get_credentials(device)
-        fabric_connection = Connection(
-            host=device.ip_address,
-            port=device.port,
-            user=username,
-            connect_kwargs={"password": password},
-        )
+        netmiko_connection = run.netmiko_connection(device)
         source_code = run.sub(run.source_code, locals())
+        script_file_name = f"{self.name}.sh"
+        run.log("info", f"Sending shell script '{script_file_name}' to run on {device.name} (Netmiko)")
+        expect_string = run.sub(run.expect_string, locals())
+        netmiko_connection.send_command(
+            f"echo '{source_code}' > '{script_file_name}'",
+            delay_factor=run.delay_factor,
+            expect_string=run.expect_string or None,
+            auto_find_prompt=run.auto_find_prompt,
+            strip_prompt=run.strip_prompt,
+            strip_command=run.strip_command,
+        )
+        result = netmiko_connection.send_command(
+            f"bash ./{script_file_name}",
+            delay_factor=run.delay_factor,
+            expect_string=run.expect_string or None,
+            auto_find_prompt=run.auto_find_prompt,
+            strip_prompt=run.strip_prompt,
+            strip_command=run.strip_command,
+        )
+        return_code = netmiko_connection.send_command(
+            f"echo $?",
+            delay_factor=run.delay_factor,
+            expect_string=run.expect_string or None,
+            auto_find_prompt=run.auto_find_prompt,
+            strip_prompt=run.strip_prompt,
+            strip_command=run.strip_command,
+        )
+        netmiko_connection.send_command(
+            f"rm -f '{script_file_name}'",
+            delay_factor=run.delay_factor,
+            expect_string=run.expect_string or None,
+            auto_find_prompt=run.auto_find_prompt,
+            strip_prompt=run.strip_prompt,
+            strip_command=run.strip_command,
+        )
         match = run.sub(run.content_match, locals())
-        run.log("info", f"Running Unix Shell Script {self.name} on {device.name}")
-        script_file_name = "unix_shell_script_service.sh"
-        with StringIO(run.source_code) as script_file:
-            fabric_connection.put(script_file, script_file_name)
-            if run.privileged_mode:
-                if not device.enable_password:
-                    raise Exception(
-                        f"Service {self.name} requested privileged mode on device "
-                        f"with no configured enable_password: {device.name}"
-                    )
-                result = fabric_connection.sudo(
-                    f"bash {script_file_name}", password=device.enable_password
-                )
-            else:
-                result = fabric_connection.run(f"bash {script_file_name}")
-            fabric_connection.run(f"rm {script_file_name}")
         return {
             "match": match,
             "negative_logic": run.negative_logic,
-            "result": f"stdout:\n{result.stdout}\nstderr:\n{result.stderr}",
-            "success": result.ok and run.match_content(result, match),
+            "return_code": return_code,
+            "result": result,
+            "success": (return_code == "0") and run.match_content(result, match),
         }
 
-
-class UnixShellScriptForm(ServiceForm, StringValidationForm):
+class UnixShellScriptForm(ServiceForm, StringValidationForm, NetmikoForm):
     form_type = HiddenField(default="UnixShellScriptService")
     privileged_mode = BooleanField("Privileged mode (run as root using sudo)")
     source_code = StringField(
         widget=TextArea(),
         render_kw={"rows": 15},
         default=(
-            "# The following example shell script returns "
-            "0 for success; non-zero for failure\n"
             "#!/bin/bash\n"
+            "# The following example shell script returns 0 for success; non-zero for failure\n"
             "directory_contents=`ls -al /root`  # Needs privileged mode\n"
-            "echo $directory_contents\n"
-            "exit 0 # Success\n"
+            "return_code=$?\n"
+            "if [ $return_code -ne 0 ]; then\n"
+            "    exit $return_code  # Indicating Failure\n"
+            "else\n"
+            "    echo -e \"$directory_contents\"\n"
+            "    exit 0  # Indicating Success\n"
+            "fi\n"
         ),
     )
+    driver = SelectField(choices=app.NETMIKO_DRIVERS, default="linux")
+    use_device_driver = BooleanField(default=True)
+    fast_cli = BooleanField()
+    timeout = IntegerField(default=10)
+    delay_factor = FloatField(default=1.0)
+    global_delay_factor = FloatField(default=1.0)
+    expect_string = SubstitutionField()
+    auto_find_prompt = BooleanField(default=True)
+    strip_prompt = BooleanField(default=True)
+    strip_command = BooleanField(default=True)
+    groups = {
+        "Main Parameters": {"commands": ["source_code"], "default": "expanded"},
+        "Netmiko Parameters": NetmikoForm.group,
+        "Advanced Netmiko Parameters": {
+            "commands": [
+                "expect_string",
+                "auto_find_prompt",
+                "strip_prompt",
+                "strip_command",
+            ],
+            "default": "hidden",
+        },
+        "Validation Parameters": StringValidationForm.group,
+    }

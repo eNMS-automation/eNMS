6d247a5ecdd905247ba3e7395bee887d2e6d4bd4
afourmy <antoine.fourmy@gmail.com>
2019-10-28 07:30:26 +0100

    improve flask config

--- eNMS/framework/config.py
+++ eNMS/framework/config.py
@@ -4,18 +4,16 @@ from os import environ
 class DebugConfig:
     MODE = "debug"
     DEBUG = True
-    JSON_SORT_KEYS = False
     SECRET_KEY = environ.get("SECRET_KEY", "get-a-real-key")
     WTF_CSRF_TIME_LIMIT = None
     ERROR_404_HELP = False
     MAX_CONTENT_LENGTH = 20 * 1024 * 1024
 
 
-class ProductionConfig:
+class ProductionConfig(DebugConfig):
     MODE = "production"
     DEBUG = False
     SECRET_KEY = environ.get("SECRET_KEY")
-    WTF_CSRF_TIME_LIMIT = None
 
 
 class TestConfig(DebugConfig):

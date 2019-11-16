6a4084f8222c18f1023ccb3218e849aa35491a81
Antoine Fourmy <antoine.fourmy@gmail.com>
2019-11-16 15:03:31 -0500

    add unix shell script service js

---
+++ eNMS/static/services/unix_shell_script_service.js
@@ -0,0 +1,20 @@
+/*
+global
+call: false
+*/
+
+/**
+ * script to handle mutual exclusion of auto_find_prompt and expect_string
+ * @param {id} id - Service id.
+ */
+// eslint-disable-next-line
+
+function job(id) {
+  const auto_find_prompt_fieldId = id ? `auto_find_prompt-${id}` : "auto_find_prompt";
+  const auto_find_prompt_field = $(`#unix_shell_script_service-${auto_find_prompt_fieldId}`);
+  const expect_string_fieldId = id ? `expect_string-${id}` : "expect_string";
+  const expect_string_field = $(`#unix_shell_script_service-${expect_string_fieldId}`);
+  auto_find_prompt_field.on("change", function() {
+    expect_string_field.prop("disabled", this.checked);
+  }).change();
+}

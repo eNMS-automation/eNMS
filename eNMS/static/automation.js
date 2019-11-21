f540dccbcdbb94881e2b5a73a5c7f43f9fa37e8d
Antoine Fourmy <antoine.fourmy@gmail.com>
2019-11-21 10:19:33 -0500

    calendar refactoring p2

--- eNMS/static/automation.js
+++ eNMS/static/automation.js
@@ -347,39 +347,41 @@ function field(name, type, id) {
 }
 
 function displayCalendar(calendarType) {
-  if (typeof $.fn.fullCalendar === "undefined") {
-    return;
-  }
-  call(`/calendar_init/${calendarType}`, function(tasks) {
-    let events = [];
-    for (const [name, properties] of Object.entries(tasks)) {
-      if (properties.service === undefined) continue;
-      events.push({
-        title: name,
-        id: properties.id,
-        description: properties.description,
-        start: new Date(...properties.start),
-        runtime: properties.runtime,
-        service: properties.service,
-      });
+  showPanel("display", calendarType, () => {
+    if (typeof $.fn.fullCalendar === "undefined") {
+      return;
     }
-    $("#calendar").fullCalendar({
-      header: {
-        left: "prev,next today",
-        center: "title",
-        right: "month,agendaWeek,agendaDay,listMonth",
-      },
-      selectable: true,
-      selectHelper: true,
-      eventClick: function(e) {
-        if (calendarType == "task") {
-          showTypePanel("task", e.id);
-        } else {
-          showRuntimePanel("results", e.service, e.title, e.runtime);
-        }
-      },
-      editable: true,
-      events: events,
+    call(`/calendar_init/${calendarType}`, function(tasks) {
+      let events = [];
+      for (const [name, properties] of Object.entries(tasks)) {
+        if (properties.service === undefined) continue;
+        events.push({
+          title: name,
+          id: properties.id,
+          description: properties.description,
+          start: new Date(...properties.start),
+          runtime: properties.runtime,
+          service: properties.service,
+        });
+      }
+      $(`#content-${calendarType}`).fullCalendar({
+        header: {
+          left: "prev,next today",
+          center: "title",
+          right: "month,agendaWeek,agendaDay,listMonth",
+        },
+        selectable: true,
+        selectHelper: true,
+        eventClick: function(e) {
+          if (calendarType == "task") {
+            showTypePanel("task", e.id);
+          } else {
+            showRuntimePanel("results", e.service, e.title, e.runtime);
+          }
+        },
+        editable: true,
+        events: events,
+      });
     });
   });
 }

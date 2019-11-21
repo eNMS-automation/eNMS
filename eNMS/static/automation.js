ac3d5a9a934600743d20b63974ddcdfda4959981
Antoine Fourmy <antoine.fourmy@gmail.com>
2019-11-21 10:16:11 -0500

    calendar refactoring p1

--- eNMS/static/automation.js
+++ eNMS/static/automation.js
@@ -346,6 +346,44 @@ function field(name, type, id) {
   return $(`#${fieldId}`);
 }
 
+function displayCalendar(calendarType) {
+  if (typeof $.fn.fullCalendar === "undefined") {
+    return;
+  }
+  call(`/calendar_init/${calendarType}`, function(tasks) {
+    let events = [];
+    for (const [name, properties] of Object.entries(tasks)) {
+      if (properties.service === undefined) continue;
+      events.push({
+        title: name,
+        id: properties.id,
+        description: properties.description,
+        start: new Date(...properties.start),
+        runtime: properties.runtime,
+        service: properties.service,
+      });
+    }
+    $("#calendar").fullCalendar({
+      header: {
+        left: "prev,next today",
+        center: "title",
+        right: "month,agendaWeek,agendaDay,listMonth",
+      },
+      selectable: true,
+      selectHelper: true,
+      eventClick: function(e) {
+        if (calendarType == "task") {
+          showTypePanel("task", e.id);
+        } else {
+          showRuntimePanel("results", e.service, e.title, e.runtime);
+        }
+      },
+      editable: true,
+      events: events,
+    });
+  });
+}
+
 Object.assign(action, {
   Edit: (service) => showTypePanel(service.type, service.id),
   Duplicate: (service) => showTypePanel(service.type, service.id, "duplicate"),

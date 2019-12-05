52df0bdb411086b0fb0b6d49fe6fec65be8090b9
Antoine Fourmy <antoine.fourmy@gmail.com>
2019-12-05 15:46:01 -0500

    fix service renaming passwords update p2

--- eNMS/database/events.py
+++ eNMS/database/events.py
@@ -78,5 +78,12 @@ def configure_events(app):
             app.log("info", f"UPDATE: {target.type} '{name}': ({changes})")
 
     @event.listens_for(models["service"].name, 'set', propagate=True)
-    def vault_update(target, value, old_value, initiator):
-        print("ttt"*200, target, value, old_value)
+    def vault_update(target, new_value, old_value, *_):
+        path = f"secret/data/{target.type}/{old_value}/password"
+        data = app.vault_client.read(path)
+        if not data:
+            return
+        app.vault_client.write(
+            f"secret/data/{target.type}/{new_value}/password",
+            data={"password": data["data"]["data"]["password"]},
+        )

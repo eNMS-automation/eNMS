436f8ef6dfcbcdeafe6d16a1965366b33acd3810
Antoine Fourmy <antoine.fourmy@gmail.com>
2019-12-21 09:59:43 -0500

    table front end rendering refactoring p78

--- eNMS/properties/table.py
+++ eNMS/properties/table.py
@@ -1,32 +1,4 @@
 from eNMS.properties.objects import object_common_properties
 
-device_table_properties = object_common_properties + [
-    "operating_system",
-    "os_version",
-    "ip_address",
-    "port",
-]
-
-link_table_properties = object_common_properties + ["source_name", "destination_name"]
-
-pool_table_properties = [
-    "name",
-    "last_modified",
-    "description",
-    "never_update",
-    "longitude",
-    "latitude",
-    "object_number",
-]
-
-service_table_properties = [
-    "name",
-    "last_modified",
-    "type",
-    "description",
-    "vendor",
-    "operating_system",
-    "creator",
-]
 
 event_table_properties = ["name", "service_name", "log_source", "log_content"]

849082acf88c817979e72bd31015a822864c25d2
aaron-messick <57632432+aaron-messick@users.noreply.github.com>
2020-01-13 15:49:05 -0600

    Added groups and labels

--- eNMS/services/operational_data/data_backup_service.py
+++ eNMS/services/operational_data/data_backup_service.py
@@ -4,7 +4,6 @@ from pathlib import Path
 from re import M, sub
 from sqlalchemy import Boolean, Float, ForeignKey, Integer
 from wtforms import FieldList, FormField, HiddenField, StringField
-from wtforms.widgets import TextArea
 
 from eNMS.database.dialect import Column, MutableList, LargeString, SmallString
 from eNMS.forms.automation import NetmikoForm
@@ -14,7 +13,7 @@ from eNMS.models.automation import ConnectionService
 class DataBackupService(ConnectionService):
 
     __tablename__ = "data_backup_service"
-    pretty_name = "Operational Data Backup"
+    pretty_name = "Netmiko Operational Data Backup"
     parent_type = "connection_service"
     id = Column(Integer, ForeignKey("connection_service.id"), primary_key=True)
     enable_mode = Column(Boolean, default=True)
@@ -25,7 +24,7 @@ class DataBackupService(ConnectionService):
     timeout = Column(Integer, default=10.0)
     global_delay_factor = Column(Float, default=1.0)
     configuration = Column(LargeString)
-    operational_data = Column(LargeString)
+    operational_data = Column(MutableList)
     replacements = Column(MutableList)
 
     __mapper_args__ = {"polymorphic_identity": "data_backup_service"}
@@ -38,13 +37,22 @@ class DataBackupService(ConnectionService):
             netmiko_connection = run.netmiko_connection(device)
             run.log("info", "Fetching Operational Data", device)
             for data in ("configuration", "operational_data"):
-                commands = run.sub(getattr(self, data), locals()).splitlines()
-                result = "\n\n".join(
-                    f"{command}:\n{netmiko_connection.send_command(command)}"
-                    for command in commands
-                )
-                for r in self.replacements:
-                    result = sub(r["pattern"], r["replace_with"], result, flags=M)
+                value = run.sub(getattr(self, data), locals())
+                if data == "configuration":
+                    result = netmiko_connection.send_command(value)
+                    for r in self.replacements:
+                        result = sub(r["pattern"], r["replace_with"], result, flags=M)
+                else:
+                    result = f"\n\n".join(
+                        f"{cmd['command']}\n"
+                        + "\n".join(
+                            f"{cmd['prefix']} - {line}" if cmd["prefix"] else line
+                            for line in netmiko_connection.send_command(
+                                cmd["command"]
+                            ).splitlines()
+                        )
+                        for cmd in value
+                    )
                 setattr(device, data, result)
                 with open(path / data, "w") as file:
                     file.write(result)
@@ -67,14 +75,27 @@ class ReplacementForm(FlaskForm):
     replace_with = StringField("Replace With")
 
 
+class OperationalDataForm(FlaskForm):
+    command = StringField("Result of this Command stored as Operational Data")
+    prefix = StringField("Label for this Command (Prefix)")
+
+
 class DataBackupForm(NetmikoForm):
     form_type = HiddenField(default="data_backup_service")
-    configuration = StringField(widget=TextArea(), render_kw={"rows": 2})
-    operational_data = StringField(widget=TextArea(), render_kw={"rows": 6})
+    configuration = StringField("Result of this Command stored as Configuration")
+    operational_data = FieldList(FormField(OperationalDataForm), min_entries=3)
     replacements = FieldList(FormField(ReplacementForm), min_entries=3)
     groups = {
-        "Main Parameters": {
-            "commands": ["configuration", "operational_data", "replacements"],
+        "Create Configuration File": {
+            "commands": ["configuration"],
+            "default": "expanded",
+        },
+        "Create Operational Data File": {
+            "commands": ["operational_data"],
+            "default": "expanded",
+        },
+        "Search Response & Replace": {
+            "commands": ["replacements"],
             "default": "expanded",
         },
         **NetmikoForm.groups,

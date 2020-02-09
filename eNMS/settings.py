44977c3b8458a846a57d792d7d9d6882c9601497
Antoine Fourmy <antoine.fourmy@gmail.com>
2020-02-09 07:48:03 -0500

    create dedicated settings folder with all json file

--- eNMS/settings.py
+++ eNMS/settings.py
@@ -1,14 +1,14 @@
 from pathlib import Path
 from json import load
 
-with open(Path.cwd() / "settings.json", "r") as file:
+with open(Path.cwd() / "settings" / "app.json", "r") as file:
     settings = load(file)
 
-with open(Path.cwd() / "properties.json", "r") as file:
+with open(Path.cwd() / "settings" / "tables.json", "r") as file:
     table_properties = load(file)
 
-with open(Path.cwd() / "dashboard.json", "r") as file:
+with open(Path.cwd() / "settings" / "dashboard.json", "r") as file:
     dashboard_properties = load(file)
 
-with open(Path.cwd() / "pools.json", "r") as file:
+with open(Path.cwd() / "settings" / "pools.json", "r") as file:
     pool_properties = load(file)

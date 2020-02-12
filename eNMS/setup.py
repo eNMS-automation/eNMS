366a56c533242a652434d73d175045622abe4a21
Antoine Fourmy <antoine.fourmy@gmail.com>
2020-02-12 05:30:03 -0500

    add settings and rbac to setup folder

--- eNMS/setup.py
+++ eNMS/setup.py
@@ -1,10 +1,10 @@
 from pathlib import Path
 from json import load
 
-with open(Path.cwd() / "settings.json", "r") as file:
+with open(Path.cwd() / "setup" / "settings.json", "r") as file:
     settings = load(file)
 
-with open(Path.cwd() / "rbac.json", "r") as file:
+with open(Path.cwd() / "setup" / "rbac.json", "r") as file:
     rbac = load(file)
 
 with open(Path.cwd() / "setup" / "properties.json", "r") as file:

1e448e4602c03fbda30fab181b49f336fb9b2575
Antoine Fourmy <antoine.fourmy@gmail.com>
2020-03-05 11:28:39 -0500

    make log per service

--- eNMS/models/automation.py
+++ eNMS/models/automation.py
@@ -442,12 +442,7 @@ class Run(AbstractBase):
                 "run": self.properties,
                 "service": self.service.get_properties(exclude=["positions"]),
             }
-            if (
-                self.runtime == self.parent_runtime
-                or len(self.devices) > 1
-                or self.run_method == "once"
-            ):
-                self.create_result(results)
+            self.create_result(results)
             Session.commit()
         return results
 
@@ -640,7 +635,7 @@ class Run(AbstractBase):
         if device:
             log += f" - DEVICE {device if isinstance(device, str) else device.name}"
         log += f" : {content}"
-        app.run_logs[self.parent_runtime].append(log)
+        app.run_logs[self.runtime].append(log)
 
     def build_notification(self, results):
         notification = {

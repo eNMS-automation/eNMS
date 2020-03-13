92aae4ada0366d29e701c7452d967eeea58f85c7
Antoine Fourmy <antoine.fourmy@gmail.com>
2020-03-13 05:13:02 -0400

    fix logging mechanism

--- eNMS/controller/automation.py
+++ eNMS/controller/automation.py
@@ -129,7 +129,7 @@ class AutomationController(BaseController):
         if log_instance:
             log = log_instance.content
         else:
-            log = "\n".join(self.run_logs[runtime].get(service, []))
+            log = "\n".join(self.run_logs[runtime].get(int(service), []))
         return {"logs": log, "refresh": not log_instance}
 
     def get_runtimes(self, type, id):

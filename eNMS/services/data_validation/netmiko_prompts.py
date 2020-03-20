b8534b65c7199e0fcf3db1731a9170777443e48d
Marc Elewitz <elewitz.marc@gmail.com>
2020-03-20 10:46:00 -0500

    Added jumpserver support to netmiko validation and prompts

--- eNMS/services/data_validation/netmiko_prompts.py
+++ eNMS/services/data_validation/netmiko_prompts.py
@@ -1,9 +1,9 @@
 from sqlalchemy import Boolean, Float, ForeignKey, Integer
 from traceback import format_exc
-from wtforms import HiddenField
+from wtforms import BooleanField, HiddenField
 
 from eNMS.database.dialect import Column, LargeString, SmallString
-from eNMS.forms.fields import SubstitutionField
+from eNMS.forms.fields import PasswordSubstitutionField, SubstitutionField
 from eNMS.forms.automation import NetmikoForm
 from eNMS.models.automation import ConnectionService
 
@@ -29,11 +29,27 @@ class NetmikoPromptsService(ConnectionService):
     timeout = Column(Integer, default=10.0)
     delay_factor = Column(Float, default=1.0)
     global_delay_factor = Column(Float, default=1.0)
+    use_jumpserver = Column(Boolean)
+    jump_command = Column(SmallString)
+    jump_username = Column(SmallString)
+    jump_password = Column(SmallString)
+    exit_command = Column(SmallString)
+    expect_username_prompt = Column(SmallString)
+    expect_password_prompt = Column(SmallString)
+    expect_prompt = Column(SmallString)
+    expect_string = Column(SmallString)
 
     __mapper_args__ = {"polymorphic_identity": "netmiko_prompts_service"}
 
     def job(self, run, payload, device):
         netmiko_connection = run.netmiko_connection(device)
+        netmiko_connection.session_log.truncate(0)
+
+        if self.use_jumpserver:
+            jumpserver_prompt, error = self.enter_jumpserver(run, netmiko_connection)
+            if error:
+                return error
+
         send_strings = (run.command, run.response1, run.response2, run.response3)
         expect_strings = (run.confirmation1, run.confirmation2, run.confirmation3, None)
         commands = []
@@ -65,7 +81,85 @@ class NetmikoPromptsService(ConnectionService):
                     {"success": False, "result": result, "match": confirmation}
                 )
                 return results
+
+        if self.use_jumpserver:
+            self.exit_jumpserver(run, netmiko_connection, jumpserver_prompt)
+
         return {"commands": commands, "result": result}
+
+    def enter_jumpserver(self, run, netmiko_connection):
+        jump_command = run.sub(run.jump_command, locals())
+        jump_username = run.sub(run.jump_username, locals())
+        jump_password = run.sub(run.jump_password, locals())
+        expect_username_prompt = run.sub(run.expect_username_prompt, locals())
+        expect_password_prompt = run.sub(run.expect_password_prompt, locals())
+        expect_prompt = run.sub(run.expect_prompt, locals())
+
+        if (
+            expect_username_prompt
+            and not jump_username
+            or expect_password_prompt
+            and not jump_password
+        ):
+            raise Exception(
+                "Jumpserver username/password is required when a username/password prompt is specified"
+            )
+
+        # Grab prompt from jumpserver for use after exit from device
+        netmiko_connection.find_prompt()
+        jumpserver_prompt = netmiko_connection.base_prompt
+
+        send_strings = (
+            text
+            for text in (jump_command, jump_username, jump_password)
+            if text not in (None, "")
+        )
+        expect_strings = (
+            text
+            for text in (expect_username_prompt, expect_password_prompt, expect_prompt)
+            if text not in (None, "")
+        )
+
+        for send_string, expect_string in zip(send_strings, expect_strings):
+            if not (send_string and expect_string):
+                return jumpserver_prompt, None
+            try:
+                netmiko_connection.send_command(
+                    send_string,
+                    expect_string=expect_string,
+                    auto_find_prompt=False,
+                    strip_prompt=False,
+                    strip_command=True,
+                    max_loops=150,  # 150 loops * .2 loop_delay = 30 second timeout
+                )
+            except Exception:
+                return (
+                    None,
+                    {
+                        "error": format_exc(),
+                        "result": (
+                            netmiko_connection.session_log.getvalue().decode()
+                            if hasattr(netmiko_connection, "session_log")
+                            else "No results available after exception. Driver does not support session_log"
+                        ),
+                        "message": f"Sent '{send_string}', waiting for '{expect_string}'",
+                        "success": False,
+                    },
+                )
+
+        return jumpserver_prompt, None
+
+    def exit_jumpserver(self, run, netmiko_connection, jumpserver_prompt):
+        exit_command = run.sub(run.exit_command, locals())
+
+        # Send exit command, wait for device prompt
+        netmiko_connection.send_command(
+            exit_command,
+            expect_string=jumpserver_prompt or None,
+            auto_find_prompt=True,
+            strip_prompt=False,
+            strip_command=True,
+        )
 
 
 class NetmikoPromptsForm(NetmikoForm):
@@ -77,6 +171,41 @@ class NetmikoPromptsForm(NetmikoForm):
     response2 = SubstitutionField()
     confirmation3 = SubstitutionField()
     response3 = SubstitutionField()
+
+    # Need to add javascript to let service editor choose a profile and
+    # populate each field from the chosen profile.
+    JUMPSERVER_DEFAULT_PROFILES = {
+        "Cisco Calvados": {
+            "jump_command": "admin",
+            "exit_command": "exit",
+            "expect_username_prompt": "Admin Username:",
+            "expect_password_prompt": "Password:",
+            "expect_prompt": "sysadmin-vm:.*#",
+        },
+    }
+
+    defaults = JUMPSERVER_DEFAULT_PROFILES["Cisco Calvados"]
+
+    use_jumpserver = BooleanField("Use Jump Server", default=False)
+    jump_command = SubstitutionField(
+        label="Command that jumps to device", default=defaults["jump_command"]
+    )
+    jump_username = SubstitutionField(label="Device username")
+    jump_password = PasswordSubstitutionField(label="Device jump_password")
+    exit_command = SubstitutionField(
+        label="Command to exit device back to jump server",
+        default=defaults["exit_command"],
+    )
+    expect_username_prompt = SubstitutionField(
+        "Expected username prompt", default=defaults["expect_username_prompt"]
+    )
+    expect_password_prompt = SubstitutionField(
+        "Expected password prompt", default=defaults["expect_password_prompt"]
+    )
+    expect_prompt = SubstitutionField(
+        "Expected prompt after login", default=defaults["expect_prompt"]
+    )
+
     groups = {
         "Main Parameters": {
             "commands": [
@@ -89,6 +218,19 @@ class NetmikoPromptsForm(NetmikoForm):
                 "response3",
             ],
             "default": "expanded",
+        },
+        "Jump Server Parameters": {
+            "commands": [
+                "use_jumpserver",
+                "jump_command",
+                "expect_username_prompt",
+                "jump_username",
+                "expect_password_prompt",
+                "jump_password",
+                "expect_prompt",
+                "exit_command",
+            ],
+            "default": "hidden",
         },
         **NetmikoForm.groups,
     }
--- eNMS/services/data_validation/netmiko_validation.py
+++ eNMS/services/data_validation/netmiko_validation.py
@@ -3,7 +3,7 @@ from traceback import format_exc
 from wtforms import BooleanField, HiddenField
 
 from eNMS.database.dialect import Column, LargeString, SmallString
-from eNMS.forms.fields import SubstitutionField
+from eNMS.forms.fields import PasswordSubstitutionField, SubstitutionField
 from eNMS.forms.automation import NetmikoForm
 from eNMS.models.automation import ConnectionService
 
@@ -23,6 +23,14 @@ class NetmikoValidationService(ConnectionService):
     timeout = Column(Float, default=10.0)
     delay_factor = Column(Float, default=1.0)
     global_delay_factor = Column(Float, default=1.0)
+    use_jumpserver = Column(Boolean)
+    jump_command = Column(SmallString)
+    jump_username = Column(SmallString)
+    jump_password = Column(SmallString)
+    exit_command = Column(SmallString)
+    expect_username_prompt = Column(SmallString)
+    expect_password_prompt = Column(SmallString)
+    expect_prompt = Column(SmallString)
     expect_string = Column(SmallString)
     auto_find_prompt = Column(Boolean, default=True)
     strip_prompt = Column(Boolean, default=True)
@@ -33,9 +41,16 @@ class NetmikoValidationService(ConnectionService):
 
     def job(self, run, payload, device):
         netmiko_connection = run.netmiko_connection(device)
+
+        if self.use_jumpserver:
+            jumpserver_prompt, error = self.enter_jumpserver(run, netmiko_connection)
+            if error:
+                return error
+
         command = run.sub(run.command, locals())
         run.log("info", f"Sending '{command}' with Netmiko", device)
         expect_string = run.sub(run.expect_string, locals())
+
         netmiko_connection.session_log.truncate(0)
         try:
             result = netmiko_connection.send_command(
@@ -54,12 +69,124 @@ class NetmikoValidationService(ConnectionService):
                 "result": netmiko_connection.session_log.getvalue().decode(),
                 "success": False,
             }
+
+        if self.use_jumpserver:
+            self.exit_jumpserver(run, netmiko_connection, jumpserver_prompt)
+
         return {"command": command, "result": result}
+
+    def enter_jumpserver(self, run, netmiko_connection):
+        jump_command = run.sub(run.jump_command, locals())
+        jump_username = run.sub(run.jump_username, locals())
+        jump_password = run.sub(run.jump_password, locals())
+        expect_username_prompt = run.sub(run.expect_username_prompt, locals())
+        expect_password_prompt = run.sub(run.expect_password_prompt, locals())
+        expect_prompt = run.sub(run.expect_prompt, locals())
+
+        if (
+            expect_username_prompt
+            and not jump_username
+            or expect_password_prompt
+            and not jump_password
+        ):
+            raise Exception(
+                "Jumpserver username/password is required when a username/password prompt is specified"
+            )
+
+        # Grab prompt from jumpserver for use after exit from device
+        netmiko_connection.find_prompt()
+        jumpserver_prompt = netmiko_connection.base_prompt
+
+        send_strings = (
+            text
+            for text in (jump_command, jump_username, jump_password)
+            if text not in (None, "")
+        )
+        expect_strings = (
+            text
+            for text in (expect_username_prompt, expect_password_prompt, expect_prompt)
+            if text not in (None, "")
+        )
+
+        for send_string, expect_string in zip(send_strings, expect_strings):
+            if not (send_string and expect_string):
+                return jumpserver_prompt, None
+            try:
+                netmiko_connection.send_command(
+                    send_string,
+                    expect_string=expect_string,
+                    auto_find_prompt=False,
+                    strip_prompt=False,
+                    strip_command=True,
+                    max_loops=150,  # 150 loops * .2 loop_delay = 30 second timeout
+                )
+            except Exception:
+                return (
+                    None,
+                    {
+                        "error": format_exc(),
+                        "result": (
+                            netmiko_connection.session_log.getvalue().decode()
+                            if hasattr(netmiko_connection, "session_log")
+                            else "No results available after exception. Driver does not support session_log"
+                        ),
+                        "message": f"Sent '{send_string}', waiting for '{expect_string}'",
+                        "success": False,
+                    },
+                )
+
+        return jumpserver_prompt, None
+
+    def exit_jumpserver(self, run, netmiko_connection, jumpserver_prompt):
+        exit_command = run.sub(run.exit_command, locals())
+
+        # Send exit command, wait for device prompt
+        netmiko_connection.send_command(
+            exit_command,
+            expect_string=jumpserver_prompt or None,
+            auto_find_prompt=True,
+            strip_prompt=False,
+            strip_command=True,
+        )
 
 
 class NetmikoValidationForm(NetmikoForm):
     form_type = HiddenField(default="netmiko_validation_service")
     command = SubstitutionField()
+
+    # Need to add javascript to let service editor choose a profile and
+    # populate each field from the chosen profile.
+    JUMPSERVER_DEFAULT_PROFILES = {
+        "Cisco Calvados": {
+            "jump_command": "admin",
+            "exit_command": "exit",
+            "expect_username_prompt": "Admin Username:",
+            "expect_password_prompt": "Password:",
+            "expect_prompt": "sysadmin-vm:.*#",
+        },
+    }
+
+    defaults = JUMPSERVER_DEFAULT_PROFILES["Cisco Calvados"]
+
+    use_jumpserver = BooleanField("Use Jump Server", default=False)
+    jump_command = SubstitutionField(
+        label="Command that jumps to device", default=defaults["jump_command"]
+    )
+    jump_username = SubstitutionField(label="Device username")
+    jump_password = PasswordSubstitutionField(label="Device password")
+    exit_command = SubstitutionField(
+        label="Command to exit device back to jump server",
+        default=defaults["exit_command"],
+    )
+    expect_username_prompt = SubstitutionField(
+        "Expected username prompt", default=defaults["expect_username_prompt"]
+    )
+    expect_password_prompt = SubstitutionField(
+        "Expected password prompt", default=defaults["expect_password_prompt"]
+    )
+    expect_prompt = SubstitutionField(
+        "Expected prompt after login", default=defaults["expect_prompt"]
+    )
     expect_string = SubstitutionField()
     auto_find_prompt = BooleanField(default=True)
     strip_prompt = BooleanField(default=True)
@@ -74,6 +201,19 @@ class NetmikoValidationForm(NetmikoForm):
                 "strip_prompt",
                 "strip_command",
                 "use_genie",
+            ],
+            "default": "hidden",
+        },
+        "Jump Server Parameters": {
+            "commands": [
+                "use_jumpserver",
+                "jump_command",
+                "expect_username_prompt",
+                "jump_username",
+                "expect_password_prompt",
+                "jump_password",
+                "expect_prompt",
+                "exit_command",
             ],
             "default": "hidden",
         },

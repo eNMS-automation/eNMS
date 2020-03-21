cdd6a77f373795d48aeb128ae9030a2c8e4d3161
Antoine Fourmy <antoine.fourmy@gmail.com>
2020-03-21 03:45:08 -0400

    flask refactoring p2

--- eNMS/framework/__init__.py
+++ eNMS/framework/__init__.py
@@ -1,12 +1,14 @@
+from click import argument, echo, option
+from json import loads
 from passlib.hash import argon2
 from flask import Flask, jsonify, make_response, render_template
 from flask_login import current_user
 from itertools import chain
 
 from eNMS import app
-from eNMS.database.functions import fetch
+from eNMS.database import Session
+from eNMS.database.functions import delete, factory, fetch
 from eNMS.forms import form_properties
-from eNMS.framework.cli import configure_cli
 from eNMS.framework.config import config_mapper
 from eNMS.framework.extensions import auth, csrf, login_manager
 from eNMS.framework.rest import configure_rest_api
@@ -16,82 +18,122 @@ from eNMS.database.properties import property_names
 from eNMS.setup import rbac
 
 
-def register_extensions(flask_app):
-    csrf.init_app(flask_app)
-    login_manager.init_app(flask_app)
-
-
-def configure_login_manager():
-    @login_manager.user_loader
-    def user_loader(id):
-        return fetch("user", allow_none=True, id=int(id))
-
-    @login_manager.request_loader
-    def request_loader(request):
-        return fetch("user", allow_none=True, name=request.form.get("name"))
-
-
-def configure_context_processor(flask_app):
-    @flask_app.context_processor
-    def inject_properties():
-        return {
-            "property_types": property_types,
-            "form_properties": form_properties,
-            "menu": rbac["menu"],
-            "names": property_names,
-            "rbac": rbac["groups"][getattr(current_user, "group", "Read Only")],
-            "relations": list(set(chain.from_iterable(relationships.values()))),
-            "relationships": relationships,
-            "service_types": {
-                service: service_class.pretty_name
-                for service, service_class in sorted(models.items())
-                if hasattr(service_class, "pretty_name")
-            },
-            "settings": app.settings,
-            "table_properties": app.properties["tables"],
-            "user": current_user.serialized if current_user.is_authenticated else None,
-            "version": app.version,
-        }
-
-
-def configure_errors(flask_app):
-    @flask_app.errorhandler(403)
-    def authorization_required(error):
-        return render_template("error.html", error=403), 403
-
-    @flask_app.errorhandler(404)
-    def not_found_error(error):
-        return render_template("error.html", error=404), 404
-
-
-def configure_authentication():
-    @auth.verify_password
-    def verify_password(username, password):
-        user = fetch("user", name=username)
-        hash = app.settings["security"]["hash_user_passwords"]
-        verify = argon2.verify if hash else str.__eq__
-        return verify(password, user.password)
-
-    @auth.get_password
-    def get_password(username):
-        return getattr(fetch("user", name=username), "password", False)
-
-    @auth.error_handler
-    def unauthorized():
-        return make_response(jsonify({"message": "Wrong credentials."}), 401)
-
-
 class WebApplication(Flask):
 
     def __init__(self, config_mode=None):
         super().__init__(__name__, static_folder=app.path / "eNMS" / "static")
         config = config_mapper[config_mode or app.settings["app"]["config_mode"]]
         self.config.from_object(config)
-        register_extensions(flask_app)
-        configure_login_manager()
-        configure_cli(flask_app)
-        configure_context_processor(flask_app)
-        configure_rest_api(flask_app)
-        configure_errors(flask_app)
-        configure_authentication()
+        self.register_extensions()
+        self.configure_login_manager()
+        self.configure_cli()
+        self.configure_context_processor()
+        self.configure_rest_api()
+        self.configure_errors()
+        self.configure_authentication()
         self.register_blueprint(blueprint)
+
+    def register_extensions(self):
+        csrf.init_app(self)
+        login_manager.init_app(self)
+        self.api = Api(self, decorators=[csrf.exempt])
+
+    def configure_cli(self):
+        @self.cli.command(name="fetch")
+        @argument("table")
+        @argument("name")
+        def cli_fetch(table, name):
+            echo(
+                app.str_dict(fetch(table, name=name).get_properties(exclude=["positions"]))
+            )
+
+        @self.cli.command()
+        @argument("table")
+        @argument("properties")
+        def update(table, properties):
+            result = factory(table, **loads(properties)).get_properties(
+                exclude=["positions"]
+            )
+            Session.commit()
+            echo(app.str_dict(result))
+
+        @self.cli.command(name="delete")
+        @argument("table")
+        @argument("name")
+        def cli_delete(table, name):
+            device = delete(table, name=name)
+            Session.commit()
+            echo(app.str_dict(device))
+
+        @self.cli.command(name="run_service")
+        @argument("name")
+        @option("--devices")
+        @option("--payload")
+        def start(name, devices, payload):
+            devices_list = devices.split(",") if devices else []
+            devices_list = [fetch("device", name=name).id for name in devices_list]
+            payload_dict = loads(payload) if payload else {}
+            payload_dict["devices"] = devices_list
+            service = fetch("service", name=name)
+            results = app.run(service.id, **payload_dict)
+            Session.commit()
+            echo(app.str_dict(results))
+
+    def configure_login_manager(self):
+        @login_manager.user_loader
+        def user_loader(id):
+            return fetch("user", allow_none=True, id=int(id))
+
+        @login_manager.request_loader
+        def request_loader(request):
+            return fetch("user", allow_none=True, name=request.form.get("name"))
+
+
+    def configure_context_processor(self):
+        @self.context_processor
+        def inject_properties():
+            return {
+                "property_types": property_types,
+                "form_properties": form_properties,
+                "menu": rbac["menu"],
+                "names": property_names,
+                "rbac": rbac["groups"][getattr(current_user, "group", "Read Only")],
+                "relations": list(set(chain.from_iterable(relationships.values()))),
+                "relationships": relationships,
+                "service_types": {
+                    service: service_class.pretty_name
+                    for service, service_class in sorted(models.items())
+                    if hasattr(service_class, "pretty_name")
+                },
+                "settings": app.settings,
+                "table_properties": app.properties["tables"],
+                "user": current_user.serialized if current_user.is_authenticated else None,
+                "version": app.version,
+            }
+
+
+    def configure_errors(self):
+        @self.errorhandler(403)
+        def authorization_required(error):
+            return render_template("error.html", error=403), 403
+
+        @self.errorhandler(404)
+        def not_found_error(error):
+            return render_template("error.html", error=404), 404
+
+
+    def configure_authentication(self):
+        @auth.verify_password
+        def verify_password(username, password):
+            user = fetch("user", name=username)
+            hash = app.settings["security"]["hash_user_passwords"]
+            verify = argon2.verify if hash else str.__eq__
+            return verify(password, user.password)
+
+        @auth.get_password
+        def get_password(username):
+            return getattr(fetch("user", name=username), "password", False)
+
+        @auth.error_handler
+        def unauthorized():
+            return make_response(jsonify({"message": "Wrong credentials."}), 401)
--- eNMS/framework/cli.py
+++ eNMS/framework/cli.py
@@ -1,48 +1,6 @@
-from click import argument, echo, option
-from json import loads
-
-from eNMS import app
-from eNMS.database import Session
-from eNMS.database.functions import delete, factory, fetch
 
 
-def configure_cli(flask_app):
-    @flask_app.cli.command(name="fetch")
-    @argument("table")
-    @argument("name")
-    def cli_fetch(table, name):
-        echo(
-            app.str_dict(fetch(table, name=name).get_properties(exclude=["positions"]))
-        )
 
-    @flask_app.cli.command()
-    @argument("table")
-    @argument("properties")
-    def update(table, properties):
-        result = factory(table, **loads(properties)).get_properties(
-            exclude=["positions"]
-        )
-        Session.commit()
-        echo(app.str_dict(result))
 
-    @flask_app.cli.command(name="delete")
-    @argument("table")
-    @argument("name")
-    def cli_delete(table, name):
-        device = delete(table, name=name)
-        Session.commit()
-        echo(app.str_dict(device))
 
-    @flask_app.cli.command(name="run_service")
-    @argument("name")
-    @option("--devices")
-    @option("--payload")
-    def start(name, devices, payload):
-        devices_list = devices.split(",") if devices else []
-        devices_list = [fetch("device", name=name).id for name in devices_list]
-        payload_dict = loads(payload) if payload else {}
-        payload_dict["devices"] = devices_list
-        service = fetch("service", name=name)
-        results = app.run(service.id, **payload_dict)
-        Session.commit()
-        echo(app.str_dict(results))
+

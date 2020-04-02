d110284e45f45a762e64732a409f4b2db62d552d
Antoine Fourmy <antoine.fourmy@gmail.com>
2020-04-02 07:42:52 -0400

    shell start enms p2

--- build/scripts/start.sh
+++ build/scripts/start.sh
@@ -1,4 +1,37 @@
-FLASK_APP="app.py"
-FLASK_DEBUG=1
-cd /media/sf_shared/eNMS/
-gunicorn --config gunicorn.py app:app
+#!/bin/bash
+
+function start() {
+  FLASK_APP="app.py"
+  FLASK_DEBUG=1
+  cd /media/sf_shared/eNMS/
+  gunicorn --config gunicorn.py app:app
+}
+
+function usage() {
+  if [ -n "$1" ]; then
+    echo -e "${RED}👉 $1${CLEAR}\n";
+  fi
+  echo "Usage: $0 [-n number-of-people] [-s section-id] [-c cache-file]"
+  echo "  -n, --number-of-people   The number of people"
+  echo "  -s, --section-id         A sections unique id"
+  echo "  -q, --quiet              Only print result"
+  echo ""
+  echo "Example: $0 --number-of-people 2 --section-id 1 --cache-file last-known-date.txt"
+  exit 1
+}
+
+# parse params
+while [[ "$#" > 0 ]]; do case $1 in
+  -r|--reload-database) RELOAD=true; shift;shift;;
+  *) usage "Unknown parameter passed: $1"; shift; shift;;
+esac; done
+
+echo $RELOAD;
+
+# verify params
+if [ "$RELOAD" = true ];
+then
+  start;
+else
+  start;
+fi;

9ebde8d3b65bd280f6397a93cbba9ca22f1093bf
Antoine Fourmy <antoine.fourmy@gmail.com>
2020-04-02 09:24:35 -0400

    shell start enms p3

--- build/scripts/start.sh
+++ build/scripts/start.sh
@@ -1,37 +1,28 @@
 #!/bin/bash
 
+while [[ "$#" -gt 0 ]]; do case $1 in
+  -r|--reload-database) reload=true; shift;shift;;
+  -p|--path) path="$2";shift;shift;;
+  *) unknown=$1; shift; shift;;
+esac; done
+
 function start() {
+  if [[ -n "$path" ]]
+  then
+    cd $path
+  fi
+  if [[ -n "$unknown" ]]
+  then
+   echo "Unknown parameter detected $unknown"
+   exit 1
+  fi
   FLASK_APP="app.py"
   FLASK_DEBUG=1
-  cd /media/sf_shared/eNMS/
+  if [ "$reload" = true ]
+  then
+    rm database.db
+  fi
   gunicorn --config gunicorn.py app:app
 }
 
-function usage() {
-  if [ -n "$1" ]; then
-    echo -e "${RED}👉 $1${CLEAR}\n";
-  fi
-  echo "Usage: $0 [-n number-of-people] [-s section-id] [-c cache-file]"
-  echo "  -n, --number-of-people   The number of people"
-  echo "  -s, --section-id         A sections unique id"
-  echo "  -q, --quiet              Only print result"
-  echo ""
-  echo "Example: $0 --number-of-people 2 --section-id 1 --cache-file last-known-date.txt"
-  exit 1
-}
-
-# parse params
-while [[ "$#" > 0 ]]; do case $1 in
-  -r|--reload-database) RELOAD=true; shift;shift;;
-  *) usage "Unknown parameter passed: $1"; shift; shift;;
-esac; done
-
-echo $RELOAD;
-
-# verify params
-if [ "$RELOAD" = true ];
-then
-  start;
-else
-  start;
-fi;
+start;

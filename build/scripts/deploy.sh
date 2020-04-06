919843d96a0650482c7747aa66acba136f916451
Antoine Fourmy <antoine.fourmy@gmail.com>
2020-04-06 18:34:28 -0400

    update vault deployment bash

--- build/scripts/deploy.sh
+++ build/scripts/deploy.sh
@@ -7,6 +7,22 @@ function install() {
     sudo ln -s ${path:-$PWD}/vault /usr/bin/vault
     sudo ln -s ${path:-$PWD}/consul /usr/bin/consul
     nohup consul agent -dev &
+    config='
+      disable_mlock = true
+
+      storage "consul" {
+        address = "127.0.0.1:8500"
+        path    = "vault/"
+      }
+
+      listener "tcp" {
+      address     = "127.0.0.1:8200"
+      tls_disable = 1
+      }
+    '
+    nohup vault server -config=<(echo "$config") &
+    export VAULT_ADDR=http://127.0.0.1:8200
+    echo vault operator init | head -n7
   elif [ "$install" = "mysql" ]; then
     sudo apt install -y mysql-server python3-mysqldb
     sudo pip3 install mysqlclient

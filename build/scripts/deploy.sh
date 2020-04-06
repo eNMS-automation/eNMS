9dbc98ef08ce5fce8c2f2377fee019a9719dcbdb
Antoine Fourmy <antoine.fourmy@gmail.com>
2020-04-06 09:53:31 -0400

    refactor deploy script p3

--- build/scripts/deploy.sh
+++ build/scripts/deploy.sh
@@ -1,41 +1,34 @@
 #!/bin/bash
 
 function install() {
-  if [ "$vault" = true ]; then
+  if [ "$install" = "vault" ]; then
     sudo apt-get install -y unzip
     find . -iname "vault_*zip" -exec unzip {} \;
     sudo ln -s ${path:-$PWD}/vault /usr/bin/vault
-  fi
-  if [[ -n "$database" ]]; then
-    if [ "$database" = "mysql" ]; then
-      sudo apt install -y mysql-server python3-mysqldb
-      sudo pip3 install mysqlclient
-      sudo mysql -u root --password=password -e 'CREATE DATABASE enms;'
-      sudo mysql -u root --password=password -e 'set global max_connections = 2000;'
-    elif [ "$database" = "pgsql" ]; then
-      sudo apt-get install -y postgresql libpq-dev postgresql-client
-      sudo pip3 install psycopg2
-      sudo -u postgres psql -c "CREATE DATABASE enms;"
-      sudo -u postgres psql -c "CREATE USER root WITH PASSWORD 'password';"
-      sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE enms TO root;"
-    fi
+  elif [ "$install" = "mysql" ]; then
+    sudo apt install -y mysql-server python3-mysqldb
+    sudo pip3 install mysqlclient
+    sudo mysql -u root --password=password -e 'CREATE DATABASE enms;'
+    sudo mysql -u root --password=password -e 'set global max_connections = 2000;'
+  elif [ "$install" = "postgresql" ]; then
+    sudo apt-get install -y postgresql libpq-dev postgresql-client
+    sudo pip3 install psycopg2
+    sudo -u postgres psql -c "CREATE DATABASE enms;"
+    sudo -u postgres psql -c "CREATE USER root WITH PASSWORD 'password';"
+    sudo -u postgres psql -c "GRANT ALL PRIVILEGES ON DATABASE enms TO root;"
   fi
 }
 
 function uninstall() {
-  if [ "$vault" = true ]; then
+  if [ "$install" = "vault" ]; then
     sudo rm /usr/bin/vault && rm vault
+  elif [ "$install" = "mysql" ]; then
+    sudo apt-get -y remove --purge "mysql*"
+  elif [ "$install" = "postgresql" ]; then
+    sudo apt-get -y --purge remove postgresql\* libpq-dev
   fi
-  if [[ -n "$database" ]]; then
-    if [ "$database" = "mysql" ]; then
-      sudo apt-get -y remove --purge "mysql*"
-    elif [ "$database" = "pgsql" ]; then
-      sudo apt-get -y --purge remove postgresql\* libpq-dev
-    fi
-    sudo apt-get -y autoremove
-    sudo apt-get -y autoclean
-    exit 0
-  fi
+  sudo apt-get -y autoremove
+  sudo apt-get -y autoclean
 }
 
 function help() {
@@ -43,32 +36,34 @@ function help() {
     Usage: $(basename $0) [OPTIONS]
 
     Options:
-      -d <database>                Database (mysql | pgsql)
       -h                           Help
-      -i                           Install
+      -i <program>                 Install
       -p <path>                    Path to folder (default: current folder \$PWD)
-      -u                           Uninstall
-      -v                           Vault
+      -u <program>                 Uninstall
+
+    Programs:
+      MySQL / PostgreSQL
+      Vault
+      eNMS (git, pip, requirements)
+      Nginx
   "
   exit 0
 }
 
-while getopts h?p:d:uiv opt; do
+while getopts h?p:i:u: opt; do
     case "$opt" in
-      d) database=$OPTARG;;
-      i) function=install;;
+      i) install=${OPTARG,,};;
       p) path=$OPTARG;;
-      u) function=uninstall;;
-      v) vault=true;;
+      u) uninstall=${OPTARG,,};;
       h|\?) help;;
     esac
 done
 
 function deploy() {
   cd ${path:-$PWD}
-  if [ "$function" = "install" ]; then
+  if [[ -n "$install" ]]; then
     install
-  elif [ "$function" = "uninstall" ]; then
+  elif [[ -n "$uninstall" ]]; then
     uninstall
   fi
 }

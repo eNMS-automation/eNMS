bef8b47872432fdd89152b0d21189ebed8711d39
Antoine Fourmy <antoine.fourmy@gmail.com>
2020-04-09 08:21:39 -0400

    remote scheduler p2

--- scheduler/scheduler.py
+++ scheduler/scheduler.py
@@ -1,25 +1,28 @@
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
-from asyncio import get_event_loop
 from datetime import datetime
 from starlette.applications import Starlette
 from starlette.responses import JSONResponse
-from starlette.routing import Route
 
 
 class Scheduler(Starlette):
 
-    def __init__(self, **kwargs):
-        super().__init__(**kwargs)
-        scheduler = AsyncIOScheduler()
-        scheduler.start()
+    def __init__(self, ):
+        super().__init__()
+        self.configure_scheduler()
+        self.register_routes()
+
+    def configure_scheduler(self):
+        self.scheduler = AsyncIOScheduler()
+        self.scheduler.start()
+
+    def register_routes(self):
+        @self.route('/')
+        async def schedule(request):
+            self.scheduler.add_job(self.run_job, "interval", seconds=3)
+            return JSONResponse(True)
+
+    def run_job(self):
+        print(f"Tick! The time is: {datetime.now()}")
 
 
-async def schedule(request):
-    print(request)
-    tick = lambda: f"Tick! The time is: {datetime.now()}"
-    scheduler.add_job(tick, "interval", seconds=3)
-    return JSONResponse({"hello": "world"})
-
-
-
-scheduler = Scheduler(debug=True, routes=[Route("/", schedule)])
+scheduler = Scheduler()

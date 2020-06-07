cd9ef9a8290326f9085d1622c576bbb448ef7e3d
Antoine Fourmy <antoine.fourmy@gmail.com>
2020-06-07 10:01:26 -0400

    move jump on connect parameters to last section

--- eNMS/forms/automation.py
+++ eNMS/forms/automation.py
@@ -260,19 +260,6 @@ class NetmikoForm(ConnectionForm):
         render_kw={"help": "netmiko/expect_prompt"},
     )
     groups = {
-        "Jump on connect Parameters": {
-            "commands": [
-                "jump_on_connect",
-                "jump_command",
-                "expect_username_prompt",
-                "jump_username",
-                "expect_password_prompt",
-                "jump_password",
-                "expect_prompt",
-                "exit_command",
-            ],
-            "default": "hidden",
-        },
         "Netmiko Parameters": {
             "commands": [
                 "driver",
@@ -287,6 +274,19 @@ class NetmikoForm(ConnectionForm):
             "default": "expanded",
         },
         "Connection Parameters": ConnectionForm.group,
+        "Jump on connect Parameters": {
+            "commands": [
+                "jump_on_connect",
+                "jump_command",
+                "expect_username_prompt",
+                "jump_username",
+                "expect_password_prompt",
+                "jump_password",
+                "expect_prompt",
+                "exit_command",
+            ],
+            "default": "hidden",
+        },
     }
 
 

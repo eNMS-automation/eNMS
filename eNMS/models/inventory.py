56aa28127a168ec1bddd16d8fe7d5afdc9f9962c
Antoine Fourmy <antoine.fourmy@gmail.com>
2020-07-02 11:57:15 -0400

    rename rbac queries

--- eNMS/models/inventory.py
+++ eNMS/models/inventory.py
@@ -343,20 +343,20 @@ class Pool(AbstractBase):
     @classmethod
     def rbac_filter(cls, query, mode, user):
         public_pools = query.filter(cls.public == true())
-        user_access_pools = (
+        user_pools = (
             query.join(cls.access)
             .join(models["user"], models["access"].users)
             .filter(models["access"].pools_access.contains(mode))
             .filter(models["user"].name == user.name)
         )
-        user_group_access_pools = (
+        user_group_pools = (
             query.join(cls.access)
             .join(models["group"], models["access"].groups)
             .join(models["user"], models["group"].users)
             .filter(models["access"].pools_access.contains(mode))
             .filter(models["user"].name == user.name)
         )
-        return public_pools.union(user_access_pools, user_group_access_pools)
+        return public_pools.union(user_pools, user_group_pools)
 
 
 class Session(AbstractBase):

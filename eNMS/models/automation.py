0830bdbd0c44759bdc11e50082faff9006f76130
Antoine Fourmy <antoine.fourmy@gmail.com>
2020-07-09 06:15:39 -0400

    comment out device rbac target check

--- eNMS/models/automation.py
+++ eNMS/models/automation.py
@@ -643,14 +643,14 @@ class Run(AbstractBase):
 
     def device_run(self, payload):
         self.devices = self.compute_devices(payload)
-        allowed_targets = db.query("device", rbac="target", username=self.creator)
-        unauthorized_targets = set(self.devices) - set(allowed_targets)
-        if unauthorized_targets:
-            result = (
-                f"Error 403: User '{self.creator}' is not allowed to set the following"
-                f" devices as targets: {', '.join(map(str, unauthorized_targets))}"
-            )
-            return {"result": result, "success": False}
+        # allowed_targets = db.query("device", rbac="target", username=self.creator)
+        # unauthorized_targets = set(self.devices) - set(allowed_targets)
+        # if unauthorized_targets:
+        #     result = (
+        #         f"Error 403: User '{self.creator}' is not allowed to use these"
+        #         f" devices as targets: {', '.join(map(str, unauthorized_targets))}"
+        #     )
+        #     return {"result": result, "success": False}
         if self.run_method != "once":
             self.write_state("progress/device/total", len(self.devices), "increment")
         if self.iteration_devices and not self.parent_device:

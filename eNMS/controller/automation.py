f250607dc80046c8e4d27749a842f973003861cd
Antoine Fourmy <antoine.fourmy@gmail.com>
2020-07-23 06:20:53 -0400

    fix workflow builder run display bug

--- eNMS/controller/automation.py
+++ eNMS/controller/automation.py
@@ -235,9 +235,9 @@ class AutomationController(BaseController):
         if runs and runtime != "normal":
             if runtime == "latest":
                 runtime = runs[-1].parent_runtime
-            runs = [r for r in runs if r.parent_runtime == runtime]
-            if runs:
-                state = runs[0].get_state()
+            latest_runs = [r for r in runs if r.parent_runtime == runtime]
+            if latest_runs:
+                state = latest_runs[0].get_state()
         return {
             "service": service.to_dict(include=["services", "edges", "superworkflow"]),
             "runtimes": sorted(set((r.parent_runtime, r.creator) for r in runs)),

d609ddf9df7b53104738fe419edafb0d3bd0f49a
Antoine Fourmy <antoine.fourmy@gmail.com>
2020-07-28 03:12:10 -0400

    forbid running service from rest api if no access

--- eNMS/server.py
+++ eNMS/server.py
@@ -473,7 +473,7 @@ class Server(Flask):
                     **request.get_json(force=True),
                 }
                 errors, devices, pools = [], [], []
-                service = db.fetch("service", name=data["name"])
+                service = db.fetch("service", name=data["name"], rbac="run")
                 handle_asynchronously = data.get("async", False)
                 for device_name in data.get("devices", ""):
                     device = db.fetch("device", name=device_name)

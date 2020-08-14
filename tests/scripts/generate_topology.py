f7a16e445a7ee6e4d4574fbfb3562d5af94376f0
Antoine Fourmy <antoine.fourmy@gmail.com>
2020-08-14 02:08:14 -0400

    update topology generation script p1

--- tests/scripts/generate_topology.py
+++ tests/scripts/generate_topology.py
@@ -2,24 +2,26 @@ from pathlib import Path
 from random import uniform
 from xlwt import easyxf, Workbook
 
-wb = Workbook()
+DEVICE_HEAHDERS = ("name", "longitude", "latitude", "subtype", "ip_address")
+LINK_HEADERS = ("name", "source_name", "destination_name", "color")
+
+workbook = Workbook()
 style0 = easyxf(
     "font: name Times New Roman, color-index black, bold on", num_format_str="#,##0.00"
 )
 style1 = easyxf(num_format_str="#,##0.00")
 
-ws = wb.add_sheet("device")
+device_sheet = workbook.add_sheet("device")
+link_sheet = workbook.add_sheet("link")
 
-for index, header in enumerate(
-    ("name", "longitude", "latitude", "subtype", "ip_address")
-):
-    ws.write(0, index, header)
+for index, header in enumerate(DEVICE_HEAHDERS):
+    device_sheet.write(0, index, header)
 
-for i in range(1, 65000):
-    ws.write(i, 0, "a" + str(i))
-    ws.write(i, 1, uniform(-180.0, 180.0))
-    ws.write(i, 2, uniform(-90.0, 90.0))
-    ws.write(i, 3, "router")
-    ws.write(i, 4, "a" + str(i))
+for i in range(1, 9000):
+    device_sheet.write(i, 0, "a" + str(i))
+    device_sheet.write(i, 1, uniform(-180.0, 180.0))
+    device_sheet.write(i, 2, uniform(-90.0, 90.0))
+    device_sheet.write(i, 3, "router")
+    device_sheet.write(i, 4, "a" + str(i))
 
-wb.save(Path.cwd() / "test_65000.xls")
+workbook.save(Path.cwd() / "test_65000.xls")

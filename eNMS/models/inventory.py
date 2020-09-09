db1c63dec6867842a27e2e40eff59d46a61481e9
Antoine Fourmy <antoine.fourmy@gmail.com>
2020-09-09 04:24:44 -0400

    fix object number count with manually defined pools

--- eNMS/models/inventory.py
+++ eNMS/models/inventory.py
@@ -333,16 +333,16 @@ class Pool(AbstractBase):
         )
 
     def compute_pool(self):
-        if self.manually_defined:
-            return
-        for object_type in ("device", "link"):
-            objects = (
-                list(filter(self.object_match, db.fetch_all(object_type)))
-                if self.compute(object_type)
-                else []
-            )
-            setattr(self, f"{object_type}s", objects)
-            setattr(self, f"{object_type}_number", len(objects))
+        if not self.manually_defined:
+            for object_type in ("device", "link"):
+                objects = (
+                    list(filter(self.object_match, db.fetch_all(object_type)))
+                    if self.compute(object_type)
+                    else []
+                )
+                setattr(self, f"{object_type}s", objects)
+        self.device_number = len(self.devices)
+        self.link_number = len(self.links)
 
     @classmethod
     def rbac_filter(cls, query, mode, user):

f3fca0da6b1a33d459c76ceb0282d3220ff3d934
Antoine Fourmy <antoine.fourmy@gmail.com>
2020-09-18 12:01:40 -0400

    remove groups p5

--- eNMS/static/js/table.js
+++ eNMS/static/js/table.js
@@ -976,40 +976,6 @@ tables.user = class UserTable extends Table {
   }
 };
 
-tables.group = class GroupTable extends Table {
-  get controls() {
-    return [
-      this.columnDisplay(),
-      this.createNewButton(),
-      this.searchTableButton(),
-      this.clearSearchButton(),
-      this.refreshTableButton(),
-    ];
-  }
-
-  buttons(row) {
-    return [
-      `
-      <ul class="pagination pagination-lg" style="margin: 0px;">
-        <li>
-          <button type="button" class="btn btn-sm btn-primary"
-          onclick="eNMS.base.showTypePanel('group', '${row.id}')" data-tooltip="Edit"
-            ><span class="glyphicon glyphicon-edit"></span
-          ></button>
-        </li>
-        <li>
-          <button type="button" class="btn btn-sm btn-primary"
-          onclick="eNMS.base.showTypePanel('group', '${row.id}', 'duplicate')"
-          data-tooltip="Duplicate"
-            ><span class="glyphicon glyphicon-duplicate"></span
-          ></button>
-        </li>
-        ${this.deleteInstanceButton(row)}
-      </ul>`,
-    ];
-  }
-};
-
 tables.access = class AccessTable extends Table {
   get controls() {
     return [

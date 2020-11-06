9c16c217e7507235ece4cb0d17c3d4988a49b84a
Antoine Fourmy <antoine.fourmy@gmail.com>
2020-11-06 04:26:52 -0500

    remove site mechanism p1

--- eNMS/static/js/visualization.js
+++ eNMS/static/js/visualization.js
@@ -327,48 +327,33 @@ function displayNetwork({ noAlert, withCluster }) {
   }
   clustered = withCluster;
   deleteAll();
-  if (page == "geographical_view" && !$("#view-type").prop("checked")) {
-    $(".menu").hide();
-    call({
-      url: "/get_all/pool",
-      callback: function (pools) {
-        for (let i = 0; i < pools.length; i++) {
-          if (pools[i].longitude) {
-            createNode(pools[i], "site");
-          }
+  call({
+    url: "/view_filtering",
+    data: data,
+    callback: function (results) {
+      if (page == "logical_view") {
+        if (
+          results.device.length > maximumSize.node ||
+          results.link.length > maximumSize.link
+        ) {
+          return notify(
+            `Too many objects to display. Use the device and link
+            filtering mechanism to reduce the size of the network`,
+            "error",
+            5
+          );
         }
-      },
-    });
-    $(".geo-menu").show();
-  } else {
-    call({
-      url: "/view_filtering",
-      data: data,
-      callback: function (results) {
-        if (page == "logical_view") {
-          if (
-            results.device.length > maximumSize.node ||
-            results.link.length > maximumSize.link
-          ) {
-            return notify(
-              `Too many objects to display. Use the device and link
-              filtering mechanism to reduce the size of the network`,
-              "error",
-              5
-            );
-          }
-          update3dGraphData(graph, results.device, results.link);
-        } else {
-          results.device.map((d) => createNode(d, "device"));
-          results.link.map(createLink);
-          if (dimension == "2D") {
-            map[clustered ? "addLayer" : "removeLayer"](markerGroup);
-          }
+        update3dGraphData(graph, results.device, results.link);
+      } else {
+        results.device.map((d) => createNode(d, "device"));
+        results.link.map(createLink);
+        if (dimension == "2D") {
+          map[clustered ? "addLayer" : "removeLayer"](markerGroup);
         }
-        if (!noAlert) notify("Filter applied.", "success", 5);
-      },
-    });
-  }
+      }
+      if (!noAlert) notify("Filter applied.", "success", 5);
+    },
+  });
 }
 
 function showPoolView(poolId) {

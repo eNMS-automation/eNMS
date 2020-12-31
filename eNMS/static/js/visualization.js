9871deed490bcee2c1c6f7f6ad94e844cc0bb118
Antoine Fourmy <antoine.fourmy@gmail.com>
2020-12-31 04:21:52 -0500

    logical framework p1

--- eNMS/static/js/visualization.js
+++ eNMS/static/js/visualization.js
@@ -44,6 +44,139 @@ let handler;
 let polylines;
 let labels;
 
+let camera, scene, renderer;
+let plane;
+let mouse,
+  raycaster,
+  isShiftDown = false;
+
+let rollOverMesh, rollOverMaterial;
+let cubeGeo, cubeMaterial;
+
+const objects = [];
+
+function init() {
+  camera = new THREE.PerspectiveCamera(
+    45,
+    window.innerWidth / window.innerHeight,
+    1,
+    10000
+  );
+  camera.position.set(500, 800, 1300);
+  camera.lookAt(0, 0, 0);
+  scene = new THREE.Scene();
+  scene.background = new THREE.Color(0xf0f0f0);
+  const rollOverGeo = new THREE.BoxBufferGeometry(50, 50, 50);
+  rollOverMaterial = new THREE.MeshBasicMaterial({
+    color: 0xff0000,
+    opacity: 0.5,
+    transparent: true,
+  });
+  rollOverMesh = new THREE.Mesh(rollOverGeo, rollOverMaterial);
+  scene.add(rollOverMesh);
+  cubeGeo = new THREE.BoxBufferGeometry(50, 50, 50);
+  cubeMaterial = new THREE.MeshLambertMaterial({
+    color: 0xfeb74c,
+    map: new THREE.TextureLoader().load("textures/square-outline-textured.png"),
+  });
+  const gridHelper = new THREE.GridHelper(1000, 20);
+  scene.add(gridHelper);
+  raycaster = new THREE.Raycaster();
+  mouse = new THREE.Vector2();
+  const geometry = new THREE.PlaneBufferGeometry(1000, 1000);
+  geometry.rotateX(-Math.PI / 2);
+  plane = new THREE.Mesh(geometry, new THREE.MeshBasicMaterial({ visible: false }));
+  scene.add(plane);
+  objects.push(plane);
+  const ambientLight = new THREE.AmbientLight(0x606060);
+  scene.add(ambientLight);
+  const directionalLight = new THREE.DirectionalLight(0xffffff);
+  directionalLight.position.set(1, 0.75, 0.5).normalize();
+  scene.add(directionalLight);
+  renderer = new THREE.WebGLRenderer({ antialias: true });
+  renderer.setPixelRatio(window.devicePixelRatio);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  document.body.appendChild(renderer.domElement);
+  document.addEventListener("mousemove", onDocumentMouseMove, false);
+  document.addEventListener("mousedown", onDocumentMouseDown, false);
+  document.addEventListener("keydown", onDocumentKeyDown, false);
+  document.addEventListener("keyup", onDocumentKeyUp, false);
+  window.addEventListener("resize", onWindowResize, false);
+}
+
+function onWindowResize() {
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
+  renderer.setSize(window.innerWidth, window.innerHeight);
+}
+
+function onDocumentMouseMove(event) {
+  event.preventDefault();
+  mouse.set(
+    (event.clientX / window.innerWidth) * 2 - 1,
+    -(event.clientY / window.innerHeight) * 2 + 1
+  );
+  raycaster.setFromCamera(mouse, camera);
+  const intersects = raycaster.intersectObjects(objects);
+  if (intersects.length > 0) {
+    const intersect = intersects[0];
+    rollOverMesh.position.copy(intersect.point).add(intersect.face.normal);
+    rollOverMesh.position.divideScalar(50).floor().multiplyScalar(50).addScalar(25);
+  }
+  render();
+}
+
+function onDocumentMouseDown(event) {
+  event.preventDefault();
+  mouse.set(
+    (event.clientX / window.innerWidth) * 2 - 1,
+    -(event.clientY / window.innerHeight) * 2 + 1
+  );
+  raycaster.setFromCamera(mouse, camera);
+  const intersects = raycaster.intersectObjects(objects);
+  if (intersects.length > 0) {
+    const intersect = intersects[0];
+    if (isShiftDown) {
+      if (intersect.object !== plane) {
+        scene.remove(intersect.object);
+        objects.splice(objects.indexOf(intersect.object), 1);
+      }
+    } else {
+      const voxel = new THREE.Mesh(cubeGeo, cubeMaterial);
+      voxel.position.copy(intersect.point).add(intersect.face.normal);
+      voxel.position.divideScalar(50).floor().multiplyScalar(50).addScalar(25);
+      scene.add(voxel);
+      objects.push(voxel);
+    }
+    render();
+  }
+}
+
+function onDocumentKeyDown(event) {
+  switch (event.keyCode) {
+    case 16:
+      isShiftDown = true;
+      break;
+  }
+}
+
+function onDocumentKeyUp(event) {
+  switch (event.keyCode) {
+    case 16:
+      isShiftDown = false;
+      break;
+  }
+}
+
+function render() {
+  renderer.render(scene, camera);
+}
+
+function initLogicalFramework() {
+  init();
+  render();
+}
+
 function initGeographicalFramework() {
   dimension = visualization.geographical.default;
   if (dimension == "2D") {
@@ -383,6 +516,7 @@ function processNetwork(network) {
 }
 
 function displayNetwork({ noAlert, withCluster }) {
+  if (page == "logical_view") return;
   const maximumSize = visualization.logical.maximum_size;
   let data = {};
   for (let type of ["device", "link"]) {
@@ -636,8 +770,10 @@ export function initView() {
   if (page == "force_directed_view") {
     create3dGraphNetwork("network");
     notify("Loading network...", "success", 5);
-  } else {
+  } else if (page == "geographical_view") {
     initGeographicalFramework();
+  } else {
+    initLogicalFramework();
   }
   displayNetwork({ noAlert: true });
 }

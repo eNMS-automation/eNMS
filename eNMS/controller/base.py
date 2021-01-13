7341e4d7a8e2169d90a11de0c1882090a9ea3715
Antoine Fourmy <antoine.fourmy@gmail.com>
2021-01-13 03:27:09 -0500

    add services programmatically in rbac

--- eNMS/controller/base.py
+++ eNMS/controller/base.py
@@ -265,7 +265,8 @@ class BaseController:
                 if not load_examples and "examples" in str(file):
                     continue
                 info(f"Loading service: {file}")
-                spec = spec_from_file_location(str(file).split("/")[-1][:-3], str(file))
+                self.rbac["get_requests"].append(f"/form/{file.stem}_service")
+                spec = spec_from_file_location(file.stem, str(file))
                 try:
                     spec.loader.exec_module(module_from_spec(spec))
                 except InvalidRequestError as exc:

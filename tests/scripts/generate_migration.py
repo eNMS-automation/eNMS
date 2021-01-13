4c2072a1193bf6ef83111101b2f6e10d05e55cd1
Antoine Fourmy <antoine.fourmy@gmail.com>
2021-01-13 11:22:25 -0500

    update migration generation script

--- tests/scripts/generate_migration.py
+++ tests/scripts/generate_migration.py
@@ -14,9 +14,25 @@ def generate_migration_file(project):
     path = Path.cwd().parent.parent.parent / "eNMS" / "files" / "migrations" / project
 
     services = [
-        {"name": f"s{index}", "shared": True, "type": choice(service_type)}
+        {
+            "name": f"[Shared] s{index}",
+            "scoped_name": f"s{index}",
+            "shared": True,
+            "type": choice(service_type),
+        }
         for index in range(1, 10001)
     ]
+
+    services.extend([
+        {
+            "name": f"w{index}",
+            "scoped_name": f"w{index}",
+            "type": "workflow",
+            "services": list(set(f"[Shared] s{randrange(1, 1001)}" for _ in range(30)))
+        }
+        for index in range(1, 1001)
+    ])
+
     with open(path / f"service.yaml", "w") as migration_file:
         yaml.dump(services, migration_file)
 

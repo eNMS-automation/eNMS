f99e09eea87da4b21a36de765b544aa1dc7bbd1f
Antoine Fourmy <antoine.fourmy@gmail.com>
2021-03-16 06:17:19 -0400

    update memory profile snippet

--- files/snippets/memory_profile.py
+++ files/snippets/memory_profile.py
@@ -11,7 +11,9 @@ from operator import itemgetter
 from pympler import muppy, summary
 
 MODULES = ["netmiko", "napalm", "sqlalchemy"]
-MAX_RESULTS = 100
+MAX_RESULTS = 50
+
+all_objects = muppy.get_objects()
 
 
 def format_size(size):
@@ -23,11 +25,7 @@ def format_size(size):
 
 
 profile = sorted(
-    (
-        object_
-        for object_ in summary.summarize(muppy.get_objects())
-        if any(module in object_[0] for module in MODULES)
-    ),
+    (object_ for object_ in summary.summarize(all_objects)),
     key=itemgetter(2),
     reverse=True,
 )
@@ -36,3 +34,8 @@ for object_ in profile[:MAX_RESULTS]:
     print(f"Name: {object_[0]}")
     print(f"Number of objects: {object_[1]}")
     print(f"Total size: {format_size(object_[2])}", end="\n\n")
+
+
+for type in (str, dict):
+    print(f"Last {MAX_RESULTS} {type} objects in memory", end="\n\n")
+    print("\n".join(muppy.filter(all_objects, Type=type)[-MAX_RESULTS:]))

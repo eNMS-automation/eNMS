6fcdef0f788520398ffaf86b1819ac1bbc610a7a
Antoine Fourmy <antoine.fourmy@gmail.com>
2021-03-16 12:50:37 -0400

    update memory profiler script

--- files/snippets/memory_profile.py
+++ files/snippets/memory_profile.py
@@ -2,17 +2,16 @@
 # By default, objects are sorted by their size in memory.
 # The order can be changed to sorting by number of objects in
 # memory by replacing "itemgetter(2)" with "itemgetter(1)".
-# The "MAX_RESULTS" variable limits the output to the first
-# MAX_RESULTS results.
+# The "max_results" variable limits the output to the first
+# max_results results.
 
 # flake8: noqa
 
 from operator import itemgetter
 from pympler import muppy, summary
 
-MODULES = ["netmiko", "napalm", "sqlalchemy"]
-MAX_RESULTS = 50
 
+max_results = 3
 all_objects = muppy.get_objects()
 
 
@@ -30,13 +29,13 @@ profile = sorted(
     reverse=True,
 )
 
-for object_ in profile[:MAX_RESULTS]:
+for object_ in profile[:max_results]:
     print(f"Name: {object_[0]}")
     print(f"Number of objects: {object_[1]}")
     print(f"Total size: {format_size(object_[2])}", end="\n\n")
 
 
 for type in (str, dict):
-    print(f"Last {MAX_RESULTS} {type} objects in memory", end="\n\n")
-    latest_objects = muppy.filter(all_objects, Type=type)[-MAX_RESULTS:]
+    print(f"Last {max_results} {type} objects in memory", end="\n\n")
+    latest_objects = muppy.filter(all_objects, Type=type)[-max_results:]
     print("\n".join(map(str, latest_objects)))

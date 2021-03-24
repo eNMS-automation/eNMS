347f243694c07139c092b7cedffacee5b159edb2
Antoine Fourmy <antoine.fourmy@gmail.com>
2021-03-24 14:56:48 -0400

    fix rbac argument bug

--- eNMS/automation.py
+++ eNMS/automation.py
@@ -794,7 +794,9 @@ class ServiceRun:
     def database_function(self, func, model, **kwargs):
         if model not in automation["workflow"]["allowed_models"][func]:
             raise db.rbac_error(f"Use of '{func}' not allowed on {model}s.")
-        return getattr(db, func)(model, rbac="edit", username=self.creator, **kwargs)
+        if "fetch" in func:
+            kwargs["rbac"] = "edit"
+        return getattr(db, func)(model, username=self.creator, **kwargs)
 
     def global_variables(_self, **locals):  # noqa: N805
         payload, device = _self.payload, locals.get("device")

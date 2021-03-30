27ba290197269cbf11a8c318bf9bab707c29f7d4
Antoine Fourmy <antoine.fourmy@gmail.com>
2021-03-30 15:05:55 -0400

    separate controller from app p44

--- eNMS/controller/administration.py
+++ eNMS/controller/administration.py
@@ -118,6 +118,32 @@ class BaseController:
             )
         )
 
+    def copy_service_in_workflow(self, workflow_id, **kwargs):
+        service_sets = list(set(kwargs["services"].split(",")))
+        service_instances = db.objectify("service", service_sets)
+        workflow = db.fetch("workflow", id=workflow_id)
+        services, errors = [], []
+        if kwargs["mode"] == "shallow":
+            for service in service_instances:
+                if not service.shared:
+                    errors.append(f"'{service.name}' is not a shared service.")
+                elif service in workflow.services:
+                    errors.append(f"This workflow already contains '{service.name}'.")
+        if errors:
+            return {"alert": errors}
+        for service in service_instances:
+            if kwargs["mode"] == "deep":
+                service = service.duplicate(workflow)
+            else:
+                workflow.services.append(service)
+            services.append(service)
+        workflow.last_modified = app.get_time()
+        db.session.commit()
+        return {
+            "services": [service.serialized for service in services],
+            "update_time": workflow.last_modified,
+        }
+
     def count_models(self):
         return {
             "counters": {
@@ -135,6 +161,17 @@ class BaseController:
     def counters(self, property, model):
         return Counter(v for v, in db.query(model, property=property, rbac=None))
 
+    def create_label(self, workflow_id, x, y, label_id, **kwargs):
+        workflow = db.fetch("workflow", id=workflow_id, rbac="edit")
+        label_id = str(uuid4()) if label_id == "undefined" else label_id
+        label = {
+            "positions": [x, y],
+            "content": kwargs["text"],
+            "alignment": kwargs["alignment"],
+        }
+        workflow.labels[label_id] = label
+        return {"id": label_id, **label}
+
     def create_view_object(self, type, view_id, **kwargs):
         node = db.factory(type, view=view_id, **kwargs)
         db.session.flush()
@@ -142,6 +179,37 @@ class BaseController:
 
     def database_deletion(self, **kwargs):
         db.delete_all(*kwargs["deletion_types"])
+
+    def delete_corrupted_edges(self):
+        edges = set(db.fetch_all("workflow_edge"))
+        duplicated_edges, number_of_corrupted_edges = defaultdict(list), 0
+        for edge in list(edges):
+            services = getattr(edge.workflow, "services", [])
+            if (
+                not edge.source
+                or not edge.destination
+                or not edge.workflow
+                or edge.source not in services
+                or edge.destination not in services
+            ):
+                edges.remove(edge)
+                db.session.delete(edge)
+                number_of_corrupted_edges += 1
+        db.session.commit()
+        for edge in edges:
+            duplicated_edges[
+                (
+                    edge.source.name,
+                    edge.destination.name,
+                    edge.workflow.name,
+                    edge.subtype,
+                )
+            ].append(edge)
+        for duplicates in duplicated_edges.values():
+            for duplicate in duplicates[1:]:
+                db.session.delete(duplicate)
+                number_of_corrupted_edges += 1
+        return number_of_corrupted_edges
 
     def delete_file(self, filepath):
         remove(Path(filepath.replace(">", "/")))
@@ -153,6 +221,25 @@ class BaseController:
         for instance_id in selection:
             db.delete("view_object", id=instance_id)
         return self.get_time()
+
+    def delete_workflow_selection(self, workflow_id, **selection):
+        workflow = db.fetch("workflow", id=workflow_id)
+        workflow.last_modified = app.get_time()
+        for edge_id in selection["edges"]:
+            db.delete("workflow_edge", id=edge_id)
+        for node_id in selection["nodes"]:
+            if isinstance(node_id, str):
+                workflow.labels.pop(node_id)
+            else:
+                service = db.fetch("service", id=node_id)
+                workflow.services.remove(service)
+                if not service.shared:
+                    db.delete("service", id=service.id)
+        return workflow.last_modified
+
+    def duplicate_workflow(self, workflow_id):
+        workflow = db.fetch("workflow", id=workflow_id)
+        return workflow.duplicate().serialized
 
     def edit_file(self, filepath):
         try:
@@ -347,8 +434,16 @@ class BaseController:
     def get_migration_folders(self):
         return listdir(app.path / "files" / "migrations")
 
+    def get_parent_workflows(self, workflow=None):
+        yield workflow
+        for parent_workflow in workflow.workflows:
+            yield from self.get_parent_workflows(parent_workflow)
+
     def get_properties(self, model, id):
         return db.fetch(model, id=id).get_properties()
+
+    def get_result(self, id):
+        return db.fetch("result", id=id).result
 
     def get_runtimes(self, type, id):
         results = db.fetch("result", allow_none=True, all_matches=True, service_id=id)

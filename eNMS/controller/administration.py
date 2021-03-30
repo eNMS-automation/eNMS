79276337e4c61f65c2a168501131c233e0f0f11d
Antoine Fourmy <antoine.fourmy@gmail.com>
2021-03-30 15:06:44 -0400

    separate controller from app p45

--- eNMS/controller/administration.py
+++ eNMS/controller/administration.py
@@ -33,6 +33,24 @@ from eNMS.setup import properties
 
 
 class BaseController:
+    def add_edge(self, workflow_id, subtype, source, destination):
+        now = app.get_time()
+        workflow = db.fetch("workflow", id=workflow_id, rbac="edit")
+        workflow_edge = self.update(
+            "workflow_edge",
+            rbac=None,
+            **{
+                "name": now,
+                "workflow": workflow_id,
+                "subtype": subtype,
+                "source": source,
+                "destination": destination,
+            },
+        )
+        db.session.commit()
+        workflow.last_modified = now
+        return {"edge": workflow_edge, "update_time": now}
+
     def add_instances_in_bulk(self, **kwargs):
         target = db.fetch(kwargs["relation_type"], id=kwargs["relation_id"])
         if target.type == "pool" and not target.manually_defined:
@@ -97,6 +115,33 @@ class BaseController:
             getattr(target, target_property).remove(instance)
         self.update_rbac(*instances)
         return len(instances)
+
+    def calendar_init(self, type):
+        results = {}
+        for instance in db.fetch_all(type):
+            if getattr(instance, "workflow", None):
+                continue
+            date = getattr(instance, "next_run_time" if type == "task" else "runtime")
+            python_month = search(r".*-(\d{2})-.*", date)
+            if not python_month:
+                continue
+            month = "{:02}".format((int(python_month.group(1)) - 1) % 12)
+            start = [
+                int(i)
+                for i in sub(
+                    r"(\d+)-(\d+)-(\d+) (\d+):(\d+).*",
+                    r"\1," + month + r",\3,\4,\5",
+                    date,
+                ).split(",")
+            ]
+            results[instance.name] = {"start": start, **instance.serialized}
+        return results
+
+    def clear_results(self, service_id):
+        for result in db.fetch(
+            "run", all_matches=True, allow_none=True, service_id=service_id
+        ):
+            db.session.delete(result)
 
     def compare(self, type, id, v1, v2, context_lines):
         if type in ("result", "device_result"):

1531421ea753995ecab3004740ec9bebc2950361
Antoine Fourmy <antoine.fourmy@gmail.com>
2021-04-03 04:49:25 -0400

    refactor setup mechanism

--- eNMS/setup.py
+++ eNMS/setup.py
@@ -1,6 +1,18 @@
 from pathlib import Path
 from json import load
 
-for setup_file in (Path.cwd() / "setup").iterdir():
-    with open(setup_file, "r") as file:
-        locals()[setup_file.stem] = load(file)
+
+class Variables(dict):
+    def __init__(self):
+        self.load_setup_variables()
+        self.load_automation_variables()
+
+    def load_setup_variables(self):
+        for setup_file in (Path.cwd() / "setup").iterdir():
+            with open(setup_file, "r") as file:
+                self[setup_file.stem] = load(file)
+
+    def load_automation_variables(self):
+        pass
+
+locals().update(Variables())

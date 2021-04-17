7dc1f536cdef842114f35ed358b9de25f874f364
Antoine Fourmy <antoine.fourmy@gmail.com>
2021-04-17 04:16:14 -0400

    add scrapli netconf service p1

---
+++ eNMS/services/netconf/scrapli_netconf.py
@@ -0,0 +1,51 @@
+from sqlalchemy import Boolean, ForeignKey, Integer
+from wtforms.widgets import TextArea
+
+from eNMS.database import db
+from eNMS.fields import BooleanField, HiddenField, SelectField, StringField
+from eNMS.forms import ConnectionForm
+from eNMS.models.automation import ConnectionService
+from eNMS.variables import vs
+
+
+class ScrapliService(ConnectionService):
+
+    __tablename__ = "scrapli_service"
+    pretty_name = "Scrapli Commands"
+    parent_type = "connection_service"
+    id = db.Column(Integer, ForeignKey("connection_service.id"), primary_key=True)
+    commands = db.Column(db.LargeString)
+    is_configuration = db.Column(Boolean, default=False)
+    driver = db.Column(db.SmallString)
+    transport = db.Column(db.SmallString, default="system")
+    use_device_driver = db.Column(Boolean, default=True)
+
+    __mapper_args__ = {"polymorphic_identity": "scrapli_service"}
+
+    def job(self, run, device):
+        commands = run.sub(run.commands, locals()).splitlines()
+        function = "send_configs" if run.is_configuration else "send_commands"
+        result = getattr(run.scrapli_connection(device), function)(commands).result
+        return {"commands": commands, "result": result}
+
+
+class ScrapliForm(ConnectionForm):
+    form_type = HiddenField(default="scrapli_service")
+    commands = StringField(substitution=True, widget=TextArea(), render_kw={"rows": 5})
+    is_configuration = BooleanField()
+    driver = SelectField(choices=vs.dualize(vs.scrapli_drivers))
+    transport = SelectField(choices=vs.dualize(("system", "paramiko", "ssh2")))
+    use_device_driver = BooleanField(default=True)
+    groups = {
+        "Main Parameters": {
+            "commands": [
+                "commands",
+                "is_configuration",
+                "driver",
+                "transport",
+                "use_device_driver",
+            ],
+            "default": "expanded",
+        },
+        **ConnectionForm.groups,
+    }
---
+++ eNMS/services/netconf/scrapli_netconf_data.py
@@ -0,0 +1,40 @@
+from sqlalchemy import Boolean, ForeignKey, Integer
+from wtforms.widgets import TextArea
+
+from eNMS.database import db
+from eNMS.fields import BooleanField, HiddenField, SelectField, StringField
+from eNMS.forms import ConnectionForm
+from eNMS.models.automation import ConnectionService
+from eNMS.variables import vs
+
+
+class ScrapliNetconfService(ConnectionService):
+
+    __tablename__ = "scrapli_netconf_service"
+    pretty_name = "Scrapli Netconf"
+    parent_type = "connection_service"
+    id = db.Column(Integer, ForeignKey("connection_service.id"), primary_key=True)
+    command = db.Column(db.SmallString)
+    content = db.Column(db.LargeString)
+
+    __mapper_args__ = {"polymorphic_identity": "scrapli_netconf_service"}
+
+    def job(self, run, device):
+        commands = run.sub(run.commands, locals()).splitlines()
+        function = "send_configs" if run.is_configuration else "send_commands"
+        result = getattr(run.scrapli_connection(device), function)(commands).result
+        return {"commands": commands, "result": result}
+
+
+class ScrapliNetconfForm(ConnectionForm):
+    form_type = HiddenField(default="scrapli_netconf_service")
+    commands = StringField(substitution=True, widget=TextArea(), render_kw={"rows": 5})
+    groups = {
+        "Main Parameters": {
+            "commands": [
+                "command",
+            ],
+            "default": "expanded",
+        },
+        **ConnectionForm.groups,
+    }

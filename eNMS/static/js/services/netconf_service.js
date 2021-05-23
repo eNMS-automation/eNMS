84e4112b35f918be4be198e0a8e7d9f5dcbb4b67
Antoine Fourmy <antoine.fourmy@gmail.com>
2021-05-23 10:53:38 -0400

    update netconf js p3

--- eNMS/static/js/services/netconf_service.js
+++ eNMS/static/js/services/netconf_service.js
@@ -1,29 +1,16 @@
 // eslint-disable-next-line
 function job(id) {
-  setTimeout(() => {
-    const dropdownBaseId = "netconf_service-nc_type";
-    const dropdownId = id ? `#${dropdownBaseId}-${id}` : `#${dropdownBaseId}`;
-    const dropdownObject = $(dropdownId)
-    let inputData = JSON.parse($("#input_data").val());
-    const fields = inputData["fields"];
-    const netconfType = inputData["netconf_type"];
-    let operation = dropdownObject.val();
-    dropdownObject.on("change", function() {
-      operation = $(`${dropdownId} option:selected`).val();
-      console.log(fields)
-      updateElements(fields, netconfType, operation);
-    }).change();
-  }, 500);
-
-  // HELPERS
-  function updateElements(fieldList, netconfTypes, selectedType) {
-    // make all fields invisible
-    fieldList.forEach((field) => {
-      const display = netconfTypes[selectedType].includes(field) ? "block" : "none";
+  const dropdownBaseId = "netconf_service-nc_type";
+  const dropdownId = id ? `#${dropdownBaseId}-${id}` : `#${dropdownBaseId}`;
+  let inputData = JSON.parse($("#input_data").val());
+  $(dropdownId).on("change", function() {
+    operation = $(`${dropdownId} option:selected`).val();
+    inputData["fields"].forEach((field) => {
+      const displayField = inputData["netconf_type"][operation].includes(field);
+      const display = displayField ? "block" : "none"
       const fieldBaseId = `#netconf_service-${field}`
       $(`label[for=${field}]`).css("display", display);
       $(id ? `${fieldBaseId}-${id}` : fieldBaseId).parent().css("display", display);
     });
-  }
-
+  }).change();
 }

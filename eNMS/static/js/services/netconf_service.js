8e72e27b16ed94ca1acd0e498a6805f51c4129e3
Antoine Fourmy <antoine.fourmy@gmail.com>
2021-05-24 05:31:17 -0400

    add container div around service field

--- eNMS/static/js/services/netconf_service.js
+++ eNMS/static/js/services/netconf_service.js
@@ -3,14 +3,18 @@ function job(id) {
   const dropdownBaseId = "netconf_service-nc_type";
   const dropdownId = id ? `#${dropdownBaseId}-${id}` : `#${dropdownBaseId}`;
   let inputData = JSON.parse($("#input_data").val());
-  $(dropdownId).on("change", function() {
-    operation = $(`${dropdownId} option:selected`).val();
-    inputData["fields"].forEach((field) => {
-      const displayField = inputData["netconf_type"][operation].includes(field);
-      const display = displayField ? "block" : "none"
-      const fieldBaseId = `#netconf_service-${field}`
-      $(`label[for=${field}]`).css("display", display);
-      $(id ? `${fieldBaseId}-${id}` : fieldBaseId).parent().css("display", display);
-    });
-  }).change();
+  $(dropdownId)
+    .on("change", function () {
+      operation = $(`${dropdownId} option:selected`).val();
+      inputData["fields"].forEach((field) => {
+        const displayField = inputData["netconf_type"][operation].includes(field);
+        const display = displayField ? "block" : "none";
+        const fieldBaseId = `#netconf_service-${field}`;
+        $(`label[for=${field}]`).css("display", display);
+        $(id ? `${fieldBaseId}-${id}` : fieldBaseId)
+          .parent()
+          .css("display", display);
+      });
+    })
+    .change();
 }

fc6964f03ff1063c01cd8bfa6d87a2f484ce0cb0
Antoine Fourmy <antoine.fourmy@gmail.com>
2021-06-16 04:42:09 -0400

    improve tooltip display

--- eNMS/static/js/base.js
+++ eNMS/static/js/base.js
@@ -234,20 +234,22 @@ export function createTooltips() {
     const id = `tooltip-${$(this).attr("data-tooltip").replace(/\s/g, "")}`;
     jsPanel.tooltip.create({
       id: id,
+      borderRadius: '10px',
       callback: () => setTimeout(() => $(`#${id}`).fadeOut(1000), 2500),
-      content: `<p style="margin-right: 10px; margin-left: 10px; color: black">
-        <b>${$(this).attr("data-tooltip")}</b></p>`,
+      content: `<p style="margin-right: 10px; margin-left: 10px;
+        margin-bottom: 3px; color: white"><b>${$(this).attr("data-tooltip")}</b></p>`,
       contentSize: "auto",
       connector: true,
       delay: 800,
       header: false,
+      opacity: 0.8,
       position: {
         my: "center-bottom",
         at: "center-top",
         of: this,
       },
       target: this,
-      theme: "primary filledlight",
+      theme: "dark filleddark",
     });
   });
 }

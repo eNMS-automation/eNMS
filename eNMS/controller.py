b169fac3e4f387769ae49e2f42e126616631c8b7
Antoine Fourmy <antoine.fourmy@gmail.com>
2021-09-03 05:04:59 -0400

    fix flake8 linting

--- eNMS/controller.py
+++ eNMS/controller.py
@@ -615,23 +615,23 @@ class Controller:
     def get_view_links(self, view_id):
         view, links = db.fetch("view", id=view_id), {}
         nodes = [obj for obj in view.objects if obj.type == "node"]
-        for nodeA in nodes:
-            for nodeB in nodes:
-                link_id = "-".join(map(str, sorted([nodeA.id, nodeB.id])))
-                if nodeA == nodeB or link_id in links:
+        for node1 in nodes:
+            for node2 in nodes:
+                link_id = "-".join(map(str, sorted([node1.id, node2.id])))
+                if node1 == node2 or link_id in links:
                     continue
                 links_objects = db.fetch(
                     "link",
                     all_matches=True,
                     allow_none=True,
-                    source=nodeA.device,
-                    destination=nodeB.device,
+                    source=node1.device,
+                    destination=node2.device,
                 ) + db.fetch(
                     "link",
                     all_matches=True,
                     allow_none=True,
-                    destination=nodeA.device,
-                    source=nodeB.device,
+                    destination=node1.device,
+                    source=node2.device,
                 )
                 links[link_id] = [link.serialized for link in links_objects]
         return links

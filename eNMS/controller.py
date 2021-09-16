15e576d64a21c3409116b651e3b3e8e5cfc90b10
Antoine Fourmy <antoine.fourmy@gmail.com>
2021-09-16 08:38:49 -0400

    fix git config comparison

--- eNMS/controller.py
+++ eNMS/controller.py
@@ -159,9 +159,10 @@ class Controller:
             second = vs.dict_to_string(getattr(db.fetch("result", id=v2), "result"))
         else:
             device = db.fetch("device", id=id)
-            result1, v1 = self.get_git_network_data(device.name, v1)
-            result2, v2 = self.get_git_network_data(device.name, v2)
-            first, second = result1[type], result2[type]
+            result1 = self.get_git_network_data(device.name, v1)
+            result2 = self.get_git_network_data(device.name, v2)
+            v1, v2 = result1["datetime"], result2["datetime"]
+            first, second = result1["result"][type], result2["result"][type]
         return "\n".join(
             unified_diff(
                 first.splitlines(),

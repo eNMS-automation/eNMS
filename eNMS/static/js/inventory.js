76a97cb0a2347688ca1a9693380cf3884fc4bfcd
Antoine Fourmy <antoine.fourmy@gmail.com>
2021-09-20 03:29:47 -0400

    fix dashboard table undefined type bug

--- eNMS/static/js/inventory.js
+++ eNMS/static/js/inventory.js
@@ -81,7 +81,9 @@ function drawDiagrams(type, objects, property) {
       id: id,
       title: `All ${tableType}s with ${property} set to "${value}"`,
       callback: function () {
-        if (formProperties[tableType][property].type == "bool") value = `bool-${value}`;
+        if (formProperties[tableType][property]?.type == "bool") {
+          value = `bool-${value}`;
+        }
         let constraints =
           value == "Empty" ? { model_filter: "empty" } : { [property]: value };
         if (type == "workflow") {

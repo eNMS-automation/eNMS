629282e649c083313898ac84d1166528e44cdd5d
Antoine Fourmy <antoine.fourmy@gmail.com>
2021-10-29 04:24:48 -0400

    forbid skipping if no edit right

--- eNMS/controller.py
+++ eNMS/controller.py
@@ -1151,7 +1151,7 @@ class Controller:
 
     def skip_services(self, workflow_id, service_ids):
         services = [db.fetch("service", id=id) for id in service_ids.split("-")]
-        workflow = db.fetch("workflow", id=workflow_id)
+        workflow = db.fetch("workflow", id=workflow_id, rbac="edit")
         skip = not all(service.skip.get(workflow.name) for service in services)
         for service in services:
             service.skip[workflow.name] = skip

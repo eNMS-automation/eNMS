cc16897ca0ba1ce3df9c42a6a7a2f843074c19ed
Antoine Fourmy <antoine.fourmy@gmail.com>
2021-11-09 05:34:19 -0500

    fix service parameterized run

--- eNMS/static/js/automation.js
+++ eNMS/static/js/automation.js
@@ -728,7 +728,7 @@ Object.assign(action, {
   Duplicate: (service) => showInstancePanel(service.type, service.id, "duplicate"),
   Run: (service) => runService({ id: service.id, form: service.parameterized_form }),
   "Parameterized Run": (service) =>
-    runService({ id: service.id, form: service.parameterized_form }),
+    runService({ id: service.id, parametrization: true }),
   Logs: (service) => showRuntimePanel("logs", service, currentRuntime),
   Results: (service) => showRuntimePanel("results", service, currentRuntime, "result"),
   Backward: () => switchToWorkflow(history[historyPosition - 1], "left"),

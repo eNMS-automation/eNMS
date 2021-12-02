0df09bf249e8e68050b8dedd16380439ad227852
Antoine Fourmy <antoine.fourmy@gmail.com>
2021-12-02 03:23:45 -0500

    troubleshoot sql performance p2

--- files/snippets/sql_performances.py
+++ files/snippets/sql_performances.py
@@ -5,18 +5,21 @@
 from datetime import datetime
 
 device_number = 200
-start_time = datetime.now()
 
+factory_time = datetime.now()
 for index in range(device_number):
     db.factory("device", name=str(index), commit=True)
+print(f"'db.factory' time: {datetime.now() - factory_time}")
 
+fetch_time = datetime.now()
 for index in range(device_number):
     db.fetch("device", name=str(index))
+print(f"'db.fetch' time: {datetime.now() - fetch_time}")
 
+delete_time = datetime.now()
 for index in range(device_number):
     db.delete("device", name=str(index))
     db.session.commit()
+print(f"'db.delete' time: {datetime.now() - delete_time}")
 
-end_time = datetime.now()
-
-print(f"Total duration: {end_time - start_time}")
+print(f"Total duration: {datetime.now() - factory_time}")

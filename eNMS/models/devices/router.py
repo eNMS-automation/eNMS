c5920e6d25a71a8ea33192c39203172a189958a5
Antoine Fourmy <antoine.fourmy@gmail.com>
2021-12-12 02:36:15 -0500

    site builder p54

---
+++ eNMS/models/devices/router.py
@@ -0,0 +1,20 @@
+from sqlalchemy import ForeignKey, Integer
+
+from eNMS.database import db
+from eNMS.forms import DeviceForm
+from eNMS.fields import HiddenField, StringField
+from eNMS.models.inventory import Device
+
+
+class Router(Device):
+
+    __tablename__ = "router"
+    __mapper_args__ = {"polymorphic_identity": "router"}
+    pretty_name = "Router"
+    id = db.Column(Integer, ForeignKey("device.id"), primary_key=True)
+    serial_number = db.Column(db.SmallString)
+
+
+class RouterForm(DeviceForm):
+    form_type = HiddenField(default="router")
+    serial_number = StringField("Serial Number", default=22)

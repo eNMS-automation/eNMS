af55ce1972aa3c36eb36238ba221f159a975d674
Antoine Fourmy <antoine.fourmy@gmail.com>
2021-12-15 06:31:38 -0500

    site builder p86

--- tests/scripts/migration.py
+++ tests/scripts/migration.py
@@ -3,7 +3,8 @@ from ruamel import yaml
 
 
 import_classes = [
-    "service",
+    "device",
+    "link"
 ]
 
 
@@ -15,14 +16,8 @@ def update_property(project, value=None, types=None):
         with open(path / f"{instance_type}.yaml", "r") as migration_file:
             objects = yaml.load(migration_file)
         for obj in objects:
-            if obj["validation_condition"] != "none":
-                if (
-                    obj["validation_method"] == "text"
-                    and obj["content_match"] == ""
-                    or obj["validation_method"] == "dict_included"
-                    and obj["dict_match"] == {}
-                ):
-                    obj["validation_condition"] = "none"
+            obj["scoped_name"] = obj["name"]
+            obj["shared"] = True
         with open(path / f"{instance_type}.yaml", "w") as migration_file:
             yaml.dump(objects, migration_file)
 

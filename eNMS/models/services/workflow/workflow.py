869c4ed15cf2b3eb71dddd9d58a59d57456b51e0
Antoine Fourmy <antoine.fourmy@gmail.com>
2022-01-25 05:55:08 -0500

    fix rbac for workflow edges: limit to users who can edit workflow

--- eNMS/models/services/workflow/workflow.py
+++ eNMS/models/services/workflow/workflow.py
@@ -256,8 +256,19 @@ class WorkflowEdge(AbstractBase):
 
     @classmethod
     def rbac_filter(cls, query, mode, user):
-        originals_alias = aliased(vs.models["service"])
         if mode == "edit":
+            originals_alias = aliased(vs.models["service"])
+            pool_alias = aliased(vs.models["pool"])
+            user_alias = aliased(vs.models["user"])
+            query = (
+                query.join(cls.workflow)
+                .join(vs.models["pool"], vs.models["service"].pools)
+                .join(vs.models["access"], vs.models["pool"].access)
+                .join(pool_alias, vs.models["access"].user_pools)
+                .join(user_alias, pool_alias.users)
+                .filter(vs.models["access"].access_type.contains(mode))
+                .filter(user_alias.name == user.name)
+            )
             query = (
                 query.join(cls.workflow)
                 .join(originals_alias, vs.models["service"].originals)

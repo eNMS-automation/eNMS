ad91d8ea3bc8e7ac05fb1c2064bf7df142772a1f
Antoine Fourmy <antoine.fourmy@gmail.com>
2022-02-17 03:11:52 -0500

    admin approved mechanism p1

--- eNMS/models/services/unix/unix_command.py
+++ eNMS/models/services/unix/unix_command.py
@@ -1,9 +1,9 @@
 from subprocess import check_output
-from sqlalchemy import ForeignKey, Integer
+from sqlalchemy import Boolean, ForeignKey, Integer
 
 from eNMS.database import db
 from eNMS.forms import ServiceForm
-from eNMS.fields import HiddenField, StringField
+from eNMS.fields import BooleanField, HiddenField, StringField
 from eNMS.models.automation import Service
 
 
@@ -13,10 +13,15 @@ class UnixCommandService(Service):
     pretty_name = "Unix Command"
     id = db.Column(Integer, ForeignKey("service.id"), primary_key=True)
     command = db.Column(db.SmallString)
+    admin_approved = db.Column(Boolean, default=False)
 
     __mapper_args__ = {"polymorphic_identity": "unix_command_service"}
 
     def job(self, run, device=None):
+        if not self.admin_approved:
+            log = "This service must be approved by an Admin user first."
+            run.log("error", log, device)
+            return {"success": False, "result": log}
         command = run.sub(run.command, locals())
         run.log("info", f"Running UNIX command: {command}", device)
         return {"command": command, "result": check_output(command.split()).decode()}
@@ -25,3 +30,4 @@ class UnixCommandService(Service):
 class UnixCommandForm(ServiceForm):
     form_type = HiddenField(default="unix_command_service")
     command = StringField(substitution=True)
+    admin_approved = BooleanField("Approved by an Admin user", default=False)

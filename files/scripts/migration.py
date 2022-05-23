79288c12db99eebe6a7a2968fd2a0ad44a4405e8
Antoine Fourmy <antoine.fourmy@gmail.com>
2022-05-23 02:35:41 -0400

    update migration scripts

--- files/scripts/migration.py
+++ files/scripts/migration.py
@@ -2,31 +2,34 @@ from pathlib import Path
 from ruamel import yaml
 
 
-import_classes = ["service"]
+PATH = Path.cwd().parent.parent.parent / "eNMS" / "files" / "migrations"
 
 
-def update_property(project, value=None, types=None):
-    if not types:
-        types = import_classes
-    path = Path.cwd().parent.parent.parent / "eNMS" / "files" / "migrations" / project
-    for instance_type in types:
-        with open(path / f"{instance_type}.yaml", "r") as migration_file:
-            objects = yaml.load(migration_file)
-        for obj in objects:
-            # obj["priority"] += 9
-            if False and obj["type"] == "rest_call_service":
-                obj["custom_username"] = obj.pop("username", "")
-                obj["custom_password"] = obj.pop("password", "")
-            if obj["type"] == "git_service":
-                actions = []
-                if "git_repository" in obj:
-                    obj["local_repository"] = obj.pop("git_repository")
-                for action in ("add_commit", "pull", "push"):
-                    if obj.pop(action, False):
-                        actions.append(action)
-                obj["actions"] = actions
-        with open(path / f"{instance_type}.yaml", "w") as migration_file:
-            yaml.dump(objects, migration_file)
+def migrate_from_4_1_to_4_2(project):
+    with open(PATH / project / "service.yaml", "r") as migration_file:
+        services = yaml.load(migration_file)
+    for service in services:
+        service["priority"] += 9
+        if service["type"] == "rest_call_service":
+            service["custom_username"] = service.pop("username", "")
+            service["custom_password"] = service.pop("password", "")
+        if service["type"] == "git_service":
+            actions = []
+            if "git_repository" in service:
+                service["local_repository"] = service.pop("git_repository")
+            for action in ("add_commit", "pull", "push"):
+                if service.pop(action, False):
+                    actions.append(action)
+            service["actions"] = actions
+    with open(PATH / project / "service.yaml", "w") as migration_file:
+        yaml.dump(services, migration_file)
 
 
-update_property("examples")
+def migrate_from_4_2_to_4_3(project):
+    with open(PATH / project / "service.yaml", "r") as migration_file:
+        services = yaml.load(migration_file)
+    for service in services:
+        if service["type"] == "netmiko_validation_service":
+            service["type"] = "netmiko_commands_service
+    with open(PATH / project / "service.yaml", "w") as migration_file:
+        yaml.dump(services, migration_file)

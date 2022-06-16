e3c501a331cbc837e8611d90e06f1966159cd882
JosephWhite <joseph.white398@yahoo.com>
2022-06-16 13:59:11 -0500

    support function default for custom properties

--- eNMS/database.py
+++ eNMS/database.py
@@ -499,6 +499,8 @@ class Database:
             if values.get("private", False):
                 kwargs = {}
             else:
+                if "default_function" in values and hasattr(vs.custom, values['default_function']):
+                    values["default"] = getattr(vs.custom, values['default_function'])()
                 kwargs = {
                     "default": values["default"],
                     "info": {"log_change": values.get("log_change", True)},

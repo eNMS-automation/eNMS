195e59c2ac6a4af518cc539a758962890e920b25
Antoine Fourmy <antoine.fourmy@gmail.com>
2022-07-13 23:36:25 -0400

    rbac refactoring p35

--- eNMS/static/js/table.js
+++ eNMS/static/js/table.js
@@ -1350,44 +1350,6 @@ tables.user = class UserTable extends Table {
   }
 };
 
-tables.access = class AccessTable extends Table {
-  get controls() {
-    return [
-      this.columnDisplay(),
-      this.refreshTableButton(),
-      this.searchTableButton(),
-      this.clearSearchButton(),
-      this.createNewButton(),
-      this.exportTableButton(),
-      this.bulkDeletionButton(),
-    ];
-  }
-
-  buttons(row) {
-    return [
-      `
-      <ul class="pagination pagination-lg" style="margin: 0px;">
-        <li>
-          <button type="button" class="btn btn-sm btn-primary"
-          onclick="eNMS.base.showInstancePanel('access', '${
-            row.id
-          }')" data-tooltip="Edit"
-            ><span class="glyphicon glyphicon-edit"></span
-          ></button>
-        </li>
-        <li>
-          <button type="button" class="btn btn-sm btn-primary"
-          onclick="eNMS.base.showInstancePanel('access', '${row.id}', 'duplicate')"
-          data-tooltip="Duplicate"
-            ><span class="glyphicon glyphicon-duplicate"></span
-          ></button>
-        </li>
-        ${this.deleteInstanceButton(row)}
-      </ul>`,
-    ];
-  }
-};
-
 tables.credential = class CredentialTable extends Table {
   get controls() {
     return [

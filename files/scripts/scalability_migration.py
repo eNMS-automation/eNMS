30bd0fef01c475feebeefd1450e5ba55f5b33011
Antoine Fourmy <antoine.fourmy@gmail.com>
2022-07-14 07:18:09 -0400

    rbac refactoring p60

--- files/scripts/scalability_migration.py
+++ files/scripts/scalability_migration.py
@@ -5,7 +5,7 @@ from ruamel import yaml
 FILENAME = "scalability"
 PATH = Path.cwd().parent.parent.parent / "eNMS-prod" / "files" / "migrations"
 PROPERTIES = {
-    #"device": ("name", "rbac_read", "rbac_edit"),
+    "device": ("name", "rbac_read", "rbac_edit"),
     "link": ("name", "source", "destination"),
     "service": ("name", "scoped_name", "shared", "services", "type"),
 }
@@ -18,7 +18,11 @@ def update_migration_files():
         updated_instances = []
         for instance in instances:
             updated_instances.append(
-                {property: instance[property] for property in properties if property in instance}
+                {
+                    property: instance[property]
+                    for property in properties
+                    if property in instance
+                }
             )
         with open(PATH / FILENAME / f"{model}.yaml", "w") as migration_file:
             yaml.dump(updated_instances, migration_file)

c24d9151bc36fe626fa7827485161d3fcdf1899d
Antoine Fourmy <antoine.fourmy@gmail.com>
2022-08-12 07:27:51 -0400

    dont display file and folder creation button if parent filtering off

--- eNMS/static/js/table.js
+++ eNMS/static/js/table.js
@@ -1499,7 +1499,7 @@ tables.file = class FileTable extends Table {
       </a>`,
       `
       <button
-        class="btn btn-primary"
+        class="btn btn-primary parent-filtering"
         onclick="eNMS.base.showInstancePanel('folder')"
         data-tooltip="Create New Folder"
         type="button"
@@ -1507,7 +1507,7 @@ tables.file = class FileTable extends Table {
         <span class="glyphicon glyphicon-folder-open"></span>
       </button>`,
       ` <button
-        class="btn btn-primary"
+        class="btn btn-primary parent-filtering"
         onclick="eNMS.administration.showFileUploadPanel()"
         data-tooltip="Upload Files"
         type="button"
@@ -1616,7 +1616,7 @@ tables.file = class FileTable extends Table {
       .selectpicker()
       .on("change", function () {
         self.table.page(0).ajax.reload(null, false);
-        $("#current-folder-path").toggle();
+        $("#current-folder-path,.parent-filtering").toggle();
       });
   }
 };

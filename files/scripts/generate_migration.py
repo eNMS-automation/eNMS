2ed0f26770f43fd381aacb0e18505caf339f6748
Antoine Fourmy <antoine.fourmy@gmail.com>
2022-08-13 08:19:32 -0400

    update scalability generation file p11

--- files/scripts/generate_migration.py
+++ files/scripts/generate_migration.py
@@ -75,16 +75,7 @@ def generate_model_scalability_pool_migration_file():
 
 def generate_model_scalability_user_migration_file():
     path = PATH / "model_scalability"
-    users = [
-        {
-            "name": f"u{index}",
-            "password": (
-                "JGFyZ29uMmlkJHY9MTkkbT0xMDI0MDAsdD0yLHA9OCQyenVuTkdi"
-                "c25iTVdZdXk5TjhhWWt3JEh5VWhTeUlRckJTY21ZZTR2WWNWeFE="
-            ),
-        }
-        for index in range(1, 1_001)
-    ]
+    users = [{"name": f"u{index}"} for index in range(1, 1_001)]
     with open(path / "user.yaml", "w") as migration_file:
         yaml.dump(users, migration_file)
 

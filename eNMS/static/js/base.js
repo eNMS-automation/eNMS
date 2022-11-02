6e7634acf354fc6bf62cc8309ae76b06c7c43e03
Antoine Fourmy <antoine.fourmy@gmail.com>
2022-11-02 09:02:37 -0400

    fix pexpect auto_prompt bug

--- eNMS/static/js/base.js
+++ eNMS/static/js/base.js
@@ -681,6 +681,7 @@ export function showInstancePanel(type, id, mode, tableId, edge) {
             const action = mode ? mode.toUpperCase() : "EDIT";
             panel.setHeaderTitle(`${action} ${type} - ${instance.name}`);
             processInstance(type, instance);
+            if (isService) loadScript(`../static/js/services/${type}.js`, id);
           },
         });
       } else if (mode == "bulk") {
@@ -693,7 +694,7 @@ export function showInstancePanel(type, id, mode, tableId, edge) {
         }
         if (page == "network_builder") updateNetworkPanel(type);
       }
-      if (isService) loadScript(`../static/js/services/${type}.js`, id);
+      if (isService && !id) loadScript(`../static/js/services/${type}.js`);
       const property = isService
         ? "scoped_name"
         : type == "folder"

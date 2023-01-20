ac180821f783db49a89d87c83a0aeb5d256d7d4f
Antoine Fourmy <antoine.fourmy@gmail.com>
2023-01-20 02:21:11 -0500

    use consistent icons for uploading files to folder

--- eNMS/static/js/table.js
+++ eNMS/static/js/table.js
@@ -1616,13 +1616,13 @@ tables.file = class FileTable extends Table {
         <ul class="pagination pagination-lg" style="margin: 0px;">
           ${this.copyClipboardButton(row)}
           ${this.downloadButton(row)}
-          ${this.editButton(row)}
           <button type="button"
             class="btn btn-sm btn-primary"
             onclick="eNMS.administration.showFileUploadPanel('${row.path}')"
           >
-            <span class="glyphicon glyphicon-upload"></span>
+            <span class="glyphicon glyphicon-import"></span>
           </button>
+          ${this.editButton(row)}
           ${this.deleteInstanceButton(row)}
         </ul>
         `,

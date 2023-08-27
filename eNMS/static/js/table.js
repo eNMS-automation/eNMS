887b0c4b32faf17fbd35b03a7efbea1c49ba9042
Antoine Fourmy <antoine.fourmy@gmail.com>
2023-08-27 06:36:09 -0400

    add delete button to worker table

--- eNMS/static/js/table.js
+++ eNMS/static/js/table.js
@@ -1735,10 +1735,13 @@ tables.worker = class WorkerTable extends Table {
       <ul class="pagination pagination-lg" style="margin: 0px;">
         <li>
           <button type="button" class="btn btn-sm btn-primary"
-          onclick="eNMS.base.showInstancePanel('worker', '${row.id}')" data-tooltip="Edit"
+          onclick="eNMS.base.showInstancePanel('worker', '${
+            row.id
+          }')" data-tooltip="Edit"
             ><span class="glyphicon glyphicon-edit"></span
           ></button>
         </li>
+        ${this.deleteInstanceButton(row)}
       </ul>`,
     ];
   }

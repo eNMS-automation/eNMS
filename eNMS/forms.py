9a0c3a9ffc0607cf7b4a7e5b5da0a85c46b194a2
Antoine Fourmy <antoine.fourmy@gmail.com>
2023-09-02 02:43:37 -0400

    add scheduler adress property p3

--- eNMS/forms.py
+++ eNMS/forms.py
@@ -633,7 +633,7 @@ class ServerForm(BaseForm):
     creator = StringField(render_kw={"readonly": True})
     description = StringField(widget=TextArea(), render_kw={"rows": 6})
     ip_address = StringField("IP address")
-    scheduler_address = StringField("Scheduler IP address")
+    scheduler_address = StringField("Scheduler IP")
     location = StringField("Location")
     version = StringField("Version")
     commit_sha = StringField("Commit SHA")

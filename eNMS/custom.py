41fe322dc54cb4b95cc997b40e01239952d242ad
Greg Hering <ghering90@gmail.com>
2024-02-29 18:31:55 -0800

    ldap added @

--- eNMS/custom.py
+++ eNMS/custom.py
@@ -17,7 +17,7 @@ class CustomApp:
             env.log("error", "LDAP authentication failed: no server configured")
             return False
         try:
-            conn = Connection(env.ldap_server, user=(env.ldap_binduser + env.ldap_userdn),
+            conn = Connection(env.ldap_server, user=(env.ldap_binduser + "@" + env.ldap_userdn),
                               password=env.ldap_bindpassword, auto_bind=True)
             conn.search(env.ldap_basedn, '(sAMAccountName={})'.format(name))
             if len(conn.entries) == 1:

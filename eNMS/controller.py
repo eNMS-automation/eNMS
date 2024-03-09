07b50626fd7a5aae998e01533872f343a1bb7960
Antoine Fourmy <antoine.fourmy@gmail.com>
2024-03-09 09:46:10 -0500

    update controller functions check path resolves in files folder

--- eNMS/controller.py
+++ eNMS/controller.py
@@ -317,8 +317,11 @@ class Controller:
 
     def edit_file(self, filepath):
         scoped_path = filepath.replace(">", "/")
+        full_path = f"{vs.file_path}{scoped_path}"
+        if not str(Path(full_path).resolve()).startswith(f"{vs.file_path}/"):
+            return {"error": "The path resolves outside of the files folder."}
         try:
-            with open(f"{vs.file_path}{scoped_path}") as file:
+            with open(full_path) as file:
                 return file.read()
         except FileNotFoundError:
             file = db.fetch("file", path=scoped_path, allow_none=True)
@@ -688,6 +691,8 @@ class Controller:
         path = f"{vs.file_path}{path.replace('>', '/')}"
         if not exists(path):
             return {"alert": "This folder does not exist on the filesystem."}
+        elif not str(Path(path).resolve()).startswith(f"{vs.file_path}/"):
+            return {"error": "The path resolves outside of the files folder."}
         folders = {Path(path)}
         files_set = {
             file
